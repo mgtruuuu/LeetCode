db3c7fe6156e0b91d84f569cc9939fdf7a33e012
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-05-28 18:08:36 +0900

    Time: 4 ms (76.33%), Space: 14.1 MB (5.64%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,19 +1,37 @@
 #include <algorithm>
-#include <vector>
+#include <queue>
+//#include <vector>
 
 class Solution {
-public:
-    int minCostClimbingStairs(vector<int>& cost) {
+  public:
+    int minCostClimbingStairs(vector<int>& cost)
+    {
+        std::queue<int> min_cost_seq;
+        min_cost_seq.push(0);
+        min_cost_seq.push(0);
 
-        std::vector<int> min_cost_seq(cost.size() + 1);
-        min_cost_seq[0] = 0;
-        min_cost_seq[1] = 0;
+        for (auto idx = 2; idx != cost.size() + 1; ++idx) {
 
-        for (auto idx = 2; idx != min_cost_seq.size(); ++idx) {
-            min_cost_seq[idx] = std::min(min_cost_seq[idx - 2] + cost[idx - 2], min_cost_seq[idx - 1] + cost[idx - 1]);
+            const auto front = min_cost_seq.front();
+
+            min_cost_seq.pop();
+            min_cost_seq.push(std::min(front + cost[idx - 2], min_cost_seq.front() + cost[idx - 1]));
         }
 
         return min_cost_seq.back();
-        
     }
+
+    // int minCostClimbingStairs(vector<int>& cost)
+    // {
+    //     std::vector<int> min_cost_seq(cost.size() + 1);
+    //     min_cost_seq[0] = 0;
+    //     min_cost_seq[1] = 0;
+
+    //     for (auto idx = 2; idx != min_cost_seq.size(); ++idx) {
+    //         min_cost_seq[idx] = std::min(min_cost_seq[idx - 2] + cost[idx - 2], min_cost_seq[idx - 1] + cost[idx -
+    //         1]);
+    //     }
+
+    //     return min_cost_seq.back();
+    // }
 };
