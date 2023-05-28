e388f5a6559ac38afeef3d74d6998e1b0d829910
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-05-28 17:29:33 +0900

    Time: 0 ms (100.00%), Space: 6 MB (63.99%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,27 +1,28 @@
-#include <vector>
+#include <queue>
 
 class Solution {
 public:
-    int fib(int n) {
-        
-        if (n == 0)
-        {
+    int fib(int n)
+    {
+        if (n == 0) {
             return 0;
         }
-        else if (n == 1)
-        {
+        else if (n == 1) {
             return 1;
         }
-        
-        std::vector<int> fibonacci_seq(n + 1, 0);
-        fibonacci_seq[0] = 0;
-        fibonacci_seq[1] = 1;
-        
-        for (auto idx = 2; idx < n + 1; ++idx)
-        {
-            fibonacci_seq[idx] = fibonacci_seq[idx - 1] + fibonacci_seq[idx - 2];
+
+        std::queue<int> fibonacci_seq;
+        fibonacci_seq.push(0);
+        fibonacci_seq.push(1);
+
+        for (auto idx = 2; idx < n + 1; ++idx) {
+
+            const auto front = fibonacci_seq.front();
+
+            fibonacci_seq.pop();
+            fibonacci_seq.push(front + fibonacci_seq.front());
         }
-        
-        return fibonacci_seq[n];
+
+        return fibonacci_seq.back();
     }
 };
