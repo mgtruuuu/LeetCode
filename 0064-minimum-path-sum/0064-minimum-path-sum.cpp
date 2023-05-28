c56347fb2fade74e827f6015ff50621bb6831e57
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-05-28 23:39:37 +0900

    Time: 7 ms (90.74%), Space: 9.6 MB (86.16%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,5 +1,3 @@
-#include <algorithm>
-
 class Solution {
   public:
     int minPathSum(vector<vector<int>>& grid)
@@ -16,7 +14,8 @@ class Solution {
             grid[row][0] += grid[row - 1][0];
 
             for (std::size_t col = 1; col != col_size; ++col) {
-                grid[row][col] += std::min(grid[row - 1][col], grid[row][col - 1]);
+                //grid[row][col] += std::min(grid[row - 1][col], grid[row][col - 1]);
+                grid[row][col] += (grid[row - 1][col] < grid[row][col - 1] ? grid[row - 1][col] : grid[row][col - 1]);
             }
         }
 
