befb65ac295c9421517f9abb492ab1d1d2bc8862
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-05-30 09:17:37 +0900

    Time: 4 ms (35.94%), Space: 6.5 MB (41.22%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    
+    std::unordered_map<char, std::string> hash{ { '2', "abc" }, { '3', "def" },  { '4', "ghi" }, { '5', "jkl" },
+                                                { '6', "mno" }, { '7', "pqrs" }, { '8', "tuv" }, { '9', "wxyz" } };
+
+    void backTracking(const std::string& digits, const std::size_t idx, std::string& letter,
+                      std::vector<std::string>& output)
+    {
+        if (idx == digits.size()) {
+            output.emplace_back(letter);
+
+            return;
+        }
+
+        const auto& str_value = hash.at(digits[idx]);
+
+        for (const auto& c : str_value) {
+            letter.push_back(c);
+            backTracking(digits, idx + 1, letter, output);
+            letter.pop_back();
+        }
+    }
+
+    vector<string> letterCombinations(string digits)
+    {
+        if (digits.empty() == true) {
+            return std::vector<std::string>{};
+        }
+
+        std::string empty;
+        std::vector<std::string> res;
+
+        backTracking(digits, 0, empty, res);
+
+        return res;
+    }
+};
