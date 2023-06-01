e5036160d445de6b36f8b6abf014b639faa6e2ab
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-06-01 20:16:01 +0900

    Time: 25 ms (54.48%), Space: 19.8 MB (37.20%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,39 @@
+constexpr auto sudoku_size = 9;
+constexpr auto sudoku_size_root = 3;
+
+class Solution {
+  public:
+    bool isValidSudoku(vector<vector<char>>& board)
+    {
+        std::set<int> rows[sudoku_size];
+        std::set<int> cols[sudoku_size];
+        std::set<int> squares[sudoku_size_root][sudoku_size_root];
+
+        for (std::size_t row = 0; row != sudoku_size; ++row) {
+            for (std::size_t col = 0; col != sudoku_size; ++col) {
+
+                if (board[row][col] == '.') {
+
+                    continue;
+                }
+
+                if (rows[row].find(board[row][col]) != rows[row].end()) {
+                    return false;
+                }
+                rows[row].insert(board[row][col]);
+
+                if (cols[col].find(board[row][col]) != cols[col].end()) {
+                    return false;
+                }
+                cols[col].insert(board[row][col]);
+
+                if (squares[row / 3][col / 3].find(board[row][col]) != squares[row / 3][col / 3].end()) {
+                    return false;
+                }
+                squares[row / 3][col / 3].insert(board[row][col]);
+            }
+        }
+
+        return true;
+    }
+};
