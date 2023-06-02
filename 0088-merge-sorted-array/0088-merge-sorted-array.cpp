6d183cc46692f449ea9e3fc28f4b81dd6298f114
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-06-02 11:42:01 +0900

    Time: 3 ms (61.95%), Space: 9 MB (85.72%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -2,10 +2,10 @@ class Solution {
   public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n)
     {
-        if (nums2.empty() == true) {
+        if (n == 0) {
             return;
         }
-        
+
         auto p_marker = nums1.end() - 1;
         auto p_nums1 = nums1.end() - 1 - n;
         auto p_nums2 = nums2.end() - 1;
@@ -28,10 +28,12 @@ class Solution {
             }
         }
 
-        while (nums2.begin() <= p_nums2) {
-            *p_marker = *p_nums2;
-            --p_nums2;
-            --p_marker;
-        }
+        memcpy(nums1.data(), nums2.data(), sizeof(int) * (std::distance(nums2.begin(), p_nums2) + 1));
+        // while (nums2.begin() <= p_nums2) {
+        //     *p_marker = *p_nums2;
+        //     --p_nums2;
+        //     --p_marker;
+        // }
+        
     }
 };
