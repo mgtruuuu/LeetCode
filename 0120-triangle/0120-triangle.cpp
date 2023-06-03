ec9e363e1388773c03ca175e9ce38e337ce84b30
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-06-03 10:29:32 +0900

    Time: 11 ms (21.29%), Space: 8.4 MB (100.00%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -7,7 +7,10 @@ class Solution {
 
         for (size_t row = row_size - 1; row-- != 0;) {
             for (size_t col = 0; col != row + 1; ++col) {
-                triangle[row][col] += std::min(triangle[row + 1][col], triangle[row + 1][col + 1]);
+                const auto min = triangle[row + 1][col] < triangle[row + 1][col + 1] ? triangle[row + 1][col]
+                                                                                     : triangle[row + 1][col + 1];
+
+                triangle[row][col] += min;
             }
         }
 
