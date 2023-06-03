4933b4916e6459b8389d71f17e858b97a9110da0
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-06-03 09:00:56 +0900

    Time: 29 ms (10.75%), Space: 6 MB (50.62%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -9,30 +9,53 @@ class Solution {
             return true;
         }
 
-        std::vector<int> nums;
-        nums.reserve(10);
-
-        auto q = x;
-        do {
-            q = x / 10;
-            const auto r = x - q * 10;
-            nums.push_back(r);
-            x = q;
-        } while (9 < q);
-        nums.push_back(q);
-
-        auto p_left = nums.begin();
-        auto p_right = nums.end() - 1;
-
-        while (p_left < p_right) {
-            if (*p_left != *p_right) {
+        const auto num_str = std::to_string(x);
+        for (std::size_t idx = 0; idx != num_str.size() / 2; ++idx) {
+            if (num_str[idx] != num_str[num_str.size() - idx - 1]) {
                 return false;
             }
-
-            ++p_left;
-            --p_right;
         }
 
         return true;
     }
 };
+
+
+// // class Solution {
+// //   public:
+// //     bool isPalindrome(int x)
+// //     {
+// //         if (x < 0) {
+// //             return false;
+// //         }
+// //         else if (x < 10) {
+// //             return true;
+// //         }
+
+// //         std::vector<int> nums;
+// //         nums.reserve(10);
+
+// //         auto q = x;
+// //         do {
+// //             q = x / 10;
+// //             const auto r = x - q * 10;
+// //             nums.push_back(r);
+// //             x = q;
+// //         } while (9 < q);
+// //         nums.push_back(q);
+
+// //         auto p_left = nums.begin();
+// //         auto p_right = nums.end() - 1;
+
+// //         while (p_left < p_right) {
+// //             if (*p_left != *p_right) {
+// //                 return false;
+// //             }
+
+// //             ++p_left;
+// //             --p_right;
+// //         }
+
+// //         return true;
+// //     }
+// // };
