caaf600d5f0a4c7a1dbaca6c656e625157cda7be
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-06-04 15:31:33 +0900

    Time: 9 ms (6.29%), Space: 5.9 MB (93.48%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int hasDivisor(int num, const int divisor)
+    {
+        auto quotient = num / divisor;
+        auto remainder = num - quotient * divisor;
+        
+        auto count = 0;
+        
+        while (remainder == 0) {
+            ++count;
+            num = quotient;
+            quotient = num / divisor;
+            remainder = num - quotient * divisor;
+        }
+        
+        return count;
+    }
+    
+    int trailingZeroes(int n) {
+        
+        auto res = 0;
+        for (auto i = 5; i <= n; i += 5) {
+            res += hasDivisor(i, 5);
+        }
+        
+        return res;
+    }
+};
