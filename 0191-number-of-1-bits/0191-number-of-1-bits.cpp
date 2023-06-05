7036fc9aada2cb38efd555d86bef32caef5370ac
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-06-05 16:31:39 +0900

    Time: 0 ms (100.00%), Space: 6.1 MB (30.47%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -2,11 +2,29 @@ class Solution {
   public:
     int hammingWeight(uint32_t n)
     {
-        const std::bitset<32> bits{ n };                
-        
-        return std::bitset<32>(n).count();
+        auto count = 0;
+        while (n != 0) {
+            ++count;
+            n = n & (n - 1); // change the first set bit from right to 0
+        }
+
+        return count;
     }
 };
+
+
+
+// // class Solution {
+// //   public:
+// //     int hammingWeight(uint32_t n)
+// //     {
+// //         const std::bitset<32> bits{ n };                
+        
+// //         return std::bitset<32>(n).count();
+// //     }
+// // };
+
+
 
 // // class Solution {
 // //   public:
