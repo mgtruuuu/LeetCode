17f5717199c65ef396ca5bd1211ea8b4e155af3b
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-06-05 16:17:09 +0900

    Time: 0 ms (100.00%), Space: 6.1 MB (30.47%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,15 +1,37 @@
 class Solution {
-public:
-    int hammingWeight(uint32_t n) {
+  public:
+    int hammingWeight(uint32_t n)
+    {
         auto count = 0;
         while (n != 0) {
-            if (n & 1) {
-                ++count;
-            }
-
-            n = n >> 1;
+            ++count;
+            n = n & (n - 1); // change the first set bit from right to 0
         }
 
         return count;
     }
 };
+
+// // class Solution {
+// //   public:
+// //     int hammingWeight(uint32_t n)
+// //     {
+// //         return bitset<32>(n).count();
+// //     }
+// // }
+
+// // class Solution {
+// //   public:
+// //     int hammingWeight(uint32_t n)
+// //     {
+// //         auto count = 0;
+// //         while (n != 0) {
+// //             if (n & 1) {
+// //                 ++count;
+// //             }
+
+// //             n = n >> 1;
+// //         }
+
+// //         return count;
+// //     
