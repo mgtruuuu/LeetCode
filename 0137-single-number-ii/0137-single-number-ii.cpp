2fabf8c834aa30598b43b93c48242bec4d4aa727
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-06-07 00:04:26 +0900

    Time: 7 ms (83.47%), Space: 9.6 MB (36.40%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -4,18 +4,17 @@ class Solution {
     {
         int bits[32]{};
 
-        std::bitset<32> num_bitset{ 0 };
         for (const auto& num : nums) {
-            num_bitset = num;
+            auto num_bitset = num;
 
             for (std::size_t j = 0; j != 32; ++j) {
-                if (num_bitset.test(j) == true) {
+
+                if (num_bitset & 1) {
                     ++bits[j];
                 }
-            }
-        }
 
-        for (std::size_t idx = 0; idx != 32; ++idx) {
+                num_bitset >>= 1;
+            }
         }
 
         auto res = 0; // 0b0000'0000'0000'0000'0000'0000'0000'0000
@@ -29,3 +28,37 @@ class Solution {
         return res;
     }
 };
+
+
+
+// class Solution {
+//   public:
+//     int singleNumber(vector<int>& nums)
+//     {
+//         int bits[32]{};
+
+//         std::bitset<32> num_bitset{ 0 };
+//         for (const auto& num : nums) {
+//             num_bitset = num;
+
+//             for (std::size_t j = 0; j != 32; ++j) {
+//                 if (num_bitset.test(j) == true) {
+//                     ++bits[j];
+//                 }
+//             }
+//         }
+
+//         for (std::size_t idx = 0; idx != 32; ++idx) {
+//         }
+
+//         auto res = 0; // 0b0000'0000'0000'0000'0000'0000'0000'0000
+//         for (std::size_t idx = 0; idx != 32; ++idx) {
+
+//             if (bits[idx] % 3 != 0) {
+//                 res |= (1 << idx);
+//             }
+//         }
+
+//         return res;
+//     }
+// };
