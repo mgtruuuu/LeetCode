614eb1514efb5fb22ab7c4c2e747e936c58c213a
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-06-06 21:49:45 +0900

    Time: 25 ms (42.61%), Space: 16.9 MB (74.16%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,19 +1,36 @@
 class Solution {
   public:
-    int singleNumber(vector<int>& nums)
+    int singleNumber(vector<int>& arr)
     {
-        std::bitset<60001> bits{ 0 };
+        const auto size_arr = arr.size(); // extracting the size of the array
 
-        for (const auto& num : nums) {
-            bits.flip(num + 30000);
+        // traverse from the array
+        for (std::size_t idx = 0; idx != size_arr - 1; ++idx) {
+            arr[idx + 1] = arr[idx] ^ arr[idx + 1]; // (prev answer xor current index)
         }
 
-        for (auto num = -30000; num != 30001; ++num) {
-            if (bits.test(num + 30000) == true) {
-                return num;
-            }
-        }
-
-        return 0;
+        return arr[size_arr - 1]; // return left over element
     }
 };
+
+
+
+// class Solution {
+//   public:
+//     int singleNumber(vector<int>& nums)
+//     {
+//         std::bitset<60001> bits{ 0 };
+
+//         for (const auto& num : nums) {
+//             bits.flip(num + 30000);
+//         }
+
+//         for (auto num = -30000; num != 30001; ++num) {
+//             if (bits.test(num + 30000) == true) {
+//                 return num;
+//             }
+//         }
+
+//         return 0;
+//     }
+// };
