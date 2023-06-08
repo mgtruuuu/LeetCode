1c3f0da0e944c5df03b58d119be2eb87f8d18316
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-06-08 23:29:55 +0900

    Time: 4 ms (43.02%), Space: 6 MB (71.72%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  public:
+    double myPow(double x, int n)
+    {
+        if (n == 0 || x == 1.0) {
+            return 1.0;
+        }
+        else if (n > 0) {
+            return myPowHelper(x, n);
+        }
+        else if (n != -2147483648) {
+            return 1.0 / myPowHelper(x, static_cast<uint32_t>(-n));
+        }
+        else {
+            return 1.0 / myPowHelper(x, static_cast<uint32_t>(2147483648));
+        }
+    }
+
+    double myPowHelper(double x, uint32_t n)
+    {
+        auto res = 1.0;
+
+        do {
+            if (n & 1) {
+                res *= x;
+            }
+
+            n >>= 1;
+            x *= x;
+        } while (n != 0);
+
+        return res;
+    }
+};
