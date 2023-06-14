944be43d81526599f1eca8c2baa8dd99d9a8bb4f
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-06-14 10:06:27 +0900

    Time: 23 ms (52.57%), Space: 19.6 MB (17.08%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -36,3 +36,22 @@ class Solution {
         return static_cast<int>(res);
     }
 };
+
+
+// // class Solution {
+// //   public:
+// //     int majorityElement(vector<int>& nums)
+// //     {
+// //         unordered_map<int, int> hash;
+// //         for (const auto num : nums) {
+// //             ++hash[num];
+// //         }
+
+// //         auto max_val = nums[0];
+// //         for (auto& [a, b] : hash) {
+// //             if (b > hash[max_val])
+// //                 max_val = a;
+// //         }
+// //         return max_val;
+// //     }
+// // };
