a559805c62bce6e0aea3f730ff0d0443c5febc95
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-06-14 13:55:51 +0900

    Time: 137 ms (80.15%), Space: 93.2 MB (76.83%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -7,11 +7,13 @@ class Solution {
         for (auto i = std::size_t(0); i < prices.size(); ++i) {
             if (prices[i] < min_price) {
                 min_price = prices[i];
-            }
-            else if (prices[i] - min_price > max_profit) {
-                max_profit = prices[i] - min_price;
-            }
                 
+                continue;
+            }
+            
+            if (prices[i] - min_price > max_profit) {
+                max_profit = prices[i] - min_price;
+            }    
         }
         
         return max_profit;
