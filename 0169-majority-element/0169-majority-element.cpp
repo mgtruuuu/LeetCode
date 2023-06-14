4dda1f7b191b7102a82fc4582265601fdbd63058
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-06-14 10:05:04 +0900

    Time: 20 ms (68.51%), Space: 19.5 MB (59.77%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,16 +2,37 @@ class Solution {
   public:
     int majorityElement(vector<int>& nums)
     {
-        unordered_map<int, int> hash;
-        for (const auto num : nums) {
-            ++hash[num];
+        std::array<int, 32> bits_count;
+        bits_count.fill(0);
+
+        for (const auto& num : nums) {
+
+            auto num_bits = static_cast<uint32_t>(num);
+            auto idx = std::size_t(0);
+
+            while (num_bits != 0) {
+                if (num_bits & 1) {
+                    ++bits_count[idx];
+                }
+
+                ++idx;
+                num_bits >>= 1;
+            }
         }
 
-        auto max_val = nums[0];
-        for (auto& [a, b] : hash) {
-            if (b > hash[max_val])
-                max_val = a;
+        auto res = std::size_t(0);
+
+        for (std::size_t idx = 0; idx != bits_count.size(); ++idx) {
+            if (bits_count[idx] > (nums.size() / 2)) {
+                auto pow_two = std::size_t(1);
+                for (std::size_t i = 0; i != idx; ++i) {
+                    pow_two *= 2;
+                }
+
+                res += pow_two;
+            }
         }
-        return max_val;
+
+        return static_cast<int>(res);
     }
 };
