fb5e8d40ad498a0805f415969c053e5ee2522cbc
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-06-14 14:01:22 +0900

    Time: 138 ms (78.60%), Space: 93.3 MB (76.83%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -2,7 +2,7 @@ class Solution {
   public:
     int maxProfit(vector<int>& prices)
     {
-        auto min_price = 100000;
+        auto min_price = std::numeric_limits<int>::max();
         auto max_profit = 0;
         for (auto i = std::size_t(0); i < prices.size(); ++i) {
             if (prices[i] < min_price) {
