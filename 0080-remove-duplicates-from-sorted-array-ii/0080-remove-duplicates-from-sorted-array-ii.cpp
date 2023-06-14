11a5f04f92b9671a9063019c928342ef424a1138
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-06-14 23:10:46 +0900

    Time: 8 ms (51.92%), Space: 10.9 MB (64.22%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -22,3 +22,47 @@ class Solution {
         return idx_lhs;
     }
 };
+
+
+
+// // class Solution {
+// //   public:
+// //     int removeDuplicates(vector<int>& nums)
+// //     {
+// //         const auto len_nums = nums.size();
+
+// //         if (len_nums <= 2) {
+// //             return len_nums;
+// //         }
+
+// //         auto idx_lhs = std::size_t(2);
+// //         while (idx_lhs != len_nums) {
+// //             if (nums[idx_lhs - 2] == nums[idx_lhs]) {
+// //                 break;
+// //             }
+
+// //             ++idx_lhs;
+// //         }
+
+// //         for (auto idx_rhs = idx_lhs + 1; idx_rhs < len_nums; ++idx_rhs) {
+
+// //             if (nums[idx_lhs - 1] == nums[idx_rhs]) {
+// //                 continue;
+// //             }
+
+// //             do {
+// //                 std::swap(nums[idx_lhs], nums[idx_rhs]);
+// //                 ++idx_lhs;
+// //                 ++idx_rhs;
+
+// //                 if (idx_rhs == len_nums) {
+// //                     return idx_lhs;
+// //                 }
+// //             } while (nums[idx_lhs - 2] != nums[idx_lhs - 1]);
+
+// //             --idx_rhs;
+// //         }
+
+// //         return idx_lhs;
+// //     }
+// // };
