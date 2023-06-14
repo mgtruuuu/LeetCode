e0b06a58f39b054cbbe699a4f044a3570e84976d
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-06-14 23:10:26 +0900

    Time: 4 ms (84.75%), Space: 10.9 MB (31.45%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -9,31 +9,14 @@ class Solution {
         }
 
         auto idx_lhs = std::size_t(2);
-        while (idx_lhs != len_nums) {
-            if (nums[idx_lhs - 2] == nums[idx_lhs]) {
-                break;
-            }
-
-            ++idx_lhs;
-        }
-
-        for (auto idx_rhs = idx_lhs + 1; idx_rhs < len_nums; ++idx_rhs) {
-
-            if (nums[idx_lhs - 1] == nums[idx_rhs]) {
+        for (auto idx_rhs = idx_lhs; idx_rhs != len_nums; ++idx_rhs) {
+            if (nums[idx_lhs - 2] == nums[idx_rhs]) {
                 continue;
             }
 
-            do {
-                std::swap(nums[idx_lhs], nums[idx_rhs]);
-                ++idx_lhs;
-                ++idx_rhs;
+            nums[idx_lhs] = nums[idx_rhs];
 
-                if (idx_rhs == len_nums) {
-                    return idx_lhs;
-                }
-            } while (nums[idx_lhs - 2] != nums[idx_lhs - 1]);
-
-            --idx_rhs;
+            ++idx_lhs;
         }
 
         return idx_lhs;
