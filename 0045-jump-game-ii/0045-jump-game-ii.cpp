0e97c576be378538e50ba696f0679d6c250bbdc9
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-06-15 11:56:52 +0900

    Time: 215 ms (35.29%), Space: 17.2 MB (29.03%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+  public:
+    int jump(vector<int>& nums)
+    {
+        auto len_nums = nums.size();
+
+        if (len_nums == 1) {
+            return 0;
+        }
+
+        if (nums[0] == 0) {
+            return false;
+        }
+
+        std::vector<int> counts(len_nums, 10000);
+
+        const auto idx_end = (nums[0] + 1 < len_nums) ? nums[0] + 1 : len_nums;
+        for (auto idx = 1; idx != idx_end; ++idx) {
+            counts[idx] = 1;
+        }
+
+        for (auto idx = std::size_t(1); idx != len_nums - 1; ++idx) {
+
+            const auto inner_end = (idx + 1 + nums[idx] < len_nums) ? idx + 1 + nums[idx] : len_nums;
+
+            for (auto idx_inner = idx + 1; idx_inner != inner_end; ++idx_inner) {
+                if (counts[idx] + 1 < counts[idx_inner]) {
+                    counts[idx_inner] = counts[idx] + 1;
+                }
+            }
+        }
+
+        return counts[len_nums - 1];
+    }
+};
