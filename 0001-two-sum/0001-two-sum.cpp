ec8d31f970a61060bec840d00251b20a68811826
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-06-18 10:36:10 +0900

    Time: 13 ms (72.37%), Space: 10.7 MB (45.22%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,18 +2,25 @@ class Solution {
   public:
     vector<int> twoSum(vector<int>& numbers, int target)
     {
+        std::vector<int> res(2);
         std::unordered_map<int, int> hash;
 
-        for (auto idx = 0; idx != static_cast<int>(numbers.size()); ++idx) {
+        auto idx = 0;
+        while (true) {
             const auto ptr = hash.find(target - numbers[idx]);
             if (ptr != hash.end()) {
-                return std::vector<int>{ ptr->second, idx };
+
+                res[0] = ptr->second;
+                res[1] = idx;
+
+                break;
             }
 
             hash.insert({ numbers[idx], idx });
+
+            ++idx;
         }
 
-        assert(false);
-        return std::vector<int>{};
+        return res;
     }
 };
