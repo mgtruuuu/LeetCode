6885f6aeacc8c123d4e56e1b5d03a416c5f453ef
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-06-19 15:46:08 +0900

    Time: 6 ms (73.76%), Space: 7.1 MB (31.29%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -2,13 +2,13 @@ class Solution {
   public:
     bool isIsomorphic(string s, string t)
     {
-        std::unordered_set<char> check_duplication;
         std::unordered_map<char, char> hash_s2t;
+        std::unordered_set<char> check_duplication;
 
         for (auto idx = std::size_t(0); idx != s.size(); ++idx) {
-            if (hash_s2t.find(s[idx]) == hash_s2t.end()) {
+            if (hash_s2t.count(s[idx]) == 0) {
 
-                if (check_duplication.find(t[idx]) == check_duplication.end()) {
+                if (check_duplication.count(t[idx]) == 0) {
                     check_duplication.insert(t[idx]);
                     hash_s2t[s[idx]] = t[idx];
                 }
