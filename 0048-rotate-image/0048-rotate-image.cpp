c826eade51ee3d32c0093aee13173b5c85e1e15a
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-06-19 14:48:40 +0900

    Time: 3 ms (45.46%), Space: 7.1 MB (53.90%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -3,10 +3,9 @@ class Solution {
     void rotate(vector<vector<int>>& matrix)
     {
         const auto n_mat = matrix.size();
-        const auto n_iteration = matrix.size() >> 1;
 
         auto temp = 0;
-        for (auto i = 0; i != n_iteration; ++i) {
+        for (auto i = 0; i != (matrix.size() >> 1); ++i) {
             for (auto k = i; k != n_mat - 1 - i; ++k) {
                 temp = matrix[i][k];
                 matrix[i][k] = matrix[n_mat - 1 - k][i];
