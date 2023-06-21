b126f825194a79d1e09ccb1af2cf7c5f14ac5ec4
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-06-21 19:45:24 +0900

    Time: 0 ms (100.00%), Space: 6.6 MB (90.76%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -2,24 +2,14 @@ class Solution {
   public:
     char findTheDifference(string s, string t)
     {
-        if (s.size() < t.size()) {
-            auto temp = std::move(s);
-            s = std::move(t);
-            t = std::move(temp);
-        }
-
-        std::unordered_map<char, int> char2count;
-        for (const auto& ch : t) {
-            ++char2count[ch];
-        }
-
+        auto sum = uint8_t(0);
         for (const auto& ch : s) {
-
-            if (--char2count[ch] == -1) {
-                return ch;
-            }
+            sum ^= static_cast<uint8_t>(ch);
+        }
+        for (const auto& ch : t) {
+            sum ^= static_cast<uint8_t>(ch);
         }
 
-        return '0'; // must not be returned
+        return static_cast<char>(sum);
     }
 };
