d99d94389d1963b2e6a1bf7e48ad386d95bfc3aa
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-06-21 19:45:57 +0900

    Time: 0 ms (100.00%), Space: 6.5 MB (90.76%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -13,3 +13,32 @@ class Solution {
         return static_cast<char>(sum);
     }
 };
+
+
+
+
+// // class Solution {
+// //   public:
+// //     char findTheDifference(string s, string t)
+// //     {
+// //         if (s.size() < t.size()) {
+// //             auto temp = std::move(s);
+// //             s = std::move(t);
+// //             t = std::move(temp);
+// //         }
+
+// //         std::unordered_map<char, int> char2count;
+// //         for (const auto& ch : t) {
+// //             ++char2count[ch];
+// //         }
+
+// //         for (const auto& ch : s) {
+
+// //             if (--char2count[ch] == -1) {
+// //                 return ch;
+// //             }
+// //         }
+
+// //         return '0'; // must not be returned
+// //     }
+// // };  
