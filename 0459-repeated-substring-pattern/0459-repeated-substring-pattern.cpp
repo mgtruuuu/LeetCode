f5d9f23b219cd614af55488fe9d07f3274003db1
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-06-23 13:21:46 +0900

    Time: 32 ms (31.03%), Space: 12.6 MB (81.27%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -0,0 +1,56 @@
+class Solution {
+  public:
+    bool repeatedSubstringPattern(string s)
+    {
+        const auto len_s = static_cast<int>(s.size());
+        const auto value = getLPS(s).back();
+
+        std::cout << value << std::endl;
+
+        if (value != 0 && len_s % (len_s - value) == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+    std::vector<int> getLPS(const std::string& arr)
+    {
+        auto lps = std::vector<int>(arr.size());
+
+        lps[0] = 0;
+
+        for (auto idx = std::size_t(1); idx != arr.size(); ++idx) {
+            if (arr[lps[idx - 1]] == arr[idx]) {
+                lps[idx] = lps[idx - 1] + 1;
+
+                continue;
+            }
+
+            auto count = lps[idx - 1];
+
+            while (count != 0) {
+                auto check = true;
+                auto idx_rhs = idx;
+                for (auto idx_lhs = count; idx_lhs-- != 0;) {
+                    if (arr[idx_lhs] != arr[idx_rhs--]) {
+                        check = false;
+
+                        break;
+                    }
+                }
+
+                if (check == true) {
+                    break;
+                }
+
+                --count;
+            }
+
+            lps[idx] = count;
+        }
+
+        return lps;
+    }
+};
