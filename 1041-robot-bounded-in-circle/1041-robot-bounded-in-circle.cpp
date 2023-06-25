f67977b9fd6668e0bcdf2962952685170c8e8110
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-06-25 21:50:49 +0900

    Time: 0 ms (100.00%), Space: 6.1 MB (82.42%) - LeetHub

---
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
@@ -0,0 +1,52 @@
+class Solution {
+  public:
+    bool isRobotBounded(string instructions)
+    {
+        auto x = 0;
+        auto y = 0;
+        auto dir = 0; // 0:UP, 1:LEFT, 2:DOWN, 3:RIGHT
+
+        for (const auto& ch : instructions) {
+            switch (ch) {
+            case 'R':
+                dir += 1;
+                if (dir == 4) {
+                    dir = 0;
+                }
+                break;
+
+            case 'L':
+                dir -= 1;
+                if (dir == -1) {
+                    dir = 3;
+                }
+                break;
+
+            default:
+                switch (dir) {
+                case 0:
+                    ++y;
+                    break;
+                case 1:
+                    --x;
+                    break;
+                case 2:
+                    --y;
+                    break;
+                default:
+                    ++x;
+                    break;
+                }
+
+                break;
+            }
+        }
+
+        if ((x != 0 || y != 0) && dir == 0) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+};
