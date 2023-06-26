88f592f4331b7f2201489496472fd4507b6cf49f
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-06-26 09:39:15 +0900

    Time: 19 ms (29.10%), Space: 11.1 MB (78.43%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -16,3 +16,24 @@ class Solution {
         return sum;
     }
 };
+
+
+
+// // class Solution {
+// //   public:
+// //     int diagonalSum(vector<vector<int>>& mat)
+// //     {
+// //         const auto len_mat = mat.size();
+// //         auto sum = 0;
+
+// //         for (auto idx_row = std::size_t(0); idx_row != len_mat; ++idx_row) {
+// //             for (auto idx_col = std::size_t(0); idx_col != len_mat; ++idx_col) {
+// //                 if (idx_row + idx_col + 1 == len_mat || idx_row == idx_col) {
+// //                     sum += mat[idx_row][idx_col];
+// //                 }
+// //             }
+// //         }
+
+// //         return sum;
+// //     }
+// // };
