16c042b0fa9bcca84d60d405703fa7d92b1121d1
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-06-26 16:35:58 +0900

    Time: 36 ms (71.95%), Space: 71.4 MB (71.93%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,13 +1,3 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
   public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
@@ -15,75 +5,71 @@ class Solution {
         auto* p_l1 = l1;
         auto* p_l2 = l2;
 
-        ListNode* res = new ListNode{};
-        auto* p_res = res;
-        ListNode* p_res_prev = nullptr;
+        ListNode* p_res = nullptr;
+        ListNode** pp_res = &p_res;
 
         auto plus_one = 0;
         while (p_l1 != nullptr && p_l2 != nullptr) {
 
+            *pp_res = new ListNode{};
+
             if (p_l1->val + p_l2->val + plus_one > 9) {
-                p_res->val = p_l1->val + p_l2->val + plus_one - 10;
+                (*pp_res)->val = p_l1->val + p_l2->val + plus_one - 10;
                 plus_one = 1;
             }
             else {
-                p_res->val = p_l1->val + p_l2->val + plus_one;
+                (*pp_res)->val = p_l1->val + p_l2->val + plus_one;
                 plus_one = 0;
             }
 
             p_l1 = p_l1->next;
             p_l2 = p_l2->next;
-            p_res_prev = p_res;
-            p_res->next = new ListNode{};
-            p_res = p_res->next;
+            pp_res = &(*pp_res)->next;
         }
 
         if (p_l1 == nullptr) {
             while (p_l2 != nullptr) {
 
+                *pp_res = new ListNode{};
+
                 if (p_l2->val + plus_one > 9) {
-                    p_res->val = p_l2->val + plus_one - 10;
+                    (*pp_res)->val = p_l2->val + plus_one - 10;
                     plus_one = 1;
                 }
                 else {
-                    p_res->val = p_l2->val + plus_one;
+                    (*pp_res)->val = p_l2->val + plus_one;
                     plus_one = 0;
                 }
 
                 p_l2 = p_l2->next;
-                p_res_prev = p_res;
-                p_res->next = new ListNode{};
-                p_res = p_res->next;
+                pp_res = &(*pp_res)->next;
             }
         }
         else {
             while (p_l1 != nullptr) {
 
+                *pp_res = new ListNode{};
+
                 if (p_l1->val + plus_one > 9) {
-                    p_res->val = p_l1->val + plus_one - 10;
+                    (*pp_res)->val = p_l1->val + plus_one - 10;
                     plus_one = 1;
                 }
                 else {
-                    p_res->val = p_l1->val + plus_one;
+                    (*pp_res)->val = p_l1->val + plus_one;
                     plus_one = 0;
                 }
 
                 p_l1 = p_l1->next;
-                p_res_prev = p_res;
-                p_res->next = new ListNode{};
-                p_res = p_res->next;
+                pp_res = &(*pp_res)->next;
             }
         }
 
         if (plus_one == 1) {
-            p_res->val = plus_one;
-            p_res->next = nullptr;
+            *pp_res = new ListNode{};
+            (*pp_res)->val = plus_one;
+            (*pp_res)->next = nullptr;
         }
-        else {
-            delete p_res;
-            p_res_prev->next = nullptr;            
-        }
-        
-        return res;
+
+        return p_res;
     }
 };
