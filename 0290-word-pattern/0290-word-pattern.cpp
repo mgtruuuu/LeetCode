00698fc1d2036d21bec5306b46d022c14b26a41c
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-06-28 15:21:33 +0900

    Time: 4 ms (39.71%), Space: 6.2 MB (99.73%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -4,7 +4,7 @@ class Solution {
     {
         std::unordered_map<char, std::string> char2str;
 
-        auto count = 0;
+        auto count = std::size_t(0);
         auto idx_head = std::size_t(0);
         for (auto idx_tail = std::size_t(0); idx_tail != s.size() + 1; ++idx_tail) {
 
@@ -31,7 +31,7 @@ class Solution {
             ++count;
         }
 
-        if (count == static_cast<int>(pattern.size())) {
+        if (count == pattern.size()) {
             return true;
         }
         else {
