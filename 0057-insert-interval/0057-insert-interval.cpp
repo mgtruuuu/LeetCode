4eeda0d17f430ef333e5259724fd82ca001b3b19
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-06-30 10:25:58 +0900

    Time: 16 ms (72.27%), Space: 17.1 MB (29.16%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -6,6 +6,9 @@ class Solution {
             return { newInterval };
         }
 
+        std::vector<std::vector<int>> res;
+        res.reserve(intervals.size() + 1);
+
         auto idx_new_front = std::size_t(0);
         while (idx_new_front != intervals.size()) {
 
@@ -13,14 +16,18 @@ class Solution {
                 break;
             }
             else {
+
+                res.push_back(intervals[idx_new_front]);
+
                 ++idx_new_front;
             }
         }
 
         if (idx_new_front == intervals.size()) {
-            std::vector<std::vector<int>> temp = intervals;
-            temp.push_back(newInterval);
-            return temp;
+            res.push_back(newInterval);
+            res.shrink_to_fit();
+
+            return res;
         }
 
         auto idx_new_back = idx_new_front;
@@ -34,26 +41,22 @@ class Solution {
         }
 
         if (idx_new_back == 0) {
-            std::vector<std::vector<int>> temp;
-            temp.reserve(intervals.size() + 1);
-            temp.push_back(newInterval);
+            res.push_back(newInterval);
             for (const auto& interval : intervals) {
-                temp.push_back(interval);
+                res.push_back(interval);
             }
-            return temp;
+            return res;
         }
 
         --idx_new_back;
 
-        std::vector<std::vector<int>> res;
-        for (auto idx = std::size_t(0); idx != idx_new_front; ++idx) {
-            res.push_back(intervals[idx]);
-        }
         res.push_back(std::vector<int>{ std::min(newInterval.front(), intervals[idx_new_front].front()),
                                         std::max(newInterval.back(), intervals[idx_new_back].back()) });
         for (auto idx = idx_new_back + 1; idx != intervals.size(); ++idx) {
             res.push_back(intervals[idx]);
         }
+        res.shrink_to_fit();
+
         return res;
     }
 };
