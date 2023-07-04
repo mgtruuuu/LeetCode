f24c3bb64657b34fa8c5b418f18084dfe6958660
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-07-04 17:10:21 +0900

    Time: 0 ms (100.00%), Space: 6 MB (19.65%) - LeetHub

---
+++ 0504-base-7/0504-base-7.cpp
@@ -0,0 +1,42 @@
+class Solution {
+  public:
+    string convertToBase7(int num)
+    {
+        std::string res;
+        auto is_positive = true;
+        
+        if (num == 0) {
+            return "0";
+        }
+        else if (num < 0) {
+            is_positive = false;
+            res.push_back('-');
+            
+            num = -num;
+        }
+
+        while (num != 0) {
+
+            auto r = num;
+            num /= 7;
+            r -= 7 * num;
+
+            res.push_back(r + static_cast<int>('0'));
+        }
+
+std::cout << res << std::endl;        
+        
+        if (is_positive == true) {
+            for (auto idx = std::size_t(0); idx != res.size() / 2; ++idx) {
+                std::swap(res[idx], res[res.size() - 1 - idx]);
+            }
+        }
+        else {
+            for (auto idx = std::size_t(0); idx != (res.size() - 1) / 2; ++idx) {
+                std::swap(res[idx + 1], res[res.size() - 1 - idx]);
+            }
+        }
+
+        return res;
+    }
+};
