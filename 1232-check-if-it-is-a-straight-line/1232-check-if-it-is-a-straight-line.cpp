d53385b5bfa446ac528aca3470e9e2d00d096b36
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-07-04 12:27:45 +0900

    Time: 12 ms (39.77%), Space: 10 MB (89.53%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
@@ -2,13 +2,16 @@ class Solution {
   public:
     bool checkStraightLine(vector<vector<int>>& coordinates)
     {
-        const auto delta_x_init = coordinates[1].front() - coordinates[0].front();
-        const auto delta_y_init = coordinates[1].back() - coordinates[0].back();
+        const auto x_init = coordinates[0].front();
+        const auto y_init = coordinates[0].back();
+        
+        const auto delta_x_init = coordinates[1].front() - x_init;
+        const auto delta_y_init = coordinates[1].back() - y_init;
 
-        for (auto idx = std::size_t(1); idx != coordinates.size() - 1; ++idx) {
+        for (auto idx = std::size_t(2); idx != coordinates.size(); ++idx) {
 
-            const auto delta_x = coordinates[idx + 1].front() - coordinates[idx].front();
-            const auto delta_y = coordinates[idx + 1].back() - coordinates[idx].back();
+            const auto delta_x = coordinates[idx].front() - x_init;
+            const auto delta_y = coordinates[idx].back() - y_init;
 
             if (delta_x_init * delta_y != delta_y_init * delta_x) {
                 return false;
