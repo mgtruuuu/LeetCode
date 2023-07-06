3e3581aec267463d1ad1d994b1696436494224bc
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-07-06 23:15:10 +0900

    Time: 0 ms (100.00%), Space: 5.8 MB (94.71%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -12,17 +12,27 @@ class Solution {
             return 1.0 / myPow(x * x, static_cast<int>(static_cast<uint32_t>(n) >> 1));
         }
         else if (n < 0) {
-            return 1.0 / myPow(x, static_cast<int>((n ^ 0xFFFFFFFF) + 1));
+            n = static_cast<int>((n ^ 0xFFFFFFFF) + 1);
+            x = 1.0 / x;
         }
 
         // Perform Binary Exponentiation.
-        // If 'n' is odd we perform Binary Exponentiation on 'n - 1' and multiply result with 'x'.
-        if (n & 1) {
-            return x * myPow(x * x, (n - 1) >> 1);
+        auto result = 1.0;
+        while (n != 0) {
+
+            // If 'n' is odd we multiply result with 'x' and reduce 'n' by '1'.
+            if (n & 1) {
+                result *= x;
+                n -= 1;
+            }
+
+            // We square 'x' and reduce 'n' by half, x^n => (x^2)^(n/2).
+            x *= x;
+            n >>= 1;
         }
-        // Otherwise we calculate result by performing Binary Exponentiation on 'n'.
-        else {
-            return myPow(x * x, n >> 1);
-        }
+
+        return result;
     }
 };
+
+
