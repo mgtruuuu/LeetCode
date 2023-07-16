5a28675a585f6118b084967ef60de0adc6d3c74f
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-07-16 12:39:14 +0900

    Time: 73 ms (21.56%), Space: 33.2 MB (13.64%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.cpp
+++ 1603-design-parking-system/1603-design-parking-system.cpp
@@ -1,62 +1,58 @@
 class ParkingSystem {
-public:
-    
-    // Parking limit for each type of car
-    int bigLimit, mediumLimit, smallLimit;
 
-    // Create an Array to store parked cars
-    int* parkingArray;
+  public:
+    const int m_max_big;
+    const int m_max_medium;
+    const int m_max_small;
+    int m_num_big = 0;
+    int m_num_medium = 0;
+    int m_num_small = 0;
 
-    ParkingSystem(int big, int medium, int small) {
-        
-        // Parking limit for each type of car
-        this->bigLimit = big;
-        this->mediumLimit = medium;
-        this->smallLimit = small;
-
-        // Create an Array to store parked cars. 
-        this->parkingArray = (int*)malloc((big + medium + small) * sizeof(int));
-        for (int i = 0; i < big + medium + small; i++) {
-            this->parkingArray[i] = -1;
-        }
+  public:
+    ParkingSystem(int big, int medium, int small) : m_max_big{ big }, m_max_medium{ medium }, m_max_small{ small }
+    {
     }
 
-    bool addCar(int carType) {
-
-        // Depending on carType, store the limit for the type of car
-        int limit = 0;
-        if (carType == 1) {
-            limit = this->bigLimit;
-        } else if (carType == 2) {
-            limit = this->mediumLimit;
-        } else {
-            limit = this->smallLimit;
-        }
-
-        // Traverse linearly through the array from the left
-        int count = 0;
-        for (int i = 0; i < this->bigLimit + this->mediumLimit + this->smallLimit; i++) {
-            // Count the number of cars parked in the system of that type
-            if (this->parkingArray[i] == carType) {
-                count++;
+    bool addCar(int carType)
+    {
+        switch (carType) {
+        case 1:
+            if (m_num_big < m_max_big) {
+                ++m_num_big;
+                return true;
             }
-
-            // Stop if the count becomes equal to the limit
-            if (count == limit) {
+            else {
                 return false;
             }
 
-            // If the count is less than the limit, then add the car
-            // to the first available empty slot from the left
-            if (this->parkingArray[i] == -1) {
-                this->parkingArray[i] = carType;
+        case 2:
+            if (m_num_medium < m_max_medium) {
+                ++m_num_medium;
                 return true;
             }
-        }
+            else {
+                return false;
+            }
+            break;
 
-        // If no empty slot is found, then return False.
-        // However, this line will never be executed if count < limit
-        // because slot will be found before count becomes equal to limit
-        return false;
+        case 3:
+            if (m_num_small < m_max_small) {
+                ++m_num_small;
+                return true;
+            }
+            else {
+                return false;
+            }
+            break;
+
+        default:
+            return false;
+        }
     }
 };
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
