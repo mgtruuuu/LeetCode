7bbffd5c93999a909fe3acd62ecbc220e44e8750
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-07-17 21:44:37 +0900

    Time: 169 ms (45.54%), Space: 60.2 MB (12.33%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.cpp
+++ 1396-design-underground-system/1396-design-underground-system.cpp
@@ -1,26 +1,43 @@
 class UndergroundSystem {
-public:
-    unordered_map<int, pair<string, int>> checkInMap; // Uid - {StationName, Time}
-    unordered_map<string, pair<double, int>> routeMap; // RouteName - {TotalTime, Count}
-    
-    UndergroundSystem() {}
-    
-    void checkIn(int id, string stationName, int t) {
-        checkInMap[id] = {stationName, t};
+
+  private:
+    std::unordered_map<int, std::pair<std::string, int>> id_2_start_info; // id, startStation, startTime
+    std::unordered_map<std::string, std::pair<int, int>> station_2_avg;   // station, sum, count
+
+  public:
+    UndergroundSystem()
+    {
     }
-    
-    void checkOut(int id, string stationName, int t) {
-        auto checkIn = checkInMap[id];
-        checkInMap.erase(id); // Remove after using it which will not make HashTable big
-            
-        string routeName = checkIn.first + "_" + stationName;
-        routeMap[routeName].first += t - checkIn.second;
-        routeMap[routeName].second += 1;
+
+    void checkIn(int id, string stationName, int t)
+    {
+        id_2_start_info[id] = { stationName, t };
     }
-    
-    double getAverageTime(string startStation, string endStation) {
-        string routeName = startStation + "_" + endStation;
-        auto& route = routeMap[routeName];
-        return route.first / route.second;
+
+    void checkOut(int id, string stationName, int t)
+    {
+        const auto station_combined = id_2_start_info[id].first + "_" + stationName;
+
+        if (station_2_avg.find(station_combined) == station_2_avg.end()) {
+            station_2_avg[station_combined] = { t - id_2_start_info[id].second, 1 };
+        }
+        else {
+            station_2_avg[station_combined].first += t - id_2_start_info[id].second;
+            ++station_2_avg[station_combined].second;
+        }
+    }
+
+    double getAverageTime(string startStation, string endStation)
+    {
+        return static_cast<double>(station_2_avg[(startStation + "_" + endStation)].first) /
+               station_2_avg[(startStation + "_" + endStation)].second;
     }
 };
+
+/**
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * UndergroundSystem* obj = new UndergroundSystem();
+ * obj->checkIn(id,stationName,t);
+ * obj->checkOut(id,stationName,t);
+ * double param_3 = obj->getAverageTime(startStation,endStation);
+ */
