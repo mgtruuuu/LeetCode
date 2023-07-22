d4fd01702e759e059fa6974a009fc4eff1eedf17
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-07-22 15:10:10 +0900

    Time: 12 ms (42.99%), Space: 18.7 MB (71.69%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -11,7 +11,6 @@
  */
 
 
-
 class Solution {
   public:
     int maxDepth(TreeNode* root)
@@ -20,6 +19,26 @@ class Solution {
             return 0;
         }
 
-        return std::max(maxDepth(root->left) + 1, maxDepth(root->right) + 1);
+        std::vector<std::pair<int, TreeNode*>> my_stack;
+        my_stack.push_back(std::pair<int, TreeNode*>(1, root));
+        auto max_depth = 0;
+        while (my_stack.empty() == false) {
+            std::pair<int, TreeNode*> my_pair = my_stack.back();
+            const auto c_depth = get<0>(my_pair);
+            const auto* c_node = get<1>(my_pair);
+            if (max_depth < c_depth) {
+                max_depth = c_depth;
+            }
+            my_stack.pop_back();
+
+            if (c_node->left != nullptr) {
+                my_stack.push_back(pair<int, TreeNode*>(c_depth + 1, c_node->left));
+            }
+            if (c_node->right != nullptr) {
+                my_stack.push_back(pair<int, TreeNode*>(c_depth + 1, c_node->right));
+            }
+        }
+
+        return max_depth;
     }
 };
