ced65c254fb2ac7d17e13dedfb04cf3cff0e6b15
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-07-24 09:38:12 +0900

    Time: 4 ms (35.47%), Space: 9.8 MB (13.21%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -9,22 +9,61 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
 
+
+/*
+// Approach 1: Recursive
+class Solution {
   public:
-    TreeNode* invertTree(TreeNode* root) {
-        
+    TreeNode* invertTree(TreeNode* root)
+    {
         if (root == nullptr) {
             return root;
         }
-        
+
         auto* temp = root->left;
         root->left = root->right;
         root->right = temp;
-        
+
         invertTree(root->left);
         invertTree(root->right);
-        
+
+        return root;
+    }
+};
+*/
+
+
+// Approach 2: Iterative
+class Solution {
+
+  public:
+    TreeNode* invertTree(TreeNode* root)
+    {
+        if (root == nullptr) {
+            return root;
+        }
+
+        std::queue<TreeNode*> q;
+        q.push(root);
+
+        while (q.empty() == false) {
+            auto* curr = q.front();
+            q.pop();
+
+            auto* temp = curr->left;
+            curr->left = curr->right;
+            curr->right = temp;
+
+            if (curr->left != nullptr) {
+                q.push(curr->left);
+            }
+
+            if (curr->right != nullptr) {
+                q.push(curr->right);
+            }
+        }
+
         return root;
     }
 };
