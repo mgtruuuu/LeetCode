0b2a64ffdf1fc2461a7b7723eb6c86f37ec5a0b3
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-07-25 13:03:53 +0900

    Time: 155 ms (50.00%), Space: 60.6 MB (48.84%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.cpp
+++ 1472-design-browser-history/1472-design-browser-history.cpp
@@ -1,3 +1,4 @@
+/*
 // Approach 1: Two Stacks
 class BrowserHistory {
   private:
@@ -43,30 +44,82 @@ class BrowserHistory {
         return curr;
     }
 };
+*/
 
 
 
-/*
+class DLLNode {
+  public:
+    std::string m_data;
+    DLLNode* m_prev;
+    DLLNode* m_next;
+
+    DLLNode(string url) : m_data{ std::move(url) }, m_prev{ nullptr }, m_next{ nullptr }
+    {
+    }
+};
+
 // Approach 2: Doubly Linked List
 class BrowserHistory {
+
+  private:
+    DLLNode* linkedListHead;
+    DLLNode* curr;
+
   public:
     BrowserHistory(string homepage)
     {
+        linkedListHead = new DLLNode{ homepage };
+        curr = linkedListHead;
     }
 
     void visit(string url)
     {
+        if (curr->m_next != nullptr) {
+
+            auto* trash = curr->m_next;
+
+            do {
+                auto* next = trash->m_next;
+                delete trash;
+                trash = next;
+                
+            } while (trash != nullptr);
+        }
+
+        curr->m_next = new DLLNode{ url };
+        curr->m_next->m_prev = curr;
+        curr = curr->m_next;
     }
 
     string back(int steps)
     {
+        while (steps-- != 0) {
+
+            if (curr->m_prev == nullptr) {
+                break;
+            }
+
+            curr = curr->m_prev;
+        }
+
+        return curr->m_data;
     }
 
     string forward(int steps)
     {
+        while (steps-- != 0) {
+
+            if (curr->m_next == nullptr) {
+                break;
+            }
+
+            curr = curr->m_next;
+        }
+
+        return curr->m_data;
     }
 };
-*/
 
 
 
