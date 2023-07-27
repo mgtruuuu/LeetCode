384ed9adc86129086de7f3c5ed07c68309cddbc1
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-07-27 09:11:36 +0900

    Time: 32 ms (63.16%), Space: 17.5 MB (48.17%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -10,24 +10,41 @@ class Solution {
 
         for (; idx_g != static_cast<int>(g.size()); ++idx_g) {
 
-            ++idx_s;
-
-            if (idx_s == static_cast<int>(s.size())) {
-                return idx_g;
-            }
-
-            while (s[idx_s] < g[idx_g]) {
-
+            do {
                 ++idx_s;
-
                 if (idx_s == static_cast<int>(s.size())) {
                     return idx_g;
                 }
-            }
 
-            
+            } while (s[idx_s] < g[idx_g]);
         }
 
         return idx_g;
     }
 };
+
+
+
+/*
+class Solution {
+  public:
+    int findContentChildren(vector<int>& g, vector<int>& s)
+    {
+        std::sort(g.begin(), g.end());
+        std::sort(s.begin(), s.end());
+
+        auto idx_g = 0;
+        auto idx_s = 0;
+        while (idx_g < static_cast<int>(g.size()) && idx_s < static_cast<int>(s.size())) {
+
+            if (g[idx_g] <= s[idx_s]) {
+                ++idx_g;
+            }
+
+            ++idx_s;
+        }
+
+        return static_cast<int>(idx_g);
+    }
+};
+*/
