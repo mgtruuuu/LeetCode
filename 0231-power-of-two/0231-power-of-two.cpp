01425a39302dbed6e1defee2b097a2c6419a0ea6
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-07-29 23:19:23 +0900

    Time: 4 ms (17.80%), Space: 5.8 MB (79.06%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    bool isPowerOfTwo(int n)
+    {
+        if (n <= 0) {
+            return false;
+        }
+        
+        uint32_t unsigned_n = n;
+        
+        auto cnt = 0;
+        
+        while (n != 0) {
+            if (n & 1) {
+                ++cnt;
+            }
+            
+            n >>= 1;
+        }
+        
+        if (cnt == 1) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
