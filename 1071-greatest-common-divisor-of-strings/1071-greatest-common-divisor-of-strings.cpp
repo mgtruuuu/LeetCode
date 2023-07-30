40c778c48424352a1d7a89b86f7de22e4e7bf89d
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-07-30 16:50:49 +0900

    Time: 0 ms (100.00%), Space: 7 MB (82.39%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -2,14 +2,33 @@ class Solution {
   public:
     string gcdOfStrings(string str1, string str2)
     {
+        return ((str1 + str2) == (str2 + str1)) ? 
+               str1.substr(0, std::gcd(str1.size(), str2.size())) :
+               std::string{};
+    }
+};
+
+
+/*
+class Solution {
+  public:
+    string gcdOfStrings(string str1, string str2)
+    {
         
         // Check if they have non-zero GCD string.
+        // If there exists chunk P containing more than one element
+        // such that str1 = PP...P and str2 = PP...P,
+        // then str1 + str2 = str2 + str1;
+        // AND VICE VERSA !!!
+        // (gcd(a, b) == gcd(a-b, b))
         if (str1 + str2 != str2 + str1) {
             return "";
         }
 
         // Get the GCD of the two lengths.
         const auto gcd_length = std::gcd(str1.size(), str2.size());
+        
         return str1.substr(0, gcd_length);
     }
 };
+*/
