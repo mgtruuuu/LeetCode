77484a33ddfe33a963c6e0d929e27f47723dae41
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-08-01 21:26:32 +0900

    Time: 23 ms (94.75%), Space: 28.4 MB (52.18%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
@@ -1,19 +1,23 @@
 class Solution {
-public:
+  public:
     vector<int> addToArrayForm(vector<int>& num, int k) {
-        int N = num.size();
-        int cur = k;
+        
+        const auto len_num = static_cast<int>(num.size());
+        auto cur = k;
         vector<int> ans;
 
-        int i = N;
+        auto i = len_num;
         while (--i >= 0 || cur > 0) {
-            if (i >= 0)
+            if (i >= 0) {
                 cur += num[i];
+            }
+
             ans.push_back(cur % 10);
             cur /= 10;
         }
 
         reverse(ans.begin(), ans.end());
+        
         return ans;
     }
 };
