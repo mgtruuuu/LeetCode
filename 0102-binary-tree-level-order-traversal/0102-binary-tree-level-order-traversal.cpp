dbab42ee71b1157e4d2e282d68b696d16fc764f2
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-08-08 15:31:11 +0900

    Time: 3 ms (93.08%), Space: 13.8 MB (12.27%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -18,6 +18,43 @@ class Solution {
     {
         std::vector<std::vector<int>> res;
 
+        std::queue<TreeNode*> q;
+        q.push(root);
+
+        while (q.empty() == false) {
+
+            std::vector<int> vals;
+
+            const auto len_q = q.size();
+            for (auto idx = std::size_t(0); idx != len_q; ++idx) {
+
+                const auto* const curr = q.front();
+                q.pop();
+
+                if (curr != nullptr) {
+                    vals.push_back(curr->val);
+                    q.push(curr->left);
+                    q.push(curr->right);
+                }
+            }
+
+            res.push_back(vals);
+        }
+
+        res.erase(res.end() - 1);
+        return res;
+    }
+};
+
+
+/*
+// Approach 2-2: Iteration
+class Solution {
+  public:
+    vector<vector<int>> levelOrder(TreeNode* root)
+    {
+        std::vector<std::vector<int>> res;
+
         std::queue<TreeNode*> queue_prev;
         queue_prev.push(root);
 
@@ -48,6 +85,7 @@ class Solution {
         return res;
     }
 };
+*/
 
 
 /*
