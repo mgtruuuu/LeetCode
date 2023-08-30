80cc2d36bd5b0905b6caa598666a5af609e9bfd5
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-08-30 11:47:32 +0900

    Time: 4 ms (88.51%), Space: 9.4 MB (51.72%) - LeetHub

--- 1279-traffic-light-controlled-intersection/1279-traffic-light-controlled-intersection.cpp
+++ 1279-traffic-light-controlled-intersection/1279-traffic-light-controlled-intersection.cpp
@@ -1,20 +1,40 @@
-class TrafficLight 
-{
-    public:
-    mutex m;
-    int light=1; //which light is currently on
-    TrafficLight() 
-    {}
+class TrafficLight {
+    
+  private:
+    std::mutex mMutex;
+    bool mIsGreenOnRoadA = true;
+    
+  public:
+    TrafficLight() {
+        
+    }
 
-    void carArrived(int carId,int roadId,int direction,function<void()> turnGreen,function<void()> crossCar) 
+    void carArrived(
+        int carId,                   // ID of the car
+        int roadId,                  // ID of the road the car travels on. Can be 1 (road A) or 2 (road B)
+        int direction,               // Direction of the car
+        function<void()> turnGreen,  // Use turnGreen() to turn light to green on current road
+        function<void()> crossCar    // Use crossCar() to make car cross the intersection
+    )
     {
-        m.lock();
-        if(light!=roadId) //if the light is not green on the current road, we should switch the light
-		{
-			turnGreen();
-			light=roadId;
-		}
-        crossCar();
-        m.unlock();
+        //printf("carId: %d\n", carId);
+            
+        const auto isRoadIdOne = (roadId == 1);
+
+        //printf("isRoadIdOne: %d\n", isRoadIdOne);
+        
+        {
+            std::lock_guard<std::mutex> lk(mMutex);
+            
+            if (mIsGreenOnRoadA != isRoadIdOne) {
+                
+                //printf("inside IF clause\n");
+                mIsGreenOnRoadA = !mIsGreenOnRoadA;
+                turnGreen();
+            }
+            
+            crossCar();
+            //printf("crossCar() called\n");
+        }
     }
 };
