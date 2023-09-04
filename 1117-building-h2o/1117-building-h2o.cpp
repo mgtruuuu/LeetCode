482d0d483b3d78ca5567bffd72664371b2e9d830
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-09-04 15:29:52 +0900

    Time: 12 ms (37.42%), Space: 8.8 MB (81.29%) - LeetHub

--- 1117-building-h2o/1117-building-h2o.cpp
+++ 1117-building-h2o/1117-building-h2o.cpp
@@ -1,32 +1,65 @@
 class H2O {
-    int d; // H - 2 * O
-    std::mutex mtx;
-    std::condition_variable cv;
-    
-public:
-    H2O(): d(0) {
-        
+  private:
+    std::condition_variable mCV;
+    std::mutex mMutex;
+    int mCountH;
+    int mCountO;
+    int mReadyH;
+    int mReadyO;
+
+    void clearGroup()
+    {
+        if (2 <= mReadyH && 1 <= mReadyO) {
+            mReadyH -= 2;
+            mReadyO -= 1;
+        }
     }
 
-    void hydrogen(function<void()> releaseHydrogen) {
-        unique_lock<std::mutex> lck(mtx);
-        while (d >= 2) {
-            cv.wait(lck);
-        }
-        // releaseHydrogen() outputs "H". Do not change or remove this line.
-        releaseHydrogen();
-        d++;
-        cv.notify_all();
+  public:
+    H2O() : mCountH{ 0 }, mCountO{ 0 }, mReadyH{ 0 }, mReadyO{ 0 }
+    {
     }
 
-    void oxygen(function<void()> releaseOxygen) {
-        unique_lock<std::mutex> lck(mtx);
-        while (d < 0) {
-            cv.wait(lck);
+    void hydrogen(function<void()> releaseHydrogen)
+    {
+        {
+            std::unique_lock<std::mutex> lk(mMutex);
+
+            // mCV.wait(lk, [this] { return mCountH == 2; });
+            while (2 * mCountO + 1 < mCountH) {
+                mCV.wait(lk);
+            }
+
+            ++mCountH;
+
+            // releaseHydrogen() outputs "H". Do not change or remove this line.
+            releaseHydrogen();
+            ++mReadyH;
+
+            clearGroup();
         }
-        // releaseOxygen() outputs "O". Do not change or remove this line.
-        releaseOxygen();
-        d-=2;
-        cv.notify_all();
+
+        mCV.notify_all();
+    }
+
+    void oxygen(function<void()> releaseOxygen)
+    {
+        {
+            std::unique_lock<std::mutex> lk(mMutex);
+
+            while (0 < 2 * mCountO - mCountH) {
+                mCV.wait(lk);
+            }
+
+            ++mCountO;
+
+            // releaseOxygen() outputs "O". Do not change or remove this line.
+            releaseOxygen();
+            ++mReadyO;
+
+            clearGroup();
+        }
+
+        mCV.notify_all();
     }
 };
