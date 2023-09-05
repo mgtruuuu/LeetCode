70eac5876ea5acf07d9ce67051824b83de6887f1
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-09-05 23:55:42 +0900

    Time: 236 ms (19.94%), Space: 49.1 MB (21.78%) - LeetHub

--- 1242-web-crawler-multithreaded/1242-web-crawler-multithreaded.cpp
+++ 1242-web-crawler-multithreaded/1242-web-crawler-multithreaded.cpp
@@ -1,80 +1,118 @@
+/**
+ * // This is the HtmlParser's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class HtmlParser {
+ *   public:
+ *     vector<string> getUrls(string url);
+ * };
+ */
 class Solution {
-public:
-    vector<string> crawl(string startUrl, HtmlParser htmlParser) {
+  private:
+    std::mutex mMutex;
+    std::queue<std::string> mQueue;
+    std::unordered_set<std::string> mHashSet;
+
+    std::string getHostname(std::string_view url) const
+    {
+        const auto idx_slash = url.find('/', 7);
         
-        string hostname = getHostname(startUrl);
-        vector<string> result;
-        unordered_set<string> visited;
-        queue<string> toVisit;
-        mutex mtx;
-        condition_variable cv;
-        int working = 0;
-
-        // Add the startUrl to the queue and mark it as visited
-        toVisit.push(startUrl);
-        visited.insert(startUrl);
-
-        // Define the worker thread function
-        auto worker = [&]() {
-            while (true) {
-                string url;
-                {
-                    // Lock the mutex before accessing shared data
-                    unique_lock<mutex> lock(mtx);
-                    // Wait until there is a URL to visit or all threads are done working
-                    cv.wait(lock, [&]() { return !toVisit.empty() || working == 0; });
-                    // If there are no URLs to visit, exit the thread
-                    if (toVisit.empty()) {
-                        return;
-                    }
-                    // Get the next URL to visit and remove it from the queue
-                    url = toVisit.front();
-                    toVisit.pop();
-                    // Increment the working counter
-                    ++working;
-                }
-                // Get the URLs on the page
-                vector<string> urls = htmlParser.getUrls(url);
-                {
-                    // Lock the mutex before accessing shared data
-                    lock_guard<mutex> lock(mtx);
-                    // For each URL on the page, if it has not been visited and it has the same hostname, add it to the queue and mark it as visited
-                    for (const string& url : urls) {
-                        if (visited.find(url) == visited.end() && getHostname(url) == hostname) {
-                            visited.insert(url);
-                            toVisit.push(url);
-                        }
-                    }
-                    // Decrement the working counter
-                    --working;
-                }
-                // Notify other threads that a URL has been processed
-                cv.notify_all();
-            }
-        };
-
-        // Create a number of worker threads equal to the number of cores on the machine
-        vector<thread> workers;
-        for (int i = 0; i < thread::hardware_concurrency(); ++i) {
-            workers.emplace_back(worker);
+        if (idx_slash == std::string_view::npos) {
+            return std::string{ url }.substr(7);    
         }
-        // Wait for all worker threads to finish
-        for (thread& worker : workers) {
-            worker.join();
-        }
-
-        // Return the visited URLs
-        return vector<string>(visited.begin(), visited.end());
+        
+        return std::string{ url }.substr(7, idx_slash - 7);
     }
 
-private:
-    // Helper function to extract the hostname from a URL
-    string getHostname(const string& url) {
-        size_t pos = string("http://").size();
-        pos = url.find('/', pos);
-        if (pos == string::npos) {
-            return url;
+    void getUrls(HtmlParser& htmlParser, const string& hostname, const std::string urlParent)
+    {
+        const auto urls = htmlParser.getUrls(urlParent);
+        
+        std::cout << "urls length : " << urls.size() << std::endl;
+        for (const auto url : urls) {
+
+            std::cout << "\turl: " << url << std::endl;
+            std::cout << "\thostname : " << hostname << std::endl;
+            
+            if (getHostname(url) != hostname) {
+                continue;
+            }
+
+            const std::lock_guard<std::mutex> lk(mMutex);
+
+            if (mHashSet.find(url) != mHashSet.end()) {
+                continue;
+            }
+
+            //mHashSet.insert(url);
+            mQueue.push(url);
         }
-        return url.substr(0, pos);
+    }
+
+    void checkBreath(HtmlParser& htmlParser, const string& hostname, std::size_t n)
+    {
+        std::vector<std::thread> threads;
+        threads.reserve(n);
+
+        std::cout << "checkBreath() called --- n : " << n << std::endl;        
+        
+        while (n-- != 0) {
+
+            const auto url = mQueue.front();
+            mQueue.pop();
+
+            std::cout << "url : " << url << std::endl;
+
+            
+            if (getHostname(url) != hostname) {
+                continue;
+            }
+            
+            {
+                const std::lock_guard<std::mutex> lk(mMutex);
+
+                if (mHashSet.find(url) != mHashSet.end()) {
+                    continue;
+                }
+                
+                mHashSet.insert(url);
+                // mQueue.push(url);
+                
+                
+                std::cout << "thread() will be calling" << std::endl;
+                threads.push_back(std::thread{ &Solution::getUrls, this, std::ref(htmlParser), std::cref(hostname), url });
+            }
+            
+
+        }
+        
+        std::cout << "size of threads:" << threads.size() << std::endl;
+        for (auto& thread : threads) {
+            thread.join();
+        }
+        std::cout << "thread through" << std::endl;
+        std::cout << "length of mQueue : " << mQueue.size() << std::endl;
+    }
+
+  public:
+    vector<string> crawl(string startUrl, HtmlParser htmlParser)
+    {
+        const auto hostname = getHostname(startUrl);
+        
+        //mHashSet.insert(startUrl);
+        mQueue.push(startUrl);
+        
+        while (mQueue.empty() == false) {
+
+            std::cout << "====checkBreath() will be calling" << std::endl;
+            checkBreath(htmlParser, hostname, mQueue.size());
+        }
+
+        std::vector<std::string> res;
+
+        for (auto itr = mHashSet.begin(); itr != mHashSet.end(); ++itr) {
+            res.push_back(*itr);
+        }
+
+        return res;
     }
 };
