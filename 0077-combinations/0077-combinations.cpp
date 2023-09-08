dd57b5ec6408934d09fe43ecf7bd8fe054765d8c
MUNGAP Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-09-08 12:40:03 +0900

    Time: 94 ms (26.61%), Space: 59.7 MB (20.48%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,22 +1,20 @@
 class Solution {
   private:
-    void backtrack(const int n, const int k, const int digit, std::vector<int>& combination,
+    void backtrack(const int n, const int k, const int first_num, std::vector<int>& combination,
                    std::vector<std::vector<int>>& combinations)
     {
-        if (digit == k) {
+        if (static_cast<int>(combination.size()) == k) {
 
             combinations.push_back(combination);
 
             return;
         }
 
-        const auto iStart = combination.empty() ? 1 : combination.back() + 1;
-        const auto iEnd = n - k + 1 + digit;
-
-        for (auto i = iStart; i <= iEnd; ++i) {
+        const auto iEnd = n - k + 1 + static_cast<int>(combination.size());
+        for (auto i = first_num; i <= iEnd; ++i) {
 
             combination.push_back(i);
-            backtrack(n, k, digit + 1, combination, combinations);
+            backtrack(n, k, i + 1, combination, combinations);
             combination.pop_back();
         }
     }
@@ -27,7 +25,7 @@ class Solution {
         std::vector<std::vector<int>> res;
         std::vector<int> combination;
 
-        backtrack(n, k, 0, combination, res);
+        backtrack(n, k, 1, combination, res);
 
         return res;
     }
