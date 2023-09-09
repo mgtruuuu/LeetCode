453a6e9e7e270e142c3fcc2e342da96b37855b7b
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-09-09 14:42:14 +0900

    Time: 28 ms (92.65%), Space: 27.7 MB (62.56%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -2,31 +2,40 @@ class Solution {
   public:
     int maximumProduct(std::vector<int>& nums)
     {
-        int min1 = std::numeric_limits<int>::max();
-        int min2 = std::numeric_limits<int>::max();
-        int max1 = std::numeric_limits<int>::min();
-        int max2 = std::numeric_limits<int>::min();
-        int max3 = std::numeric_limits<int>::min();
-        
-        for (int n : nums) {
-            if (n <= min1) {
+        auto min1 = std::numeric_limits<int>::max();
+        auto min2 = std::numeric_limits<int>::max();
+        auto max1 = std::numeric_limits<int>::min();
+        auto max2 = std::numeric_limits<int>::min();
+        auto max3 = std::numeric_limits<int>::min();
+
+        for (const auto num : nums) {
+
+            if (num <= min1) {
+
                 min2 = min1;
-                min1 = n;
-            } else if (n <= min2) {
-                min2 = n;
+                min1 = num;
             }
-            if (n >= max1) {
+            else if (num <= min2) {
+
+                min2 = num;
+            }
+
+            if (num >= max1) {
+
                 max3 = max2;
                 max2 = max1;
-                max1 = n;
-            } else if (n >= max2) {
+                max1 = num;
+            }
+            else if (num >= max2) {
+
                 max3 = max2;
-                max2 = n;
-            } else if (n >= max3) {
-                max3 = n;
+                max2 = num;
+            }
+            else if (num >= max3) {
+                max3 = num;
             }
         }
-        
+
         return std::max(min1 * min2 * max1, max1 * max2 * max3);
     }
 };
