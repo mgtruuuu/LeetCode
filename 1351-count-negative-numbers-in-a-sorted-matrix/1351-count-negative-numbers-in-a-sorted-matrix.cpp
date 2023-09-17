4a15a2d83599446556c2f83437282ce17a43c629
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-09-17 14:06:04 +0900

    Time: 15 ms (37.34%), Space: 11 MB (5.56%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
@@ -2,13 +2,29 @@ class Solution {
   public:
     int countNegatives(vector<vector<int>>& grid)
     {
+        const auto len_row = static_cast<int>(grid.size());
+        auto len_col = static_cast<int>(grid.front().size());
+
         auto count = 0;
-        const auto len_col = static_cast<int>(grid.front().size());
 
-        for (const auto& row : grid) {
+        auto idx_row = 0;
+        while (true) {
 
-            count += static_cast<int>(
-                std::distance(std::upper_bound(row.begin(), row.end(), 0, std::greater<int>()), row.end()));
+            if (idx_row == len_row) {
+                break;
+            }
+
+            const auto upper_bound_zero =
+                std::upper_bound(grid[idx_row].begin(), grid[idx_row].begin() + len_col, 0, std::greater<int>());
+
+            const auto idx_start_neg =
+                static_cast<int>(std::distance(upper_bound_zero, grid[idx_row].begin() + len_col));
+
+            count += idx_start_neg * (len_row - idx_row);
+
+            len_col -= idx_start_neg;
+
+            ++idx_row;
         }
 
         return count;
