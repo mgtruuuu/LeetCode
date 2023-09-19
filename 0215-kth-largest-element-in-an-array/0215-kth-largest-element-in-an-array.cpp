a32fcc4d919f268b019ff486852f4066ab7a62d3
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-09-19 12:50:49 +0900

    Time: 104 ms (23.19%), Space: 59.6 MB (8.27%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,40 +1,34 @@
 class Solution {
+  private:
+    template <typename U>
+    int findKthLargestHelper(const std::vector<int>& nums, U comp, const int k)
+    {
+        const auto len_nums = static_cast<int>(nums.size());
+
+        std::priority_queue<int, std::vector<int>, U> pq(comp);
+
+        for (auto n = 0; n != len_nums; ++n) {
+
+            pq.push(nums[n]);
+
+            if (len_nums < n + k) {
+                pq.pop();
+            }
+        }
+
+        return pq.top();
+    }
+
   public:
     int findKthLargest(vector<int>& nums, int k)
     {
         const auto len_nums = static_cast<int>(nums.size());
 
         if (k <= len_nums + 1 - k) {
-
-            std::priority_queue<int> pq;
-
-            for (auto n = 0; n != len_nums; ++n) {
-
-                pq.push(nums[n]);
-                
-                if (len_nums < n + k) {
-                    pq.pop();
-                }
-            }
-
-            return pq.top();
+            return findKthLargestHelper(nums, std::less<int>(), k);
         }
         else {
-
-            k = len_nums + 1 - k;
-
-            std::priority_queue<int, std::vector<int>, std::greater<int>> pq;
-
-            for (auto n = 0; n != len_nums; ++n) {
-
-                pq.push(nums[n]);
-                
-                if (len_nums < n + k) {
-                    pq.pop();
-                }
-            }
-
-            return pq.top();
+            return findKthLargestHelper(nums, std::greater<int>(), len_nums + 1 - k);
         }
     }
 };
