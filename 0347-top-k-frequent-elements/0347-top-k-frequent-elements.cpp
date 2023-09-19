d5777b001a86f41d55d735561b8de4cc04ff3680
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-09-19 17:59:16 +0900

    Time: 8 ms (90.28%), Space: 14.1 MB (18.07%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -2,31 +2,32 @@ class Solution {
   public:
     vector<int> topKFrequent(vector<int>& nums, int k)
     {
-        if (k == static_cast<int>(nums.size())) {
-            return nums;
-        }
-
         std::unordered_map<int, int> num2count;
+
         for (const auto num : nums) {
             ++num2count[num];
         }
 
-        const auto comp = [&num2count](const int k1, const int k2) { return num2count[k1] < num2count[k2]; };
+        std::vector<int> res;
+        res.reserve(k);
 
-        std::vector<int> keys;
-        keys.reserve(num2count.size());
+        std::priority_queue<std::pair<int, int>> pq;
+
+        const auto len_nums = static_cast<int>(num2count.size());
+        auto count = 0;
 
         for (const auto& [key_num, value_count] : num2count) {
-            keys.push_back(key_num);
+
+            pq.push(std::make_pair(value_count, key_num));
+
+            if (len_nums < k + count++) {
+
+                res.push_back(pq.top().second);
+                pq.pop();
+            }
         }
 
-        for (auto i = 0; i != k; ++i) {
-
-            std::make_heap(keys.begin(), keys.end() - i, comp);
-
-            std::pop_heap(keys.begin(), keys.end() - i);
-        }
-
-        return std::vector(keys.end() - k, keys.end());
+        res.push_back(pq.top().second);
+        return res;
     }
 };
