74d448a33ae04dc3ace5c20dea4e15a9c15b0cf2
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-09-21 16:41:27 +0900

    Time: 15 ms (61.12%), Space: 12.2 MB (58.13%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -1,3 +1,5 @@
+/*
+// Approach 1: Priority Queues
 class Solution {
   public:
     int minMeetingRooms(vector<vector<int>>& intervals)
@@ -23,3 +25,44 @@ class Solution {
         return len_ends;
     }
 };
+*/
+
+
+///*
+// Approach 2: Chronological Ordering
+class Solution {
+  public:
+    int minMeetingRooms(vector<vector<int>>& intervals)
+    {
+        std::vector<int> interval_starts;
+        interval_starts.reserve(intervals.size());
+        std::vector<int> interval_ends;
+        interval_ends.reserve(intervals.size());
+
+        for (const auto& interval : intervals) {
+            interval_starts.push_back(interval.front());
+            interval_ends.push_back(interval.back());
+        }
+
+        std::sort(interval_starts.begin(), interval_starts.end());
+        std::sort(interval_ends.begin(), interval_ends.end());
+
+        auto count = 0;
+        auto p_start = interval_starts.begin();
+        auto p_end = interval_ends.begin();
+
+        while (p_start != interval_starts.end()) {
+
+            if (*p_start >= *p_end) {
+                ++p_end;
+                --count;
+            }
+
+            ++p_start;
+            ++count;
+        }
+
+        return count;
+    }
+};
+//*/
