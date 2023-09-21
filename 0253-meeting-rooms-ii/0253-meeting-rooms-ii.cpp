9701b70a9c1d49a312399fb7f365bc5c412808f1
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-09-21 15:42:25 +0900

    Time: 8 ms (96.01%), Space: 12 MB (69.58%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -2,27 +2,24 @@ class Solution {
   public:
     int minMeetingRooms(vector<vector<int>>& intervals)
     {
-        std::sort(intervals.begin(), intervals.end());
-        
-        std::priority_queue<int, std::vector<int>, std::greater<int>> ends;
-        auto count = 0;
-        auto max = count;
+        std::sort(intervals.begin(), intervals.end(),
+                  [](const std::vector<int>& a, const std::vector<int>& b) { return (a.front() < b.front()); });
 
+        std::priority_queue<int, std::vector<int>, std::greater<int>> ends;
+        auto len_ends = 0;
         for (const auto& interval : intervals) {
 
-            if (ends.empty() == true || interval.front() < ends.top()) {
-                count += 1;
-                if (max < count) {
-                    max = count;
-                }
-            }
-            else {
+            while (ends.empty() == false && ends.top() <= interval.front()) {
                 ends.pop();
             }
 
             ends.push(interval.back());
+
+            if (len_ends < static_cast<int>(ends.size())) {
+                len_ends = static_cast<int>(ends.size());
+            }
         }
 
-        return max;
+        return len_ends;
     }
 };
