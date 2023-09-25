b33d558a9b053c48f0f0f69bb4734834c61c2976
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-09-25 20:03:16 +0900

    Time: 85 ms (42.05%), Space: 54 MB (29.02%) - LeetHub

---
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -0,0 +1,50 @@
+class Solution {
+  public:
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders)
+    {
+        using my_t = unsigned long long;
+        
+        auto sum_diff_ladders = my_t(0);
+        auto sum_diff_bricks = my_t(0);
+        std::priority_queue<int, std::vector<int>, std::greater<int>> pq_diff_ladders;
+
+        auto idx = 1;
+        for (; idx != static_cast<int>(heights.size()); ++idx) {
+
+            if (heights[idx] <= heights[idx - 1]) {
+                continue;
+            }
+
+            const auto diff = heights[idx] - heights[idx - 1];
+
+            pq_diff_ladders.push(diff);
+
+            if (pq_diff_ladders.size() > std::size_t(ladders)) {
+
+                sum_diff_ladders += my_t(diff - pq_diff_ladders.top());
+                sum_diff_bricks += pq_diff_ladders.top();
+                pq_diff_ladders.pop();
+            }
+            else {
+                sum_diff_ladders += my_t(diff);
+            }
+
+            if (bricks != 0) {
+                if (bricks < sum_diff_bricks) {
+                    return idx - 1;
+                }
+                else if (bricks == sum_diff_bricks) {
+                    continue;
+                }
+            }
+            else if (pq_diff_ladders.empty() == true) {
+                return idx - 1;
+            }
+            else if (pq_diff_ladders.size() == std::size_t(ladders)) {
+                return idx;
+            }
+        }
+
+        return idx - 1; // idx == heights.size()
+    }
+};
