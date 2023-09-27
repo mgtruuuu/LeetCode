1e4c4bb9b78746dc290cba9da9cbac2a948d3fc2
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-09-27 09:34:57 +0900

    Time: 18 ms (40.51%), Space: 16.9 MB (12.50%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -2,6 +2,30 @@ class Solution {
   public:
     bool stoneGame(vector<int>& piles)
     {
-        return true;
-    }   
+        const auto len_piles = piles.size();
+
+        std::vector<std::vector<int>> scores(len_piles, std::vector<int>(len_piles));
+
+        // case : diff == 0
+        auto diff = std::size_t(0);
+        for (auto r = std::size_t(0), c = r + diff; c != len_piles; ++r, ++c) {
+            scores[r][c] = -piles[r];
+        }
+        ++diff;
+
+        for (; diff != len_piles; ++diff) {
+            for (auto r = std::size_t(0), c = r + diff; c != len_piles; ++r, ++c) {
+
+                // diff is odd
+                if (diff & 1) {
+                    scores[r][c] = std::max(piles[r] + scores[r + 1][c], piles[c] + scores[r][c - 1]);
+                }
+                else {
+                    scores[r][c] = std::min(-piles[r] + scores[r + 1][c], -piles[c] + scores[r][c - 1]);
+                }
+            }
+        }
+
+        return scores[0][len_piles - 1];
+    }
 };
