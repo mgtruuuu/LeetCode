5a265308f3476589c927e5c98fdfe0af38146701
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-09-28 13:04:11 +0900

    Time: 110 ms (23.40%), Space: 10.7 MB (53.77%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -3,17 +3,23 @@ class Solution {
     int stoneGameII(vector<int>& piles)
     {
         const auto len_piles = piles.size();
+        constexpr auto zero = std::size_t(0);
         constexpr auto one = std::size_t(1);
 
         std::array<std::vector<std::vector<int>>, 2> scores;
-        for (auto& i : scores) {
-            i = std::vector<std::vector<int>>(len_piles + 1, std::vector<int>(len_piles, 0));
+        for (auto& player : scores) {
+
+            player.resize(len_piles);
+
+            for (auto j = zero; j != player.size(); ++j) {
+                player[j].resize(len_piles - j, 0);
+            }
         }
 
         auto rc = len_piles - 1;
         {
             auto sum_piles = 0;
-            for (auto c = std::size_t(0); c != len_piles; ++c) {
+            for (auto c = zero; c != len_piles; ++c) {
 
                 const auto r = rc - c;
 
@@ -24,24 +30,38 @@ class Solution {
         }
 
         for (; rc-- != 0;) {
-            for (auto c = std::size_t(0); c <= rc; ++c) {
+            for (auto c = zero; c <= rc; ++c) {
 
                 const auto r = rc - c;
 
                 auto value_max = std::numeric_limits<int>::min();
                 auto value_min = std::numeric_limits<int>::max();
                 auto sum_max = 0;
-                for (auto x = one; x <= std::size_t(2) * (c + one) && x <= len_piles - r; ++x) {
+                {
+                    auto x = one;
+                    for (; x <= std::size_t(2) * (c + one) && x < len_piles - r; ++x) {
 
-                    sum_max += piles[r + x - one];
+                        sum_max += piles[r + x - one];
 
-                    const auto c_next = std::min(std::max(c + one, x), len_piles) - one;
-                    if (value_max < sum_max + scores[1][r + x][std::min(c_next, len_piles - r - x - 1)]) {
-                        value_max = sum_max + scores[1][r + x][std::min(c_next, len_piles - r - x - 1)];
+                        const auto c_next = std::min(std::max(c + one, x), len_piles) - one;
+
+                        if (value_max < sum_max + scores[1][r + x][std::min(c_next, len_piles - r - x - 1)]) {
+                            value_max = sum_max + scores[1][r + x][std::min(c_next, len_piles - r - x - 1)];
+                        }
+
+                        if (value_min > scores[0][r + x][std::min(c_next, len_piles - r - x - 1)]) {
+                            value_min = scores[0][r + x][std::min(c_next, len_piles - r - x - 1)];
+                        }
                     }
 
-                    if (value_min > scores[0][r + x][std::min(c_next, len_piles - r - x - 1)]) {
-                        value_min = scores[0][r + x][std::min(c_next, len_piles - r - x - 1)];
+                    if (x <= std::size_t(2) * (c + one)) {
+
+                        sum_max += piles[r + x - one];
+
+                        if (value_max < sum_max) {
+                            value_max = sum_max;
+                        }
+                        value_min = 0;
                     }
                 }
 
