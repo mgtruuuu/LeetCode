61e0122b28c657ab343cace258c21e09c5d4ef54
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-09-30 13:49:13 +0900

    Time: 41 ms (44.78%), Space: 11 MB (53.23%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -2,74 +2,45 @@ class Solution {
   public:
     int stoneGameII(vector<int>& piles)
     {
-        const auto len_piles = piles.size();
-        constexpr auto zero = std::size_t(0);
-        constexpr auto one = std::size_t(1);
+        const auto len_piles = static_cast<int>(piles.size());
 
-        std::array<std::vector<std::vector<int>>, 2> scores;
-        for (auto& player : scores) {
-
-            player.resize(len_piles);
-
-            for (auto j = zero; j != player.size(); ++j) {
-                player[j].resize(len_piles - j, 0);
+        std::vector dp(2, std::vector<std::vector<int>>(len_piles));
+        for (auto& player : dp) {
+            for (auto i = 0; i != len_piles; ++i) {
+                player[i] = std::vector<int>(len_piles + 1 - i, -1);
             }
         }
 
-        auto rc = len_piles - 1;
-        {
-            auto sum_piles = 0;
-            for (auto c = zero; c != len_piles; ++c) {
-
-                const auto r = rc - c;
-
-                sum_piles += piles[len_piles - c - one];
-                scores[0][r][c] = sum_piles;
-                scores[1][r][c] = 0;
+        std::function<int(int, int, int)> fcn = [&](int p, int i, int m) -> int {
+            
+            if (i == len_piles) {
+                return 0;
             }
-        }
+            
+            if (i + m > len_piles) {
+                m = len_piles - i;
+            }
+            
+            if (dp[p][i][m] != -1) {
+                return dp[p][i][m];
+            }
 
-        for (; rc-- != 0;) {
-            for (auto c = zero; c <= rc; ++c) {
+            auto res = p == 1 ? std::numeric_limits<int>::max() : -1;
+            auto sum = 0;
+            for (int x = 1; x <= min(2 * m, len_piles - i); ++x) {
 
-                const auto r = rc - c;
-
-                auto value_max = std::numeric_limits<int>::min();
-                auto value_min = std::numeric_limits<int>::max();
-                auto sum_max = 0;
-                {
-                    auto x = one;
-                    for (; x <= std::size_t(2) * (c + one) && x < len_piles - r; ++x) {
-
-                        sum_max += piles[r + x - one];
-
-                        const auto c_next = std::min(std::max(c + one, x), len_piles) - one;
-
-                        if (value_max < sum_max + scores[1][r + x][std::min(c_next, len_piles - r - x - 1)]) {
-                            value_max = sum_max + scores[1][r + x][std::min(c_next, len_piles - r - x - 1)];
-                        }
-
-                        if (value_min > scores[0][r + x][std::min(c_next, len_piles - r - x - 1)]) {
-                            value_min = scores[0][r + x][std::min(c_next, len_piles - r - x - 1)];
-                        }
-                    }
-
-                    if (x <= std::size_t(2) * (c + one)) {
-
-                        sum_max += piles[r + x - one];
-
-                        if (value_max < sum_max) {
-                            value_max = sum_max;
-                        }
-                        value_min = 0;
-                    }
+                sum += piles[i + x - 1];
+                if (p == 0) {
+                    res = std::max(res, sum + fcn(1, i + x, max(m, x)));
                 }
-
-                scores[0][r][c] = value_max;
-                scores[1][r][c] = value_min;
+                else {
+                    res = std::min(res, fcn(0, i + x, max(m, x)));
+                }
             }
-        }
 
-        return scores[0][0][0];
+            return (dp[p][i][m] = res);
+        };
+
+        return fcn(0, 0, 1);
     }
 };
