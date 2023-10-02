ded5e5101a9673dacf92551335f9ab334fe5ab48
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-02 23:02:39 +0900

    Time: 342 ms (6.73%), Space: 128.2 MB (5.75%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,3 +1,31 @@
+/*
+// Approach 2: Insertion Sort <------------ Time Limit Exceeded
+class MedianFinder {
+  private:
+    std::vector<int> m_store; // resize-able container
+
+  public:
+    MedianFinder()
+    {
+    }
+
+    // Adds a number into the data structure.
+    void addNum(int num)
+    {
+        m_store.insert(lower_bound(m_store.begin(), m_store.end(), num), num); // binary search and insertion combined
+    }
+
+    double findMedian()
+    {
+        const auto len = m_store.size();
+        return len & 1 ? m_store[len / 2] : (static_cast<double>(m_store[(len / 2) - 1]) + m_store[len / 2]) * 0.5;
+    }
+};
+*/
+
+
+/*
+// Approach 3: Two Heaps
 class MedianFinder {
   private:
     std::priority_queue<int> m_pq_low;                                       // 5 1 2 3 4
@@ -27,6 +55,58 @@ class MedianFinder {
                                                    : static_cast<double>(m_pq_low.top());
     }
 };
+*/
+
+
+class MedianFinder {
+    multiset<int> data;
+    multiset<int>::iterator lo_median, hi_median;
+
+public:
+    MedianFinder()
+        : lo_median(data.end())
+        , hi_median(data.end())
+    {
+    }
+
+    void addNum(int num)
+    {
+        const size_t n = data.size();   // store previous size
+
+        data.insert(num);               // insert into multiset
+
+        if (!n) {
+            // no elements before, one element now
+            lo_median = hi_median = data.begin();
+        }
+        else if (n & 1) {
+            // odd size before (i.e. lo == hi), even size now (i.e. hi = lo + 1)
+
+            if (num < *lo_median)       // num < lo
+                lo_median--;
+            else                        // num >= hi
+                hi_median++;            // insertion at end of equal range
+        }
+        else {
+            // even size before (i.e. hi = lo + 1), odd size now (i.e. lo == hi)
+
+            if (num > *lo_median && num < *hi_median) {
+                lo_median++;                    // num in between lo and hi
+                hi_median--;
+            }
+            else if (num >= *hi_median)         // num inserted after hi
+                lo_median++;
+            else                                // num <= lo < hi
+                lo_median = --hi_median;        // insertion at end of equal range spoils lo
+        }
+    }
+
+    double findMedian()
+    {
+        return ((double) *lo_median + *hi_median) * 0.5;
+    }
+};
+
 
 /**
  * Your MedianFinder object will be instantiated and called as such:
