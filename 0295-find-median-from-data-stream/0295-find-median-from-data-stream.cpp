37eebc2b83cba937bcf60e1c6a7ae452f14fe2af
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-02 15:01:50 +0900

    Time: 317 ms (12.95%), Space: 117.3 MB (34.30%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,25 +1,36 @@
 class MedianFinder {
-    priority_queue<int> lo;                              // max heap
-    priority_queue<int, vector<int>, greater<int>> hi;   // min heap
+  private:
+    std::priority_queue<int> m_pq_low;                                       // 5 1 2 3 4
+    std::priority_queue<int, std::vector<int>, std::greater<int>> m_pq_high; // 7 10 8 14 21
 
-public:
-    // Adds a number into the data structure.
+  public:
+    MedianFinder()
+    {
+    }
+
     void addNum(int num)
     {
-        lo.push(num);                                    // Add to max heap
+        m_pq_low.push(num);
+        m_pq_high.push(m_pq_low.top());
+        m_pq_low.pop();
 
-        hi.push(lo.top());                               // balancing step
-        lo.pop();
+        if (m_pq_low.size() < m_pq_high.size()) {
 
-        if (lo.size() < hi.size()) {                     // maintain size property
-            lo.push(hi.top());
-            hi.pop();
+            m_pq_low.push(m_pq_high.top());
+            m_pq_high.pop();
         }
     }
 
-    // Returns the median of current data stream
     double findMedian()
     {
-        return lo.size() > hi.size() ? lo.top() : ((double) lo.top() + hi.top()) * 0.5;
+        return m_pq_low.size() == m_pq_high.size() ? (m_pq_low.top() + m_pq_high.top()) / 2.0
+                                                   : static_cast<double>(m_pq_low.top());
     }
 };
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
