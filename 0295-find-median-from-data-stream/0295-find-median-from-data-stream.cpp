514200b0a7128688318643be9d05e0299a2e2f45
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-03 22:18:10 +0900

    Time: 348 ms (6.32%), Space: 128.2 MB (5.79%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -59,51 +59,55 @@ class MedianFinder {
 
 
 class MedianFinder {
-    multiset<int> data;
-    multiset<int>::iterator lo_median, hi_median;
+    std::multiset<int> m_data;
+    std::multiset<int>::iterator lo_median;
+    std::multiset<int>::iterator hi_median;
 
-public:
+  public:
     MedianFinder()
-        : lo_median(data.end())
-        , hi_median(data.end())
     {
     }
 
     void addNum(int num)
     {
-        const size_t n = data.size();   // store previous size
+        const auto len = m_data.size(); // store previous size
 
-        data.insert(num);               // insert into multiset
+        m_data.insert(num); // insert into multiset
 
-        if (!n) {
-            // no elements before, one element now
-            lo_median = hi_median = data.begin();
-        }
-        else if (n & 1) {
-            // odd size before (i.e. lo == hi), even size now (i.e. hi = lo + 1)
+        if (len & 1) {
 
-            if (num < *lo_median)       // num < lo
+            if (num < *lo_median) {
                 lo_median--;
-            else                        // num >= hi
-                hi_median++;            // insertion at end of equal range
+            }
+            else {
+                hi_median++; // insertion at end of equal range
+            }
         }
-        else {
-            // even size before (i.e. hi = lo + 1), odd size now (i.e. lo == hi)
+        else if (len != 0) {
 
+            // even size before (i.e. hi = lo + 1), odd size now (i.e. lo == hi)
             if (num > *lo_median && num < *hi_median) {
-                lo_median++;                    // num in between lo and hi
+                lo_median++; // num in between lo and hi
                 hi_median--;
             }
-            else if (num >= *hi_median)         // num inserted after hi
+            // num inserted after hi
+            else if (num >= *hi_median) {
                 lo_median++;
-            else                                // num <= lo < hi
-                lo_median = --hi_median;        // insertion at end of equal range spoils lo
+            }
+            // num <= lo < hi
+            else {
+                lo_median = --hi_median; // insertion at end of equal range spoils lo
+            }
+        }
+        else {
+            // no elements before, one element now
+            lo_median = hi_median = m_data.begin();
         }
     }
 
     double findMedian()
     {
-        return ((double) *lo_median + *hi_median) * 0.5;
+        return (static_cast<double>(*lo_median) + *hi_median) * 0.5;
     }
 };
 
