0057768ceba3930fb699115e10337ac588248e6f
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-04 14:27:58 +0900

    Time: 27 ms (74.84%), Space: 14 MB (38.68%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -102,7 +102,7 @@ class Solution {
 };
 */
 
-///*
+/*
 // Approach #2: BFS
 class Solution {
   public:
@@ -154,4 +154,115 @@ class Solution {
         return num_islands;
     }
 };
-//*/
+*/
+
+
+
+class UnionFind {
+  private:
+    std::vector<int> m_parents;
+    std::vector<int> m_ranks;
+    int m_num_islands;
+
+  public:
+    UnionFind(const int len) : m_num_islands{ len }
+    {
+        m_parents.resize(len);
+        m_ranks.resize(len);
+        for (auto i = 0; i != len; ++i) {
+            m_parents[i] = i;
+            m_ranks[i] = 1;
+        }
+    }
+
+    int find(const int x)
+    {
+        if (x == m_parents[x]) {
+            return x;
+        }
+
+        return (m_parents[x] = find(m_parents[x]));
+    }
+
+    void unionSet(const int x, const int y)
+    {
+        const auto root_x = find(x);
+        const auto root_y = find(y);
+
+        if (root_x == root_y) {
+            return;
+        }
+
+        if (m_ranks[root_x] < m_ranks[root_y]) {
+            m_parents[root_x] = root_y;
+        }
+        else if (m_ranks[root_x] > m_ranks[root_y]) {
+            m_parents[root_y] = root_x;
+        }
+        else {
+            m_parents[root_x] = root_y;
+            ++m_ranks[root_y];
+        }
+
+        --m_num_islands;
+    }
+
+    bool isConnected(const int x, const int y)
+    {
+        return find(x) == find(y);
+    }
+
+    int getNumIslands() const
+    {
+        return m_num_islands;
+    }
+};
+
+class Solution {
+  private:
+    int get1DIndex(int r, int c, const int len_col)
+    {
+        return len_col * r + c;
+    }
+
+  public:
+    int numIslands(vector<vector<char>>& grid)
+    {
+        const auto len_row = static_cast<int>(grid.size());
+        const auto len_col = static_cast<int>(grid.front().size());
+        auto num_zeros = 0;
+        
+        UnionFind uf{ len_row * len_col };
+
+        for (auto r = 0; r != len_row; ++r) {
+            for (auto c = 0; c != len_col; ++c) {
+
+                if (grid[r][c] == '0') {
+                    
+                    ++num_zeros;
+                    continue;
+                }
+
+                const auto idx = get1DIndex(r, c, len_col);
+
+                if (0 < r && grid[r - 1][c] == '1') {
+                    uf.unionSet(idx, get1DIndex(r - 1, c, len_col));
+                }
+
+                if (r < len_row - 1 && grid[r + 1][c] == '1') {
+                    uf.unionSet(idx, get1DIndex(r + 1, c, len_col));
+                }
+
+                if (0 < c && grid[r][c - 1] == '1') {
+                    uf.unionSet(idx, get1DIndex(r, c - 1, len_col));
+                }
+
+                if (c < len_col - 1 && grid[r][c + 1] == '1') {
+                    uf.unionSet(idx, get1DIndex(r, c + 1, len_col));
+                }
+            }
+        }
+
+        return uf.getNumIslands() - num_zeros;
+    }
+};
