c6159115db80588afe2ba19b325107561a4424bd
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-04 11:55:53 +0900

    Time: 36 ms (25.32%), Space: 13 MB (54.42%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,70 +1,52 @@
-class UnionFind {
-public:
-  UnionFind(vector<vector<char>>& grid) {
-    count = 0;
-    int m = grid.size();
-    int n = grid[0].size();
-      for (int i = 0; i < m; ++i) {
-        for (int j = 0; j < n; ++j) {
-          if (grid[i][j] == '1') {
-            parent.push_back(i * n + j);
-            ++count;
-          }
-          else parent.push_back(-1);
-          rank.push_back(0);
-        }
-    }
-  }
-
-  int find(int i) { // path compression
-    if (parent[i] != i) parent[i] = find(parent[i]);
-    return parent[i];
-  }
-
-  void Union(int x, int y) { // union with rank
-    int rootx = find(x);
-    int rooty = find(y);
-    if (rootx != rooty) {
-      if (rank[rootx] > rank[rooty]) parent[rooty] = rootx;
-      else if (rank[rootx] < rank[rooty]) parent[rootx] = rooty;
-      else {
-        parent[rooty] = rootx; rank[rootx] += 1;
-      }
-      --count;
-    }
-  }
-
-  int getCount() const {
-    return count;
-  }
-
-private:
-  vector<int> parent;
-  vector<int> rank;
-  int count; // # of connected components
-};
-
 class Solution {
-public:
-  int numIslands(vector<vector<char>>& grid) {
-    int nr = grid.size();
-    if (!nr) return 0;
-    int nc = grid[0].size();
-
-    UnionFind uf (grid);
-    int num_islands = 0;
-    for (int r = 0; r < nr; ++r) {
-      for (int c = 0; c < nc; ++c) {
-        if (grid[r][c] == '1') {
-          grid[r][c] = '0';
-          if (r - 1 >= 0 && grid[r-1][c] == '1') uf.Union(r * nc + c, (r-1) * nc + c);
-          if (r + 1 < nr && grid[r+1][c] == '1') uf.Union(r * nc + c, (r+1) * nc + c);
-          if (c - 1 >= 0 && grid[r][c-1] == '1') uf.Union(r * nc + c, r * nc + c - 1);
-          if (c + 1 < nc && grid[r][c+1] == '1') uf.Union(r * nc + c, r * nc + c + 1);
+  private:
+    void dfs(const int r, const int c, const std::vector<std::vector<char>>& grid,
+             std::vector<std::vector<bool>>& visited)
+    {
+        if (grid[r][c] == '0' || visited[r][c] == true) {
+            return;
         }
-      }
+
+        visited[r][c] = true;
+
+        if (0 < r && visited[r - 1][c] == false) {
+            dfs(r - 1, c, grid, visited);
+        }
+
+        if (r < static_cast<int>(grid.size()) - 1 && visited[r + 1][c] == false) {
+            dfs(r + 1, c, grid, visited);
+        }
+
+        if (0 < c && visited[r][c - 1] == false) {
+            dfs(r, c - 1, grid, visited);
+        }
+
+        if (c < static_cast<int>(grid.front().size()) - 1 && visited[r][c + 1] == false) {
+            dfs(r, c + 1, grid, visited);
+        }
     }
 
-    return uf.getCount();
-  }
+  public:
+    int numIslands(vector<vector<char>>& grid)
+    {
+        const auto len_row = static_cast<int>(grid.size());
+        const auto len_col = static_cast<int>(grid.front().size());
+
+        std::vector<std::vector<bool>> visited(len_row, std::vector<bool>(len_col, false));
+
+        auto num_islands = 0;
+
+        for (auto r = 0; r != len_row; ++r) {
+            for (auto c = 0; c != len_col; ++c) {
+
+                if (grid[r][c] == '1' && visited[r][c] == false) {
+
+                    ++num_islands;
+                    dfs(r, c, grid, visited);
+                }
+            }
+        }
+
+        return num_islands;
+    }
 };
