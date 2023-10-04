07ff2f30eb6259d85e4b3387503b921d3d688704
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-04 12:03:30 +0900

    Time: 19 ms (98.76%), Space: 12.5 MB (73.05%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,3 +1,5 @@
+/*
+// Approach #1 - 1 : DFS
 class Solution {
   private:
     void dfs(const int r, const int c, const std::vector<std::vector<char>>& grid,
@@ -50,3 +52,59 @@ class Solution {
         return num_islands;
     }
 };
+*/
+
+
+///*
+// Approach #1 - 2 : DFS
+class Solution {
+  private:
+    void dfs(const int r, const int c, std::vector<std::vector<char>>& grid)
+    {
+        if (grid[r][c] == '0') {
+            return;
+        }
+
+        grid[r][c] = '0';
+
+        if (0 < r) {
+            dfs(r - 1, c, grid);
+        }
+
+        if (r < static_cast<int>(grid.size()) - 1) {
+            dfs(r + 1, c, grid);
+        }
+
+        if (0 < c) {
+            dfs(r, c - 1, grid);
+        }
+
+        if (c < static_cast<int>(grid.front().size()) - 1) {
+            dfs(r, c + 1, grid);
+        }
+    }
+
+  public:
+    int numIslands(vector<vector<char>>& grid)
+    {
+        const auto len_row = static_cast<int>(grid.size());
+        const auto len_col = static_cast<int>(grid.front().size());
+
+        auto num_islands = 0;
+
+        for (auto r = 0; r != len_row; ++r) {
+            for (auto c = 0; c != len_col; ++c) {
+
+                if (grid[r][c] == '1') {
+
+                    ++num_islands;
+                    dfs(r, c, grid);
+                }
+            }
+        }
+
+        return num_islands;
+    }
+};
+//*/
+
