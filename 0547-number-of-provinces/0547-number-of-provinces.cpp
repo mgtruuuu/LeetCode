273489997c41f02e02535ef0cdbdb513de68760e
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-04 11:14:05 +0900

    Time: 15 ms (96.36%), Space: 15.9 MB (60.07%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,3 +1,86 @@
+/*
+// Approach 1: Depth First Search
+class Solution {
+  public:
+    void dfs(int node, const std::vector<std::vector<int>>& isConnected, std::vector<bool>& visited)
+    {
+        if (visited[node] == true) {
+            return;
+        }
+
+        visited[node] = true;
+        for (auto i = 0; i < static_cast<int>(isConnected.size()); ++i) {
+            if (isConnected[node][i] == 1 && visited[i] == false) {
+                dfs(i, isConnected, visited);
+            }
+        }
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected)
+    {
+        const auto num_nodes = static_cast<int>(isConnected.size());
+        auto num_components = 0;
+        std::vector<bool> visited(num_nodes, false);
+
+        for (auto i = 0; i != num_nodes; ++i) {
+
+            if (visited[i] == false) {
+                ++num_components;
+                dfs(i, isConnected, visited);
+            }
+        }
+
+        return num_components;
+    }
+};
+*/
+
+
+class Solution {
+  public:
+    int findCircleNum(vector<vector<int>>& isConnected)
+    {
+        const auto num_nodes = static_cast<int>(isConnected.size());
+        auto num_components = 0;
+        vector<bool> visited(num_nodes, false);
+
+        std::queue<int> q;
+
+        for (auto i = 0; i != num_nodes; ++i) {
+
+            if (visited[i] == true) {
+                continue;
+            }
+
+            q.push(i);
+            visited[i] = true;
+
+            while (q.empty() == false) {
+
+                const auto curr = q.front();
+                q.pop();
+
+                for (auto j = 0; j != num_nodes; ++j) {
+
+                    if (isConnected[curr][j] == 1 && visited[j] == false) {
+
+                        q.push(j);
+                        visited[j] = true;
+                    }
+                }
+            }
+
+            ++num_components;
+        }
+
+        return num_components;
+    }
+};
+
+
+
+/*
+// Approach 3: Union-find
 class UnionFind {
   private:
     int m_len;
@@ -79,3 +162,4 @@ class Solution {
         return uf.getLength();
     }
 };
+*/
