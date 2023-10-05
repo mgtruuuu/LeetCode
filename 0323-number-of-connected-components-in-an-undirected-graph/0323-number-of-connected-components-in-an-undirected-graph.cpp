1b93c1ed8d129aa194a37ce5fc9e5e2afb86fb27
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-05 10:41:36 +0900

    Time: 19 ms (47.71%), Space: 12.3 MB (75.46%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -87,6 +87,7 @@ class Solution {
 */
 
 
+/*
 // Approach 2 : Iterative BFS
 class Solution {
   public:
@@ -129,7 +130,79 @@ class Solution {
         return num_components;
     }
 };
+*/
 
-// Approach 1: Depth-First Search (DFS)
 
-// Approach 2: Disjoint Set Union (DSU)
+// Approach 3: Disjoint Set Union (DSU)
+class UnionFind {
+  private:
+    std::vector<int> m_parents;
+    std::vector<int> m_ranks;
+    int m_num_components;
+
+  public:
+    UnionFind(const int len) : m_num_components{ len }
+    {
+        m_parents.resize(len);
+        m_ranks.resize(len);
+        for (auto i = 0; i != len; ++i) {
+            m_parents[i] = i;
+            m_ranks[i] = 1;
+        }
+    }
+
+    int find(const int x)
+    {
+        if (m_parents[x] == x) {
+            return x;
+        }
+
+        return (m_parents[x] = find(m_parents[x]));
+    }
+
+    void unionSet(const int x, const int y)
+    {
+        const auto root_x = find(x);
+        const auto root_y = find(y);
+
+        if (root_x == root_y) {
+            return;
+        }
+
+        if (m_ranks[root_x] < m_ranks[root_y]) {
+            m_parents[root_x] = root_y;
+        }
+        else if (m_ranks[root_x] > m_ranks[root_y]) {
+            m_parents[root_y] = root_x;
+        }
+        else {
+            m_parents[root_x] = root_y;
+            ++m_ranks[root_y];
+        }
+
+        --m_num_components;
+    }
+
+    bool isConnected(const int x, const int y)
+    {
+        return (find(x) == find(y));
+    }
+
+    int getNumComponents() const
+    {
+        return m_num_components;
+    }
+};
+
+class Solution {
+  public:
+    int countComponents(int n, vector<vector<int>>& edges)
+    {
+        UnionFind uf{ n };
+        for (const auto& edge : edges) {
+            uf.unionSet(edge.front(), edge.back());
+        }
+
+        return uf.getNumComponents();
+    }
+};
