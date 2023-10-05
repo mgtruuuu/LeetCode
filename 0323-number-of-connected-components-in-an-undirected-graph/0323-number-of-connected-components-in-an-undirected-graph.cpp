d75588daf7c05184f7ea00ad5c581c6f0e9efd17
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-05 10:16:53 +0900

    Time: 18 ms (55.26%), Space: 13.8 MB (45.73%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -1,5 +1,5 @@
 /*
-// Approach 1 - 1: Iterative DFS
+// Approach 1 - 1 : Iterative DFS
 class Solution {
   public:
     int countComponents(int n, vector<vector<int>>& edges)
@@ -44,6 +44,7 @@ class Solution {
 */
 
 
+/*
 // Approach 1 - 2 : Recursive DFS
 class Solution {
   private:
@@ -78,6 +79,51 @@ class Solution {
 
             ++num_components;
             bfs(adj_list, idx, visited);
+        }
+
+        return num_components;
+    }
+};
+*/
+
+
+// Approach 2 : Iterative BFS
+class Solution {
+  public:
+    int countComponents(int n, vector<vector<int>>& edges)
+    {
+        std::vector<std::vector<int>> adj_list(n);
+        for (const auto& edge : edges) {
+            adj_list[edge.front()].push_back(edge.back());
+            adj_list[edge.back()].push_back(edge.front());
+        }
+        std::vector<bool> visited(n, false);
+
+        auto num_components = 0;
+        std::queue<int> q;
+
+        for (auto idx = 0; idx != n; ++idx) {
+
+            if (visited[idx] == true) {
+                continue;
+            }
+
+            ++num_components;
+            q.push(idx);
+            visited[idx] = true;
+            while (q.empty() == false) {
+
+                const auto node = q.front();
+                q.pop();
+
+                for (const auto child : adj_list[node]) {
+
+                    if (visited[child] == false) {
+                        q.push(child);
+                        visited[child] = true;
+                    }
+                }
+            }
         }
 
         return num_components;
