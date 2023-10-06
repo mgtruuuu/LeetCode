041d43ff8f5690915e84a73bd7be02046013572c
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-06 12:19:32 +0900

    Time: 20 ms (18.05%), Space: 12.1 MB (6.88%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -14,6 +14,33 @@ public:
 };
 */
 
+
+// Approach 1: Recursive
+class Solution {
+  private:
+    std::unordered_map<Node*, Node*> hash_visited;
+
+  public:
+    Node* copyRandomList(Node* head)
+    {
+        if (head == nullptr) {
+            return nullptr;
+        }
+
+        Node* node = new Node{ head->val };
+        hash_visited[head] = node;
+        node->next = copyRandomList(head->next);
+
+        for (auto& [k, v] : hash_visited) {
+            v->random = (k->random == nullptr) ? nullptr : hash_visited[k->random];
+        }
+
+        return node;
+    }
+};
+
+/*
+// Approach 2: Iterative with O(N) Space
 class Solution {
   public:
     Node* copyRandomList(Node* head)
@@ -22,24 +49,25 @@ class Solution {
 
         Node* res = nullptr;
         Node** pp_dst = &res;
-        
+
         while (head != nullptr) {
-            
+
             *pp_dst = new Node{ head->val };
-            
-            hash_visited[head] = *pp_dst;    
-            
+
+            hash_visited[head] = *pp_dst;
+
             pp_dst = &((**pp_dst).next);
-            
+
             head = head->next;
         }
-        
+
         *pp_dst = nullptr;
-        
+
         for (auto& [k, v] : hash_visited) {
             v->random = (k->random == nullptr) ? nullptr : hash_visited[k->random];
         }
-        
+
         return res;
     }
 };
+*/
