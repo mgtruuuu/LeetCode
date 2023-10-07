498636997b61d04250c003a434474514b93d1365
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-07 15:12:21 +0900

    Time: 39 ms (97.83%), Space: 20 MB (83.39%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,111 @@
+///*
+// Approach 1: Breadth-first Search (BFS), Overwriting Input
+class Solution {
+  public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid)
+    {
+        const auto len_grid = static_cast<int>(grid.size());
+
+        if (grid[0][0] != 0 || grid[len_grid - 1][len_grid - 1] != 0) {
+            return -1;
+        }
+
+        std::queue<std::pair<int, int>> q;
+        q.emplace(0, 0);
+        grid[0][0] = 1;
+
+        while (q.empty() == false) {
+
+            const auto rc = q.front();
+            q.pop();
+
+            if (rc.first == len_grid - 1 && rc.second == len_grid - 1) {
+                return grid[rc.first][rc.second];
+            }
+
+            for (auto r = rc.first - 1; r <= rc.first + 1; ++r) {
+
+                if (r < 0 || r >= len_grid) {
+                    continue;
+                }
+
+                for (auto c = rc.second - 1; c <= rc.second + 1; ++c) {
+
+                    if (c < 0 || c >= len_grid) {
+                        continue;
+                    }
+
+                    if (grid[r][c] == 0) {
+
+                        q.emplace(r, c);
+                        grid[r][c] = grid[rc.first][rc.second] + 1;
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+};
+//*/
+
+
+/*
+// Approach 2: Breadth-first Search (Without Overwriting the Input)
+class Solution {
+  public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid)
+    {
+        const auto len_grid = static_cast<int>(grid.size());
+        
+        if (grid[0][0] != 0 || grid[len_grid - 1][len_grid - 1] != 0) {
+            return -1;
+        }
+
+        std::vector<std::vector<bool>> visited(len_grid, std::vector<bool>(len_grid, false));
+        
+        std::queue<std::pair<int, int>> q;
+        q.emplace(0, 0);
+        visited[0][0] = true;
+        auto len_path = 0;
+  
+        while (q.empty() == false) {
+
+            ++len_path;
+            auto len_q = q.size();
+
+            while (len_q-- != 0) {
+
+                const auto rc = q.front();
+                q.pop();
+                
+                if (rc.first == len_grid - 1 && rc.second == len_grid - 1) {
+                    return len_path;
+                }
+
+                for (auto r = rc.first - 1; r <= rc.first + 1; ++r) {
+                    
+                    if (r < 0 || r >= len_grid) {
+                        continue;
+                    }
+                    
+                    for (auto c = rc.second - 1; c <= rc.second + 1; ++c) {
+                     
+                        if (c < 0 || c >= len_grid) {
+                            continue;
+                        }
+                        
+                        if (grid[r][c] == 0 && visited[r][c] == false) {
+
+                            q.emplace(r, c);
+                            visited[r][c] = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+};
+*/
