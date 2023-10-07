1edaa05e0193447d7b7ac54fcc8c6f1b9835b1f6
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-07 12:07:39 +0900

    Time: 2 ms (42.64%), Space: 10.5 MB (10.26%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -9,8 +9,37 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
+/*
 class Solution {
-public:
+  public:
+    bool isSameTree(TreeNode* p, TreeNode* q)
+    {
+        if (p == nullptr) {
+            return (q == nullptr ? true : false);
+        }
+        else if (q == nullptr) {
+            return false;
+        }
+        // if (p == nullptr && q == nullptr) {
+        //     return true;
+        // }
+        // if (p == nullptr || q == nullptr) {
+        //     return false;
+        // }
+
+        if (p->val != q->val) {
+            return false;
+        }
+
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
+*/
+
+class Solution {
+  public:
     bool isSameTree(TreeNode* p, TreeNode* q)
     {
         if (p == nullptr) {
@@ -20,10 +49,45 @@ public:
             return false;
         }
 
-        if (p->val != q->val) {
-            return false;
+        std::stack<TreeNode*> s_p, s_q;
+
+        const auto lambda = [&](TreeNode* p, TreeNode* q) -> bool {
+            if (p == nullptr) {
+                if (q == nullptr) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+            else if (q == nullptr) {
+                return false;
+            }
+            else {
+                s_p.push(p);
+                s_q.push(q);
+                return true;
+            }
+        };
+
+        s_p.push(p);
+        s_q.push(q);
+        while (s_p.empty() == false && s_q.empty() == false) {
+
+            const auto* node_p = s_p.top();
+            s_p.pop();
+            const auto* node_q = s_q.top();
+            s_q.pop();
+
+            if (node_p->val != node_q->val) {
+                return false;
+            }
+
+            if (lambda(node_p->left, node_q->left) == false || lambda(node_p->right, node_q->right) == false) {
+                return false;
+            }
         }
 
-        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        return s_p.empty() && s_p.empty() ? true : false;
     }
 };
