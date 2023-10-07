ab35ca064ccaafd71d7a8cf30544badfdd7a1052
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-07 22:10:56 +0900

    Time: 22 ms (55.76%), Space: 13.5 MB (29.00%) - LeetHub

---
+++ 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.cpp
@@ -0,0 +1,81 @@
+class UnionFind {
+  private:
+    std::vector<int> m_parents;
+    std::vector<int> m_ranks;
+    int m_num_components;
+
+  public:
+    UnionFind(const int num_nodes) : m_num_components{ num_nodes }
+    {
+        m_parents.resize(num_nodes);
+        m_ranks.resize(num_nodes);
+
+        for (auto idx = 0; idx != num_nodes; ++idx) {
+            m_parents[idx] = idx;
+            m_ranks[idx] = 1;
+        }
+    }
+
+    int find(const int x)
+    {
+        if (x == m_parents[x]) {
+            return x;
+        }
+
+        return (m_parents[x] = find(m_parents[x]));
+    }
+    
+    void unionSet(const int x, const int y)
+    {
+        const auto root_x = find(x);
+        const auto root_y = find(y);
+
+        if (root_x == root_y) {
+            return;
+        }
+
+        if (m_ranks[root_x] < m_ranks[root_y]) {
+            m_parents[root_x] = root_y;
+        }
+        else if (m_ranks[root_x] > m_ranks[root_y]) {
+            m_parents[root_y] = root_x;
+        }
+        else {
+            m_parents[root_y] = root_x;
+            ++m_ranks[root_x];
+        }
+        
+        --m_num_components;
+    }
+
+    bool isAllConnected() const
+    {
+        return (m_num_components == 1);
+    }
+};
+
+class Solution {
+  public:
+    int earliestAcq(vector<vector<int>>& logs, int n)
+    {
+        if (static_cast<int>(logs.size()) < n - 1) {
+            return -1;
+        }
+        
+        UnionFind uf{ n };
+
+        std::sort(logs.begin(), logs.end(),
+                  [](const std::vector<int>& a, const std::vector<int>& b) { return (a.front() < b.front()); });
+
+        for (const auto& log : logs) {
+
+            uf.unionSet(log[1], log[2]);
+
+            if (uf.isAllConnected() == true) {
+                return log.front();
+            }
+        }
+
+        return -1;
+    }
+};
