92eec4a2eaecef4a28de31fbfa08085b241d90c8
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-08 17:34:34 +0900

    Time: 19 ms (62.94%), Space: 13.7 MB (46.27%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,4 +1,4 @@
-///*
+/*
 // Approach #1: Coloring by DFS
 class Solution {
   public:
@@ -44,7 +44,7 @@ class Solution {
         return true;
     }
 };
-//*/
+*/
 
 
 
@@ -97,3 +97,51 @@ class Solution {
     }
 };
 */
+
+constexpr auto color_a = std::numeric_limits<int>::min();
+constexpr auto color_b = std::numeric_limits<int>::max();
+
+class Solution {
+  private:
+    bool dfs(const int node, const std::vector<std::vector<int>>& graph, std::vector<int>& colors)
+    {
+        for (const auto neighbor : graph[node]) {
+
+            if (colors[node] == colors[neighbor]) {
+                return false;
+            }
+
+            if (colors[neighbor] != 0) {
+                continue;
+            }
+
+            colors[neighbor] = (color_a == colors[node] ? color_b : color_a);
+            if (dfs(neighbor, graph, colors) == false) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+  public:
+    bool isBipartite(vector<vector<int>>& graph)
+    {
+        const auto num_nodes = static_cast<int>(graph.size());
+        std::vector<int> colors(num_nodes, 0);
+
+        for (auto idx = 0; idx != num_nodes; ++idx) {
+
+            if (colors[idx] != 0) {
+                continue;
+            }
+
+            colors[idx] = color_a;
+            if (dfs(idx, graph, colors) == false) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
