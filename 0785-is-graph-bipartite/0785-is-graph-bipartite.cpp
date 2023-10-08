65b22318d2a104f07a752f2a04702001db94081a
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-08 17:36:57 +0900

    Time: 19 ms (62.94%), Space: 13.9 MB (29.54%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,5 +1,57 @@
+///*
+// Approach #1: Coloring by Recursive DFS
+constexpr auto color_a = std::numeric_limits<int>::min();
+constexpr auto color_b = std::numeric_limits<int>::max();
+
+class Solution {
+  private:
+    bool dfs(const int node, const std::vector<std::vector<int>>& graph, std::vector<int>& colors)
+    {
+        for (const auto neighbor : graph[node]) {
+
+            if (colors[node] == colors[neighbor]) {
+                return false;
+            }
+
+            if (colors[neighbor] != 0) {
+                continue;
+            }
+
+            colors[neighbor] = (color_a == colors[node] ? color_b : color_a);
+            if (dfs(neighbor, graph, colors) == false) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+  public:
+    bool isBipartite(vector<vector<int>>& graph)
+    {
+        const auto num_nodes = static_cast<int>(graph.size());
+        std::vector<int> colors(num_nodes, 0);
+
+        for (auto idx = 0; idx != num_nodes; ++idx) {
+
+            if (colors[idx] != 0) {
+                continue;
+            }
+
+            colors[idx] = color_a;
+            if (dfs(idx, graph, colors) == false) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+//*/
+
+
 /*
-// Approach #1: Coloring by DFS
+// Approach #2: Coloring by Iterative DFS
 class Solution {
   public:
     bool isBipartite(vector<vector<int>>& graph)
@@ -51,7 +103,7 @@ class Solution {
 
 
 /*
-// Approach #2: Coloring by BFS
+// Approach #3: Coloring by Iterative BFS
 class Solution {
   public:
     bool isBipartite(vector<vector<int>>& graph)
@@ -97,51 +149,3 @@ class Solution {
     }
 };
 */
-
-constexpr auto color_a = std::numeric_limits<int>::min();
-constexpr auto color_b = std::numeric_limits<int>::max();
-
-class Solution {
-  private:
-    bool dfs(const int node, const std::vector<std::vector<int>>& graph, std::vector<int>& colors)
-    {
-        for (const auto neighbor : graph[node]) {
-
-            if (colors[node] == colors[neighbor]) {
-                return false;
-            }
-
-            if (colors[neighbor] != 0) {
-                continue;
-            }
-
-            colors[neighbor] = (color_a == colors[node] ? color_b : color_a);
-            if (dfs(neighbor, graph, colors) == false) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-  public:
-    bool isBipartite(vector<vector<int>>& graph)
-    {
-        const auto num_nodes = static_cast<int>(graph.size());
-        std::vector<int> colors(num_nodes, 0);
-
-        for (auto idx = 0; idx != num_nodes; ++idx) {
-
-            if (colors[idx] != 0) {
-                continue;
-            }
-
-            colors[idx] = color_a;
-            if (dfs(idx, graph, colors) == false) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-};
