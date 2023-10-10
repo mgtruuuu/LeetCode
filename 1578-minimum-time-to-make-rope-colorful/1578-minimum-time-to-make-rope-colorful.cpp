5210c15be3465daf27d19dbf2cd864d32fd77082
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-10 15:56:20 +0900

    Time: 122 ms (45.15%), Space: 95.7 MB (34.44%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -4,6 +4,36 @@ class Solution {
     {
         const auto len = colors.size();
 
+        auto prev_color = 'A';
+        auto max = 0;
+        auto res = 0;
+        for (auto idx = std::size_t(0); idx != len; ++idx) {
+
+            if (prev_color != colors[idx]) {
+                prev_color = colors[idx];
+                max = neededTime[idx];
+            }
+            else if (max < neededTime[idx]) {
+                res += max;
+                max = neededTime[idx];
+            }
+            else {
+                res += neededTime[idx];
+            }
+        }
+
+        return res;
+    }
+};
+
+
+/*
+class Solution {
+  public:
+    int minCost(string colors, vector<int>& neededTime)
+    {
+        const auto len = colors.size();
+
         auto sum_cost = 0;
         auto idx_lhs = std::size_t(0);
 
@@ -34,3 +64,4 @@ class Solution {
         return sum_cost;
     }
 };
+*/
