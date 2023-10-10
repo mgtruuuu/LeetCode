7d15c6a9f92295da59bfeabfd80343d6b401b236
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-10 15:42:14 +0900

    Time: 112 ms (76.82%), Space: 95.7 MB (46.26%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,27 +1,36 @@
 class Solution {
-public:
-    int minCost(string colors, vector<int>& neededTime) {
-     
+  public:
+    int minCost(string colors, vector<int>& neededTime)
+    {
         const auto len = colors.size();
-        
-        auto prev_color = 'A';
-        auto max = 0;
-        auto res = 0;
-        for (auto idx = std::size_t(0); idx != len; ++idx) {
-            
-            if (prev_color != colors[idx]) {
-                prev_color = colors[idx];
-                max = neededTime[idx];
+
+        auto sum_cost = 0;
+        auto idx_lhs = std::size_t(0);
+
+        auto idx_rhs = std::size_t(1);
+        while (idx_rhs != colors.size()) {
+
+            auto cost_delete = 0;
+            auto cost_max = neededTime[idx_lhs];
+            while (idx_rhs != colors.size() && colors[idx_lhs] == colors[idx_rhs]) {
+
+                if (cost_max < neededTime[idx_rhs]) {
+                    cost_delete += cost_max;
+                    cost_max = neededTime[idx_rhs];
+                }
+                else {
+                    cost_delete += neededTime[idx_rhs];
+                }
+
+                if (++idx_rhs == len) {
+                    return sum_cost + cost_delete;
+                }
             }
-            else if (max < neededTime[idx]) {
-                res += max;
-                max = neededTime[idx];
-            }
-            else {
-                res += neededTime[idx];
-            }
+
+            idx_lhs = idx_rhs++;
+            sum_cost += cost_delete;
         }
-        
-        return res;
+
+        return sum_cost;
     }
 };
