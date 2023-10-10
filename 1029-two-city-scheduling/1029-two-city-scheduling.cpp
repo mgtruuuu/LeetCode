827c7d8a4a2ce4a37f4c5cc4ff929192af71b884
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-10 17:03:33 +0900

    Time: 0 ms (100.00%), Space: 8.1 MB (48.60%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,28 +1,18 @@
-using AMinusB = int;
-using B = int;
-using Pair = std::pair<AMinusB, B>;
-
 class Solution {
   public:
     int twoCitySchedCost(vector<vector<int>>& costs)
     {
-        const auto len = costs.size();
-
-        std::vector<Pair> pairs;
-        pairs.reserve(len);
-        for (const auto& cost : costs) {
-            pairs.emplace_back(cost.front() - cost.back(), cost.back());
-        }
-
-        std::sort(pairs.begin(), pairs.end());
+        std::sort(costs.begin(), costs.end(), [](const std::vector<int>& a, const std::vector<int>& b) {
+            return (a.front() - a.back() < b.front() - b.back());
+        });
 
         auto min_cost = 0;
-        auto idx = 0;
-        for (; idx != len * 0.5; ++idx) {
-            min_cost += pairs[idx].first + pairs[idx].second;
-        }
-        for (; idx != len; ++idx) {
-            min_cost += pairs[idx].second;
+
+        const auto len = costs.size();
+        const auto len_half = std::size_t(costs.size() * 0.5);
+
+        for (auto idx = std::size_t(0); idx != len_half; ++idx) {
+            min_cost += costs[idx].front() + costs[idx + len_half].back();
         }
 
         return min_cost;
