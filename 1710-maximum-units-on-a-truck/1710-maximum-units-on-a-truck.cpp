5df453904b7b7d400f9119937c90864aae342da3
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-10 10:11:37 +0900

    Time: 56 ms (26.70%), Space: 20.6 MB (20.49%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
   public:
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize)
@@ -13,6 +14,45 @@ class Solution {
             num_unit += box_count * box_type.back();
             truckSize -= box_count;
             
+            if (truckSize == 0) {
+                return num_unit;
+            };
+        }
+
+        return num_unit;
+    }
+};
+*/
+
+class Solution {
+  private:
+    struct Comparator {
+        bool operator()(const std::vector<int>& p1, const std::vector<int>& p2)
+        {
+            return p1[1] < p2[1];
+        }
+    };
+
+  public:
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize)
+    {
+        std::priority_queue<std::vector<int>, std::vector<std::vector<int>>, Comparator> pq;
+        for (const auto& box_type : boxTypes) {
+            pq.push(box_type);
+        }
+
+        auto num_unit = 0;
+
+        while (pq.empty() == false) {
+
+            const auto box_type = pq.top();
+            pq.pop();
+
+            const auto box_count = std::min(box_type.front(), truckSize);
+
+            num_unit += box_count * box_type.back();
+            truckSize -= box_count;
+
             if (truckSize == 0) {
                 return num_unit;
             };
