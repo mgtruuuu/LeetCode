f81d9c4fb8461b6bb53b73c22b6d06dc10928443
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-10 09:53:30 +0900

    Time: 64 ms (25.23%), Space: 16.2 MB (62.70%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -6,16 +6,16 @@ class Solution {
                   [](const std::vector<int>& a, const std::vector<int>& b) { return a.back() > b.back(); });
 
         auto num_unit = 0;
-        auto count = 0;
         for (const auto& box_type : boxTypes) {
-            if (box_type.front() >= truckSize - count) {
-                num_unit += (truckSize - count) * box_type.back();
-
-                break;
+            
+            auto num_box = box_type.front();
+            while (num_box-- != 0) {
+                
+                num_unit += box_type.back();
+                if (--truckSize == 0) {
+                    return num_unit;
+                };
             }
-
-            num_unit += box_type.front() * box_type.back();
-            count += box_type.front();
         }
 
         return num_unit;
