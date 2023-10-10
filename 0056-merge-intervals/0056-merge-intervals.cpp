560bee6b90cf06062c3c2fe0741865af878213d9
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-10 19:42:15 +0900

    Time: 34 ms (32.67%), Space: 19.2 MB (56.57%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -2,26 +2,29 @@ class Solution {
   public:
     vector<vector<int>> merge(vector<vector<int>>& intervals)
     {
-        std::sort(intervals.begin(), intervals.end(), [](const std::vector<int>& a, const std::vector<int>& b) {
-            return a.front() != b.front() ? (a.front() < b.front()) : (a.back() < b.back());
-        });
+        std::sort(intervals.begin(), intervals.end());
 
         std::vector<std::vector<int>> res;
-        auto prev_left = intervals.front().front();
-        auto prev_right = intervals.front().back();
+        res.reserve(intervals.size());
+
+        auto prev_start = intervals[0].front();
+        auto prev_end = intervals[0].back();
+
         for (auto idx = std::size_t(1); idx != intervals.size(); ++idx) {
 
-            if (prev_right < intervals[idx].front()) {
-                res.push_back(std::vector<int>{ prev_left, prev_right });
-                prev_left = intervals[idx].front();
-                prev_right = intervals[idx].back();
+            if (prev_end < intervals[idx].front()) {
+
+                res.push_back({ prev_start, prev_end });
+
+                prev_start = intervals[idx].front();
+                prev_end = intervals[idx].back();
             }
-            else if (prev_right < intervals[idx].back()) {
-                prev_right = intervals[idx].back();
+            else if (prev_end < intervals[idx].back()) {
+                prev_end = intervals[idx].back();
             }
         }
 
-        res.push_back(std::vector<int>{ prev_left, prev_right });
+        res.push_back({ prev_start, prev_end });
 
         return res;
     }
