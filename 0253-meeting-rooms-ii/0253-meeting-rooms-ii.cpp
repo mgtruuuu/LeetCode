989e0634f50c63420a0e5c20b11d96fe572a9245
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-10 17:31:42 +0900

    Time: 11 ms (88.65%), Space: 11.9 MB (81.86%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -1,68 +1,23 @@
-///*
-// Approach 1: Priority Queues
 class Solution {
   public:
     int minMeetingRooms(vector<vector<int>>& intervals)
     {
-        std::sort(intervals.begin(), intervals.end(),
-                  [](const std::vector<int>& a, const std::vector<int>& b) { return (a.front() < b.front()); });
+        std::sort(intervals.begin(), intervals.end());
 
-        std::priority_queue<int, std::vector<int>, std::greater<int>> ends;
-        auto len_ends = 0;
+        auto res = std::size_t(0); // the minimum number of conference rooms required
+        std::priority_queue<int, std::vector<int>, std::greater<int>> pq;
         for (const auto& interval : intervals) {
 
-            while (ends.empty() == false && ends.top() <= interval.front()) {
-                ends.pop();
+            while (pq.empty() == false && pq.top() <= interval.front()) {
+                pq.pop();
             }
 
-            ends.push(interval.back());
-
-            if (len_ends < static_cast<int>(ends.size())) {
-                len_ends = static_cast<int>(ends.size());
+            pq.push(interval.back());
+            if (res < pq.size()) {
+                res = pq.size();
             }
         }
 
-        return len_ends;
+        return res;
     }
 };
-//*/
-
-
-/*
-// Approach 2: Chronological Ordering
-class Solution {
-  public:
-    int minMeetingRooms(vector<vector<int>>& intervals)
-    {
-        std::vector<int> interval_starts;
-        interval_starts.reserve(intervals.size());
-        std::vector<int> interval_ends;
-        interval_ends.reserve(intervals.size());
-
-        for (const auto& interval : intervals) {
-            interval_starts.push_back(interval.front());
-            interval_ends.push_back(interval.back());
-        }
-
-        std::sort(interval_starts.begin(), interval_starts.end());
-        std::sort(interval_ends.begin(), interval_ends.end());
-
-        auto count = 0;
-        auto p_start = interval_starts.begin();
-        auto p_end = interval_ends.begin();
-
-        while (p_start != interval_starts.end()) {
-
-            if (*p_start >= *p_end) {
-                ++p_end;
-                --count;
-            }
-
-            ++p_start;
-            ++count;
-        }
-
-        return count;
-    }
-};
-*/
