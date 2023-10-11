db0c15bc3abef83310360251b36dfcda7b762c36
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-11 17:09:09 +0900

    Time: 75 ms (87.99%), Space: 68.1 MB (25.76%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,3 +1,40 @@
+// Approach 1: Optimized Brute Force
+
+
+///*
+// Approach 2 - 1 : Dynamic Programming, Kadane's Algorithm
+class Solution {
+  public:
+    int maxSubArray(std::vector<int>& nums)
+    {
+        // Initialize our variables using the first element.
+        auto current_subarray = nums[0];
+        auto max_subarray = nums[0];
+
+        // Start with the 2nd element since we already used the first one.
+        for (auto idx = std::size_t(1); idx != static_cast<int>(nums.size()); ++idx) {
+
+            
+            if (current_subarray < 0) {
+                current_subarray = nums[idx];
+            }
+            else {
+                current_subarray += nums[idx];
+            }
+            
+            if (max_subarray < current_subarray) {
+                max_subarray = current_subarray;
+            }
+        }
+
+        return max_subarray;
+    }
+};
+//*/
+
+
+/*
+// Approach 2 - 2 : Dynamic Programming, Kadane's Algorithm <------ My solution
 class Solution {
   public:
     int maxSubArray(vector<int>& nums)
@@ -24,3 +61,4 @@ class Solution {
         return res;
     }
 };
+*/
