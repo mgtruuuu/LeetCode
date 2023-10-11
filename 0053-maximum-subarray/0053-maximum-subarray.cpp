ad014bf982118cf03dceb86d7e7f8bafceb91112
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-11 16:32:54 +0900

    Time: 86 ms (42.42%), Space: 74.3 MB (5.56%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,49 +1,15 @@
 class Solution {
-private:
-    std::vector<int> numsArray;
-    
-    int findBestSubarray(int left, int right) {
-        // Base case - empty array.
-        if (left > right) {
-            return INT_MIN;
-        }
-        
-        int mid = left + (right - left) / 2;
-        int curr = 0;
-        int bestLeftSum = 0;
-        int bestRightSum = 0;
-        
-        // Iterate from the middle to the beginning.
-        for (int i = mid - 1; i >= left; i--) {
-            curr += numsArray[i];
-            bestLeftSum = std::max(bestLeftSum, curr);
-        }
-        
-        // Reset curr and iterate from the middle to the end.
-        curr = 0;
-        for (int i = mid + 1; i <= right; i++) {
-            curr += numsArray[i];
-            bestRightSum = std::max(bestRightSum, curr);
-        }
-        
-        // The bestCombinedSum uses the middle element and the best
-        // possible sum from each half.
-        int bestCombinedSum = numsArray[mid] + bestLeftSum + bestRightSum;
-        
-        // Find the best subarray possible from both halves.
-        int leftHalf = findBestSubarray(left, mid - 1);
-        int rightHalf = findBestSubarray(mid + 1, right);
-        
-        // The largest of the 3 is the answer for any given input array.
-        return std::max(bestCombinedSum, std::max(leftHalf, rightHalf));
-    }
-
 public:
-    int maxSubArray(std::vector<int>& nums) {
-        numsArray = nums;
-        
-        // Our helper function is designed to solve this problem for
-        // any array - so just call it using the entire input!
-        return findBestSubarray(0, numsArray.size() - 1);
+	int globalMaxSum;
+	int maxSubArray(vector<int> &nums, int n) {
+		if (n == 1) return nums[0];
+		int currMaxSum = max(nums[n - 1], maxSubArray(nums, n - 1) + nums[n - 1]);
+		globalMaxSum = max(globalMaxSum, currMaxSum);
+		return currMaxSum;
+	}
+    int maxSubArray(vector<int> &nums) {
+        globalMaxSum = nums[0];
+		maxSubArray(nums, nums.size());
+        return globalMaxSum;
     }
 };
