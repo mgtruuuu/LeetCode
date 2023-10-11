c7e2a4f558f92e5188793accc3b1465047ff2dbb
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-11 16:47:15 +0900

    Time: 72 ms (93.17%), Space: 68.1 MB (25.76%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,21 +1,26 @@
 class Solution {
-public:
-    int maxSubArray(vector<int>& nums) {
-        int maxSum = INT_MIN;
-        int currentSum = 0;
-        
-        for (int i = 0; i < nums.size(); i++) {
-            currentSum += nums[i];
-            
-            if (currentSum > maxSum) {
-                maxSum = currentSum;
-            }
-            
-            if (currentSum < 0) {
-                currentSum = 0;
-            }
+  public:
+    int maxSubArray(vector<int>& nums)
+    {
+        const auto len = static_cast<int>(nums.size());
+
+        auto res = INT_MIN;
+
+        auto idx = 0;
+        while (idx != len) {
+
+            auto sum_part = 0;
+
+            while (idx != len && sum_part >= 0) {
+
+                sum_part += nums[idx++];
+
+                if (res < sum_part) {
+                    res = sum_part;
+                }
+            };
         }
-        
-        return maxSum;
+
+        return res;
     }
 };
