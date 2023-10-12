f8616b5795462b78d51ebbdaeb41c084d43f9cba
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-12 10:36:58 +0900

    Time: 301 ms (97.76%), Space: 120.6 MB (94.51%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,38 +1,71 @@
-// Approach 3: Depth First Search (DFS): Iterative
+class UnionFind {
+  private:
+    std::vector<int> m_parents;
+    std::vector<int> m_ranks;
+    int m_num_components;
+
+  public:
+    UnionFind(const int len) : m_num_components{ len }
+    {
+        m_parents.resize(len);
+        m_ranks.resize(len);
+        for (auto i = 0; i != len; ++i) {
+            m_parents[i] = i;
+            m_ranks[i] = 1;
+        }
+    }
+
+    int find(const int x)
+    {
+        if (x == m_parents[x]) {
+            return x;
+        }
+
+        return (m_parents[x] = find(m_parents[x]));
+    }
+
+    void unionSet(const int x, const int y)
+    {
+        const auto root_x = find(x);
+        const auto root_y = find(y);
+
+        if (root_x == root_y) {
+            return;
+        }
+
+        if (m_ranks[root_x] < m_ranks[root_y]) {
+            m_parents[root_x] = root_y;
+        }
+        else if (m_ranks[root_x] > m_ranks[root_y]) {
+            m_parents[root_y] = root_x;
+        }
+        else {
+            m_parents[root_x] = root_y;
+            ++m_ranks[root_y];
+        }
+    }
+
+    bool isConnected(const int x, const int y)
+    {
+        return find(x) == find(y);
+    }
+
+    int getNumComponents() const
+    {
+        return m_num_components;
+    }
+};
+
 class Solution {
   public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination)
     {
-        std::vector<std::vector<int>> adjacency_list(n);
+        UnionFind uf{ n };
 
-        for (std::vector<int> edge : edges) {
-            adjacency_list[edge[0]].push_back(edge[1]);
-            adjacency_list[edge[1]].push_back(edge[0]);
+        for (const auto& edge : edges) {
+            uf.unionSet(edge.front(), edge.back());
         }
 
-        std::stack<int> st;
-        st.push(source);
-        std::vector<bool> seen(n, false);
-
-        while (st.empty() == false) {
-
-            const auto node = st.top();
-            st.pop();
-
-            if (node == destination) {
-                return true;
-            }
-
-            if (seen[node] == true) {
-                continue;
-            }
-
-            seen[node] = true;
-            for (const auto i : adjacency_list[node]) {
-                st.push(i);
-            }
-        }
-
-        return false;
+        return uf.isConnected(source, destination);
     }
 };
