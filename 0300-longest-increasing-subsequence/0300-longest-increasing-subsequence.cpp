5f90e9a00840116d2f487564cab844e53871d473
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-13 15:16:36 +0900

    Time: 234 ms (63.95%), Space: 10.9 MB (40.00%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -4,16 +4,26 @@ class Solution {
     {
         const auto len_nums = nums.size();
 
-        std::vector<int> dps(len_nums, 1);
+        auto res = INT_MIN;
+        
+        std::vector<int> dps(len_nums);
 
-        for (auto idx_rhs = std::size_t(0); idx_rhs != len_nums; ++idx_rhs) {
-            for (auto idx_lhs = std::size_t(0); idx_lhs != idx_rhs; ++idx_lhs) {
-                if (nums[idx_lhs] < nums[idx_rhs] && dps[idx_rhs] < dps[idx_lhs] + 1) {
-                    dps[idx_rhs] = dps[idx_lhs] + 1;
+        for (auto idx_right = std::size_t(0); idx_right != len_nums; ++idx_right) {
+
+            dps[idx_right] = 1;
+
+            for (auto idx_left = std::size_t(0); idx_left != idx_right; ++idx_left) {
+                
+                if (nums[idx_left] < nums[idx_right] && dps[idx_right] < dps[idx_left] + 1) {
+                    dps[idx_right] = dps[idx_left] + 1;
                 }
+            }
+            
+            if (res < dps[idx_right]) {
+                res = dps[idx_right];
             }
         }
 
-        return *std::max_element(dps.begin(), dps.end());
+        return res;
     }
 };
