864f05a94b6c248b1fcf706a89130ecaa88a6a45
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-16 11:13:45 +0900

    Time: 120 ms (65.44%), Space: 13.5 MB (27.89%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
@@ -1,33 +1,44 @@
 class Solution {
-public:
-    int findNumberOfLIS(std::vector<int>& nums) {
-        int n = nums.size();
-        vector<int> length(n, 1);
-        vector<int> count(n, 1);
+  public:
+    int findNumberOfLIS(vector<int>& nums)
+    {
+        const auto len_nums = nums.size();
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i]) {
-                    if (length[j] + 1 > length[i]) {
-                        length[i] = length[j] + 1;
-                        count[i] = 0;
+        std::vector<int> lengths(len_nums);
+        std::vector<int> counts(len_nums);
+
+        auto max_length = 0;
+
+        for (auto idx_right = std::size_t(0); idx_right != len_nums; ++idx_right) {
+            lengths[idx_right] = 1;
+            counts[idx_right] = 1;
+            for (auto idx_left = 0; idx_left != idx_right; ++idx_left) {
+
+                if (nums[idx_left] < nums[idx_right]) {
+
+                    if (lengths[idx_right] < lengths[idx_left] + 1) {
+
+                        lengths[idx_right] = lengths[idx_left] + 1;
+                        counts[idx_right] = counts[idx_left];
                     }
-                    if (length[j] + 1 == length[i]) {
-                        count[i] += count[j];
+                    else if (lengths[idx_right] == lengths[idx_left] + 1) {
+                        counts[idx_right] += counts[idx_left];
                     }
                 }
             }
-        }
 
-        int maxLength = *max_element(length.begin(), length.end());
-        int result = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (length[i] == maxLength) {
-                result += count[i];
+            if (max_length < lengths[idx_right]) {
+                max_length = lengths[idx_right];
             }
         }
 
-        return result;
+        auto sum_counts = 0;
+        for (auto idx = std::size_t(0); idx != len_nums; ++idx) {
+            if (lengths[idx] == max_length) {
+                sum_counts += counts[idx];
+            }
+        }
+
+        return sum_counts;
     }
 };
