33e8e2a6757d7b273f84e895bcb968af10a1f606
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-16 11:54:00 +0900

    Time: 130 ms (53.41%), Space: 13.6 MB (27.89%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
@@ -1,43 +1,111 @@
+/*
+// Approach 1: Bottom-up Dynamic Programming
 class Solution {
-public:
-    int findNumberOfLIS(std::vector<int>& nums) {
-        int n = nums.size();
-        vector<int> length(n, 0);
-        vector<int> count(n, 0);
+  public:
+    int findNumberOfLIS(vector<int>& nums)
+    {
+        const auto len_nums = nums.size();
 
-        function<void(int)> calculateDP = [&](int i) {
-            if (length[i] != 0)
-                return;
+        std::vector<int> lengths(len_nums);
+        std::vector<int> counts(len_nums);
 
-            length[i] = 1;
-            count[i] = 1;
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i]) {
-                    calculateDP(j);
-                    if (length[j] + 1 > length[i]) {
-                        length[i] = length[j] + 1;
-                        count[i] = 0;
+        auto max_length = 0;
+
+        for (auto idx_right = std::size_t(0); idx_right != len_nums; ++idx_right) {
+
+            lengths[idx_right] = 1;
+            counts[idx_right] = 1;
+            for (auto idx_left = std::size_t(0); idx_left != idx_right; ++idx_left) {
+
+                if (nums[idx_left] < nums[idx_right]) {
+
+                    if (lengths[idx_right] < lengths[idx_left] + 1) {
+
+                        lengths[idx_right] = lengths[idx_left] + 1;
+                        counts[idx_right] = counts[idx_left];
                     }
-                    if (length[j] + 1 == length[i]) {
-                        count[i] += count[j];
+                    else if (lengths[idx_right] == lengths[idx_left] + 1) {
+                        counts[idx_right] += counts[idx_left];
                     }
                 }
             }
-        };
 
-	int maxLength = 0;
-        int result = 0;
-        for (int i = 0; i < n; i++) {
-            calculateDP(i);
-            if (length[i] > maxLength)
-                maxLength = length[i];
+            if (max_length < lengths[idx_right]) {
+                max_length = lengths[idx_right];
+            }
         }
 
-        for (int i = 0; i < n; i++) {
-            if (length[i] == maxLength)
-                result += count[i];
+        
+        
+        auto sum_counts = 0;
+        for (auto idx = std::size_t(0); idx != len_nums; ++idx) {
+            if (lengths[idx] == max_length) {
+                sum_counts += counts[idx];
+            }
         }
 
-        return result;
+        return sum_counts;
+    }
+};
+*/
+
+
+// Approach 2: Top-down Dynamic Programming (Memoization)
+class Solution {
+  private:
+    void calculateDP(const std::size_t idx_right, std::vector<int>& nums, std::vector<int>& lengths,
+                     std::vector<int>& counts)
+    {
+        if (lengths[idx_right] != 0) {
+            return;
+        }
+
+        lengths[idx_right] = 1;
+        counts[idx_right] = 1;
+        for (auto idx_left = std::size_t(0); idx_left != idx_right; ++idx_left) {
+
+            if (nums[idx_left] < nums[idx_right]) {
+
+                calculateDP(idx_left, nums, lengths, counts);
+
+                if (lengths[idx_right] < lengths[idx_left] + 1) {
+
+                    lengths[idx_right] = lengths[idx_left] + 1;
+                    counts[idx_right] = counts[idx_left];
+                }
+                else if (lengths[idx_right] == lengths[idx_left] + 1) {
+                    counts[idx_right] += counts[idx_left];
+                }
+            }
+        }
+    }
+
+  public:
+    int findNumberOfLIS(vector<int>& nums)
+    {
+        const auto len_nums = nums.size();
+
+        std::vector<int> lengths(len_nums, 0);
+        std::vector<int> counts(len_nums, 0);
+
+        calculateDP(len_nums - 1, nums, lengths, counts);
+
+        auto max_length = 0;
+        for (auto idx = std::size_t(0); idx != len_nums; ++idx) {
+
+            calculateDP(idx, nums, lengths, counts);
+            if (max_length < lengths[idx]) {
+                max_length = lengths[idx];
+            }
+        }
+
+        auto sum_counts = 0;
+        for (auto idx = std::size_t(0); idx != len_nums; ++idx) {
+            if (lengths[idx] == max_length) {
+                sum_counts += counts[idx];
+            }
+        }
+
+        return sum_counts;
     }
 };
