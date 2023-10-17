2fb46f3520eb4f6da9e2ea99333025529ef48233
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-17 16:41:41 +0900

    Time: 11 ms (24.80%), Space: 11.2 MB (29.08%) - LeetHub

---
+++ 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    int findLengthOfLCIS(vector<int>& nums)
+    {
+        auto max_length = 1;
+        auto current_length = 1;
+        for (auto idx = std::size_t(1); idx != nums.size(); ++idx) {
+
+            if (nums[idx - 1] < nums[idx]) {
+
+                ++current_length;
+
+                continue;
+            }
+            else if (max_length < current_length) {
+                max_length = current_length;
+            }
+            
+            current_length = 1;
+        }
+
+        if (max_length < current_length) {
+            max_length = current_length;
+        }
+        return max_length;
+    }
+};
