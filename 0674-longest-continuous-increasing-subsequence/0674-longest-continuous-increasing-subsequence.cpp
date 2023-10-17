08784f442ecfd13c3c49e6d05bcdc82085adb65b
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-17 17:10:56 +0900

    Time: 7 ms (73.73%), Space: 11.2 MB (29.08%) - LeetHub

--- 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.cpp
+++ 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.cpp
@@ -1,11 +1,33 @@
 class Solution {
-public:
-    int findLengthOfLCIS(vector<int>& nums) {
-        int ans = 0, anchor = 0;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (i > 0 && nums[i-1] >= nums[i]) anchor = i;
-            ans = max(ans, i - anchor + 1);
+  public:
+    int findLengthOfLCIS(vector<int>& nums)
+    {
+        auto len_nums_minus_one = nums.size();
+        --len_nums_minus_one;
+
+        auto max_length = 1;
+        auto current_length = 1;
+
+        auto idx = 0;
+        while (idx != len_nums_minus_one) {
+
+            auto temp = nums[idx++];
+            if (temp < nums[idx]) {
+
+                ++current_length;
+
+                continue;
+            }
+            else if (max_length < current_length) {
+                max_length = current_length;
+            }
+
+            current_length = 1;
         }
-        return ans;
+
+        if (max_length < current_length) {
+            max_length = current_length;
+        }
+        return max_length;
     }
 };
