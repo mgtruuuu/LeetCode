c4e8960577b47d494fa21faa64e5202ceb3ef1e1
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-17 17:36:03 +0900

    Time: 119 ms (88.11%), Space: 77.7 MB (42.34%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,3 +1,5 @@
+///*
+// Approach #3: Hash Table
 class Solution {
   public:
     bool containsNearbyDuplicate(vector<int>& nums, int k)
@@ -25,3 +27,29 @@ class Solution {
         return false;
     }
 };
+//*/
+
+
+/*
+// Approach #3: Hash Table - My sol
+class Solution {
+  public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k)
+    {
+        std::unordered_map<int, int> value2idx;
+
+        for (auto idx = 0; idx != static_cast<int>(nums.size()); ++idx) {
+
+            const auto p = value2idx.find(nums[idx]);
+
+            if (p != value2idx.end() && (idx - p->second) <= k) {
+                return true;
+            }
+
+            value2idx[nums[idx]] = idx;
+        }
+
+        return false;
+    }
+};
+*/
