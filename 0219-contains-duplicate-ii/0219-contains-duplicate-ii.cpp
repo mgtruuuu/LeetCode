7d80eba0dac67706f184ed52dc9e04257f4993c5
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-17 17:27:26 +0900

    Time: 136 ms (41.19%), Space: 77.8 MB (32.81%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -2,17 +2,24 @@ class Solution {
   public:
     bool containsNearbyDuplicate(vector<int>& nums, int k)
     {
-        std::unordered_map<int, int> value2idx;
-        
-        for (auto idx = 0; idx != static_cast<int>(nums.size()); ++idx) {
+        const auto len_nums = static_cast<int>(nums.size());
 
-            const auto p = value2idx.find(nums[idx]);
+        std::unordered_map<int, int> num2count;
 
-            if (p != value2idx.end() && (idx - p->second) <= k) {
+        for (auto idx = 0; idx < std::min(k + 1, len_nums); ++idx) {
+
+            if (++num2count[nums[idx]] == 2) {
                 return true;
             }
+        }
 
-            value2idx[nums[idx]] = idx;
+        for (auto idx = k + 1; idx < len_nums; ++idx) {
+
+            --num2count[nums[idx - k - 1]];
+
+            if (++num2count[nums[idx]] == 2) {
+                return true;
+            }
         }
 
         return false;
