175331a9fcf938d741a3d0d17600d16b6b3cdb9b
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-18 15:19:07 +0900

    Time: 8 ms (35.10%), Space: 7.7 MB (33.98%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -5,22 +5,22 @@ class Solution {
         auto ptr_left = s.begin();
         auto ptr_right = s.end() - 1;
         while (ptr_left <= ptr_right) {
+
             if (isalnum(*ptr_left) == false) {
+
                 ++ptr_left;
                 continue;
             }
 
             if (isalnum(*ptr_right) == false) {
+
                 --ptr_right;
                 continue;
             }
 
-            if (tolower(*ptr_left) != tolower(*ptr_right)) {
+            if (tolower(*ptr_left++) != tolower(*ptr_right--)) {
                 return false;
             }
-
-            ++ptr_left;
-            --ptr_right;
         }
 
         return true;
