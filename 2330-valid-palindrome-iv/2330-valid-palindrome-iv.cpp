57254a36a238655854741e7b1a8d160a0b9d4a4d
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-18 16:18:26 +0900

    Time: 10 ms (33.54%), Space: 8.7 MB (15.24%) - LeetHub

--- 2330-valid-palindrome-iv/2330-valid-palindrome-iv.cpp
+++ 2330-valid-palindrome-iv/2330-valid-palindrome-iv.cpp
@@ -1,27 +1,28 @@
 class Solution {
-  private:
-    bool checkPalindrome(const std::string& s, int idx_left, int idx_right, int count_left)
-    {
-        if (count_left < 0) {
-            return false;
-        }
-
-        while (idx_left < idx_right) {
-
-            if (s[idx_left] != s[idx_right]) {
-                return checkPalindrome(s, idx_left + 1, idx_right - 1, count_left - 1);
-            }
-
-            ++idx_left;
-            --idx_right;
-        }
-
-        return true;
-    }
-
   public:
     bool makePalindrome(string s)
     {
-        return checkPalindrome(s, 0, static_cast<int>(s.size()) - 1, 2);
+        auto idx_left = 0;
+        auto idx_right = static_cast<int>(s.size()) - 1;
+
+        auto count_left = 2;
+        while (count_left-- != 0) {
+
+            while (idx_left < idx_right && s[idx_left++] == s[idx_right--]) {
+            }
+        }
+
+        if (idx_left >= idx_right) {
+            return true;
+        }
+        
+        while (idx_left < idx_right) {
+
+            if (s[idx_left++] != s[idx_right--]) {
+                return false;
+            }
+        }
+        
+        return true;
     }
 };
