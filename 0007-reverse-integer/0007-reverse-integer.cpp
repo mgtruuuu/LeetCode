afc625ccce80fe28df96b85d40ab4feb0a21755a
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-19 11:38:06 +0900

    Time: 2 ms (38.24%), Space: 6.3 MB (40.91%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,53 +1,24 @@
+constexpr auto int_max_last = 7;
+constexpr auto int_min_last = -8;
+
 class Solution {
   public:
     int reverse(int x)
     {
-        if (x == 0) {
-            return 0;
-        }
-
-        std::stack<int> s;
-        while (x != 0) {
-            const auto quotient = x / 10;
-            s.push(x - 10 * quotient);
-            x = quotient;
-        }
-
         auto res = 0;
-        const auto len_q = static_cast<int>(s.size());
-        
-        for (auto i = 0; i != len_q - 1; ++i) {
+        while (x != 0) {
+            auto pop = x % 10;
+            x /= 10;
 
-            auto mul = s.top();
-            for (auto j = 0; j != i; ++j) {
-                mul *= 10;
-            }
-
-            res += mul;
-            s.pop();
-        }
-
-        {
-            auto mul = s.top();
-            
-            if (len_q == 10 && (mul > 2 || mul < -2)) {
+            if (res > INT_MAX / 10 || (res == INT_MAX / 10 && pop > int_max_last)) {
                 return 0;
             }
-            
-            for (auto j = 0; j != len_q - 1; ++j) {
-                mul *= 10;
+
+            if (res < INT_MIN / 10 || (res == INT_MIN / 10 && pop < int_min_last)) {
+                return 0;
             }
 
-            if (len_q == 10) {
-
-                
-                if ((res > 0 && int(0x7FFFFFFF) - res < mul) || res < 0 && (int(0x80000000) - res > mul)) {
-                    return 0;
-                }
-            }
-
-            res += mul;
-            s.pop();
+            res = res * 10 + pop;
         }
 
         return res;
