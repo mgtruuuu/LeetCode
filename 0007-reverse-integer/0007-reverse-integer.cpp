624f3e4188bbc65659e983e9ced782e07669ba91
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-19 11:42:04 +0900

    Time: 0 ms (100.00%), Space: 7.3 MB (10.79%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,3 +1,4 @@
+/*
 constexpr auto int_max_last = 7;
 constexpr auto int_min_last = -8;
 
@@ -7,6 +8,7 @@ class Solution {
     {
         auto res = 0;
         while (x != 0) {
+            
             auto pop = x % 10;
             x /= 10;
 
@@ -19,6 +21,75 @@ class Solution {
             }
 
             res = res * 10 + pop;
+        }
+
+        return res;
+    }
+};
+*/
+
+
+class Solution {
+  public:
+    int reverse(int x)
+    {
+        if (x == 0) {
+            return 0;
+        }
+
+        std::stack<int> s;
+        while (x != 0) {
+            const auto quotient = x / 10;
+            s.push(x - 10 * quotient);
+            x = quotient;
+        }
+
+        auto res = 0;
+        const auto len_q = static_cast<int>(s.size());
+
+        if (len_q < 10) {
+            for (auto i = 0; i != len_q; ++i) {
+
+                auto mul = s.top();
+                for (auto j = 0; j != i; ++j) {
+                    mul *= 10;
+                }
+
+                res += mul;
+                s.pop();
+            }
+        }
+        else {
+
+            for (auto i = 0; i != len_q - 1; ++i) {
+
+                auto mul = s.top();
+                for (auto j = 0; j != i; ++j) {
+                    mul *= 10;
+                }
+
+                res += mul;
+                s.pop();
+            }
+
+            {
+                auto mul = s.top();
+
+                if (mul > 2 || mul < -2) {
+                    return 0;
+                }
+
+                for (auto j = 0; j != len_q - 1; ++j) {
+                    mul *= 10;
+                }
+
+                if ((res > 0 && INT_MAX - res < mul) || (res < 0 && INT_MIN - res > mul)) {
+                    return 0;
+                }
+
+                res += mul;
+                s.pop();
+            }
         }
 
         return res;
