2e482c3e38218b514ae8844f763a671b8d034761
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-22 12:06:03 +0900

    Time: 7 ms (72.30%), Space: 10 MB (84.56%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,5 +1,3 @@
-constexpr auto one = std::size_t(1);
-
 class Solution {
   public:
     int minPathSum(vector<vector<int>>& grid)
@@ -7,34 +5,31 @@ class Solution {
         const auto len_row = grid.size();
         const auto len_col = grid.front().size();
 
-        std::vector<std::vector<int>> paths(len_row, std::vector<int>(len_col));
+        std::vector<int> paths(len_col);
 
         {
-            paths[0][0] = grid.front().front();
-
             auto sum = 0;
-            auto row = std::size_t(0);
-            while (row != len_row) {
-
-                sum += grid[row][0];
-                paths[row++][0] = sum;
-            }
-
-            sum = 0;
             auto col = std::size_t(0);
             while (col != len_col) {
 
                 sum += grid[0][col];
-                paths[0][col++] = sum;
+                paths[col++] = sum;
             }
         }
 
-        for (auto row = one; row != len_row; ++row) {
-            for (auto col = one; col != len_col; ++col) {
-                paths[row][col] = std::min(paths[row - one][col], paths[row][col - one]) + grid[row][col];
+        for (auto row = std::size_t(1); row != len_row; ++row) {
+            
+            paths[0] += grid[row][0];
+            
+            for (auto col = std::size_t(1); col != len_col; ++col) {
+                
+                if (paths[col] > paths[col - 1]) {
+                    paths[col] = paths[col - 1];
+                }
+                paths[col] += grid[row][col];
             }
         }
 
-        return paths[len_row - one][len_col - one];
+        return paths[len_col - std::size_t(1)];
     }
 };
