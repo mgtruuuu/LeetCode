2ff51ea217bfba617316813cbf884286b00e4aaf
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-22 23:48:56 +0900

    Time: 2 ms (60.86%), Space: 8 MB (48.45%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,4 +1,4 @@
-///*
+/*
 // Approach 1: Dynamic Programming - 1d array
 constexpr auto one = std::size_t(1);
 class Solution {
@@ -30,6 +30,42 @@ class Solution {
         }
 
         return dp.back();
+    }
+};
+*/
+
+
+///*
+// 
+constexpr auto one = std::size_t(1);
+class Solution {
+  public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid)
+    {
+        const auto len_row = obstacleGrid.size();
+        const auto len_col = obstacleGrid.front().size();
+
+        {
+            auto col = 0;
+            while (col != len_col && obstacleGrid[0][col] == 0) {
+                obstacleGrid[0][col++] = 1;
+            }
+            while (col != len_col) {
+                obstacleGrid[0][col++] = 0;
+            }
+        }
+
+        for (auto row = one; row != len_row; ++row) {
+
+            obstacleGrid[row][0] = (obstacleGrid[row - one][0] == 0 || obstacleGrid[row][0] == 1) ? 0 : 1;
+
+            for (auto col = one; col != len_col; ++col) {
+                obstacleGrid[row][col] =
+                    obstacleGrid[row][col] == 1 ? 0 : obstacleGrid[row - one][col] + obstacleGrid[row][col - one];
+            }
+        }
+
+        return obstacleGrid.back().back();
     }
 };
 //*/
