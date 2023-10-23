4cac8d0d9cf8bcdd52c2df88f799748d25474dd4
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-23 14:17:55 +0900

    Time: 3 ms (40.65%), Space: 7.1 MB (20.70%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,8 +1,9 @@
-std::unordered_map<char, std::string> digit2letters{ { '2', "abc" }, { '3', "def" },  { '4', "ghi" }, { '5', "jkl" },
-                                                     { '6', "mno" }, { '7', "pqrs" }, { '8', "tuv" }, { '9', "wxyz" } };
-
 class Solution {
   private:
+    std::unordered_map<char, std::string> digit2letters{ { '2', "abc" }, { '3', "def" }, { '4', "ghi" },
+                                                         { '5', "jkl" }, { '6', "mno" }, { '7', "pqrs" },
+                                                         { '8', "tuv" }, { '9', "wxyz" } };
+    
     void backtracking(const std::string& digits, std::string& letters, std::vector<std::string>& res)
     {
         if (letters.size() == digits.size()) {
@@ -12,8 +13,7 @@ class Solution {
             return;
         }
 
-        const auto idx = letters.size();
-        for (const auto ch : digit2letters[digits[idx]]) {
+        for (const auto ch : digit2letters[digits[letters.size()]]) {
 
             letters.push_back(ch);
             backtracking(digits, letters, res);
@@ -27,7 +27,7 @@ class Solution {
         if (digits.empty() == true) {
             return {};
         }
-        
+
         std::vector<std::string> res;
         std::string empty;
         backtracking(digits, empty, res);
