7502b5d88b6c95cfa63136fa37b5650857f73105
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-23 15:09:28 +0900

    Time: 108 ms (80.27%), Space: 52.7 MB (99.45%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,54 +1,55 @@
 class Solution {
   private:
-    void backtrack(const int n, const int k, const int first_num, std::vector<int>& combination,
-                   std::vector<std::vector<int>>& combinations)
+    void backtracking(const int n, const int k, std::vector<int>& combination, std::vector<std::vector<int>>& res)
     {
-        if (static_cast<int>(combination.size()) == k) {
+        if (combination.size() == k) {
 
-            combinations.push_back(combination);
+            res.push_back(combination);
 
             return;
         }
 
-        const auto iEnd = n - k + 1 + static_cast<int>(combination.size());
-        for (auto i = first_num; i <= iEnd; ++i) {
+        for (auto num = combination.empty() == true ? 1 : combination.back() + 1; num <= n; ++num) {
 
-            combination.push_back(i);
-            backtrack(n, k, i + 1, combination, combinations);
+            combination.push_back(num);
+            backtracking(n, k, combination, res);
             combination.pop_back();
         }
     }
-    
-    unsigned long long getCombination(const int n, int r);
+
+    int getCombination(int n, int k)
+    {
+        if (k > n / 2) {
+            k = n - k;
+        }
+
+        if (k == 0) {
+            return 1;
+        }
+
+        auto numerator = n;
+        auto denominator = 1;
+
+        long res = 1;
+        auto count = k;
+        while (count-- != 0) {
+
+            res *= (numerator--);
+            res /= (denominator++);
+        }
+
+        return res;
+    }
 
   public:
     vector<vector<int>> combine(int n, int k)
     {
+        std::vector<int> combination;
         std::vector<std::vector<int>> res;
         res.reserve(getCombination(n, k));
 
-        std::vector<int> combination;
-        combination.reserve(k);
-
-        backtrack(n, k, 1, combination, res);
+        backtracking(n, k, combination, res);
 
         return res;
     }
 };
-
-unsigned long long Solution::getCombination(const int n, int r)
-{
-    r = std::min(r, n - r);
-
-    unsigned long long numerator = 1;
-    for (auto i = n - r + 1; i <= n; ++i) {
-        numerator *= i;
-    }
-
-    unsigned long long denominator = 1;
-    for (auto i = 1; i <= r; ++i) {
-        denominator *= i;
-    }
-
-    return (numerator / denominator);
-}
