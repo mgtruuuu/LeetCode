c7df22b2a35e3e6ce232a1cee3ba5d76f2acc916
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-24 11:26:11 +0900

    Time: 6 ms (26.59%), Space: 14.9 MB (11.00%) - LeetHub

---
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+  private:
+    TreeNode* buildTreeHelper(const std::vector<int>& preorder, const std::unordered_map<int, int>& inorderNode2idx,
+                              const int idx_in_start, const int idx_in_end, const int idx_pre)
+    {
+        if (idx_in_start == idx_in_end) {
+            return new TreeNode{ preorder[idx_pre] };
+        }
+        else if (idx_in_start > idx_in_end) {
+            return nullptr;
+        }
+
+        const auto dist = inorderNode2idx.at(preorder[idx_pre]) - idx_in_start;
+
+        return new TreeNode{
+            preorder[idx_pre],
+            buildTreeHelper(preorder, inorderNode2idx, idx_in_start, idx_in_start + dist - 1, idx_pre + 1),
+            buildTreeHelper(preorder, inorderNode2idx, idx_in_start + dist + 1, idx_in_end, idx_pre + dist + 1)
+        };
+    }
+
+  public:
+    TreeNode* bstFromPreorder(vector<int>& preorder)
+    {
+        auto inorder = preorder;
+        std::sort(inorder.begin(), inorder.end());
+
+        std::unordered_map<int, int> inorderNode2idx;
+        for (auto idx = 0; idx != static_cast<int>(inorder.size()); ++idx) {
+            inorderNode2idx[inorder[idx]] = idx;
+        }
+
+        return buildTreeHelper(preorder, inorderNode2idx, 0, preorder.size() - 1, 0);
+    }
+};
