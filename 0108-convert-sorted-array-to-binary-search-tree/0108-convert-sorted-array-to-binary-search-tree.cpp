194a538296009e7b5114e24e4b11e8b50ee020e9
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-24 13:01:48 +0900

    Time: 12 ms (41.34%), Space: 21.7 MB (51.45%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -10,11 +10,11 @@
  * };
  */
 
+/*
 // Approach 1: Preorder Traversal: Always Choose Left Middle Node as a Root
 class Solution {
   private:
-    TreeNode* sortedArrayToBSTHelper(const std::vector<int>& nums, const int idx_start,
-                                     const int idx_end)
+    TreeNode* sortedArrayToBSTHelper(const std::vector<int>& nums, const int idx_start, const int idx_end)
     {
         if (idx_start > idx_end) {
             return nullptr;
@@ -24,6 +24,35 @@ class Solution {
         }
 
         const auto idx_middle_left = (idx_start + idx_end) / 2;
+        auto* res = new TreeNode{ nums[idx_middle_left] };
+        res->left = sortedArrayToBSTHelper(nums, idx_start, idx_middle_left - 1);
+        res->right = sortedArrayToBSTHelper(nums, idx_middle_left + 1, idx_end);
+
+        return res;
+    }
+
+  public:
+    TreeNode* sortedArrayToBST(vector<int>& nums)
+    {
+        return sortedArrayToBSTHelper(nums, 0, nums.size() - 1);
+    }
+};
+*/
+
+
+// Approach 2: Approach 2: Preorder Traversal: Always Choose Right Middle Node as a Root
+class Solution {
+  private:
+    TreeNode* sortedArrayToBSTHelper(const std::vector<int>& nums, const int idx_start, const int idx_end)
+    {
+        if (idx_start > idx_end) {
+            return nullptr;
+        }
+        else if (idx_start == idx_end) {
+            return new TreeNode{ nums[idx_start] };
+        }
+
+        const auto idx_middle_left = (idx_start + idx_end + 1) / 2;
         auto* res = new TreeNode{ nums[idx_middle_left] };
         res->left = sortedArrayToBSTHelper(nums, idx_start, idx_middle_left - 1);
         res->right = sortedArrayToBSTHelper(nums, idx_middle_left + 1, idx_end);
