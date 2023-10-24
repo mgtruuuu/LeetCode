15c5531a44a5317d95d0cdb7853a25b95599004e
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-24 16:21:09 +0900

    Time: 15 ms (98.66%), Space: 32.9 MB (42.04%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -11,6 +11,8 @@
  */
 
 
+/*
+//
 class Solution {
   private:
     TreeNode* minKeyNode(TreeNode* node)
@@ -52,6 +54,63 @@ class Solution {
             else {
 
                 root->val = minKeyNode(root->right)->val;
+                root->right = deleteNode(root->right, root->val);
+            }
+        }
+
+        return root;
+    }
+};
+*/
+
+
+//
+class Solution {
+  private:
+    TreeNode* secondMinKeyNode(TreeNode* node)
+    {
+        while (node->left->left != nullptr) {
+            node = node->left;
+        }
+
+        return node;
+    }
+
+  public:
+    TreeNode* deleteNode(TreeNode* root, int key)
+    {
+        if (root == nullptr) {
+            return nullptr;
+        }
+
+        if (key < root->val) {
+            root->left = deleteNode(root->left, key);
+        }
+        else if (key > root->val) {
+            root->right = deleteNode(root->right, key);
+        }
+        else {
+
+            TreeNode* temp = root;
+
+            if (root->left == nullptr) {
+
+                root = root->right;
+                delete temp;
+            }
+            else if (root->right == nullptr) {
+
+                root = root->left;
+                delete temp;
+            }
+            else if (root->right->left == nullptr) {
+
+                root->val = root->right->val;
+                root->right = deleteNode(root->right, root->val);
+            }
+            else {
+
+                root->val = secondMinKeyNode(root->right)->left->val;
                 root->right = deleteNode(root->right, root->val);
             }
         }
