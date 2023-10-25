60d311034879a6d78672072a66d458930a8061a8
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-25 23:31:52 +0900

    Time: 45 ms (80.49%), Space: 40.2 MB (31.24%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -1,55 +1,28 @@
 class Solution {
-  public:
-    int maxSubarraySumCircular(vector<int>& nums)
-    {
-        auto curr_max_subarray = nums.front();
-        auto curr_min_subarray = nums.front();
-        auto sum_max_subarray = curr_max_subarray;
-        auto sum_min_subarray = curr_min_subarray;
-        auto sum_total = nums[0];
-        bool all_positive = true;
-        bool all_negative = true;
+public:
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int curMax = 0;
+        int curMin = 0;
+        int maxSum = nums[0];
+        int minSum = nums[0];
+        int totalSum = 0;
         
-        for (auto idx = std::size_t(1); idx != static_cast<int>(nums.size()); ++idx) {
-
-            if (curr_max_subarray < 0) {
-                curr_max_subarray = nums[idx];
-            }
-            else {
-                curr_max_subarray += nums[idx];
-            }
-
-            if (sum_max_subarray < curr_max_subarray) {
-                sum_max_subarray = curr_max_subarray;
-            }
-
+        for (int num: nums) {
+            // Normal Kadane's
+            curMax = max(curMax, 0) + num;
+            maxSum = max(maxSum, curMax);
             
-            if (curr_min_subarray > 0) {
-                curr_min_subarray = nums[idx];
-            }
-            else {
-                curr_min_subarray += nums[idx];
-            }
-
-            if (sum_min_subarray > curr_min_subarray) {
-                sum_min_subarray = curr_min_subarray;
-            }
-
-            sum_total += nums[idx];
+            // Kadane's but with min to find minimum subarray
+            curMin = min(curMin, 0) + num;
+            minSum = min(minSum, curMin);
             
-            if (all_positive == true && nums[idx] < 0) {
-                all_positive = false;
-            }
-            if (all_negative == true && nums[idx] > 0) {
-                all_negative = false;
-            }
+            totalSum += num;  
         }
 
-        if (all_positive == true || all_negative == true) {
-            return sum_max_subarray;
+        if (totalSum == minSum) {
+            return maxSum;
         }
-        else {
-            return std::max(sum_total - sum_min_subarray, sum_max_subarray);
-        }
+        
+        return max(maxSum, totalSum - minSum);
     }
 };
