4e0e4f91dfe3ed774cf11a442be4a1848cdc03d8
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-25 16:10:33 +0900

    Time: 86 ms (44.60%), Space: 68.1 MB (37.19%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,64 +1,27 @@
-// Approach 1: Optimized Brute Force
-
-
-///*
-// Approach 2 - 1 : Dynamic Programming, Kadane's Algorithm
-class Solution {
-  public:
-    int maxSubArray(std::vector<int>& nums)
-    {
-        // Initialize our variables using the first element.
-        auto current_subarray = nums[0];
-        auto max_subarray = nums[0];
-
-        // Start with the 2nd element since we already used the first one.
-        for (auto idx = std::size_t(1); idx != static_cast<int>(nums.size()); ++idx) {
-
-            
-            if (current_subarray < 0) {
-                current_subarray = nums[idx];
-            }
-            else {
-                current_subarray += nums[idx];
-            }
-            
-            if (max_subarray < current_subarray) {
-                max_subarray = current_subarray;
-            }
-        }
-
-        return max_subarray;
-    }
-};
-//*/
-
-
-/*
-// Approach 2 - 2 : Dynamic Programming, Kadane's Algorithm <------ My solution
 class Solution {
   public:
     int maxSubArray(vector<int>& nums)
     {
-        const auto len = static_cast<int>(nums.size());
+        auto sum = nums.front();
+        auto max_sum = sum;
 
-        auto res = INT_MIN;
-
-        auto idx = 0;
-        while (idx != len) {
-
-            auto sum_part = 0;
-
-            while (idx != len && sum_part >= 0) {
-
-                sum_part += nums[idx++];
-
-                if (res < sum_part) {
-                    res = sum_part;
-                }
-            };
+        if (sum < 0) {
+            sum = 0;
         }
 
-        return res;
+        for (auto idx = std::size_t(1); idx != nums.size(); ++idx) {
+        
+            sum += nums[idx];
+
+            if (sum > max_sum) {
+                max_sum = sum;
+            }
+
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        return max_sum;
     }
 };
-*/
