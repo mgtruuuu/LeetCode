6d3f4d614c6dfb5476d86a795ad430aaf9071412
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-26 08:50:24 +0900

    Time: 3 ms (73.71%), Space: 10 MB (46.54%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -2,24 +2,24 @@ class Solution {
   public:
     int searchInsert(vector<int>& nums, int target)
     {
-        auto idx_lhs = 0;
-        auto idx_rhs = static_cast<int>(nums.size()) - 1;
+        auto idx_left = 0;
+        auto idx_right = static_cast<int>(nums.size()) - 1;
 
-        while (idx_lhs <= idx_rhs) {
+        while (idx_left <= idx_right) {
 
-            const auto idx_mid = (idx_lhs + idx_rhs) / 2;
+            const auto idx_mid = (idx_left + idx_right) / 2;
 
             if (nums[idx_mid] < target) {
-                idx_lhs = idx_mid + 1;
+                idx_left = idx_mid + 1;
             }
             else if (nums[idx_mid] > target) {
-                idx_rhs = idx_mid - 1;
+                idx_right = idx_mid - 1;
             }
             else {
                 return idx_mid;
             }
         }
 
-        return idx_lhs;
+        return idx_left;
     }
 };
