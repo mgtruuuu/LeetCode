4bff44932a3084f4251aa22c9562a85e155a6edd
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-26 12:05:57 +0900

    Time: 7 ms (63.20%), Space: 13.7 MB (30.94%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,31 +1,40 @@
+///*//*/
+// Approach 1: Breadth-First Search
 class Solution {
-public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> words(wordDict.begin(), wordDict.end());
-        queue<int> queue;
-        vector<bool> seen(s.length(), false);
-        queue.push(0);
-        
-        while (!queue.empty()) {
-            int start = queue.front();
-            queue.pop();
-            
-            if (start == s.length()) {
+  public:
+    bool wordBreak(string s, vector<string>& wordDict)
+    {
+        const auto len_s = static_cast<int>(s.size());
+
+        std::unordered_set<std::string> words(wordDict.begin(), wordDict.end());
+        std::queue<int> q;
+        std::vector<bool> seen(len_s + 1, false);
+        q.push(0);
+        seen[0] = true;
+
+        while (q.empty() == false) {
+
+            const auto start = q.front();
+            q.pop();
+
+            if (start == len_s) {
                 return true;
             }
-            
-            for (int end = start + 1; end <= s.length(); end++) {
-                if (seen[end]) {
+
+            for (auto end = start + 1; end <= len_s; ++end) {
+
+                if (seen[end] == true) {
                     continue;
                 }
 
                 if (words.find(s.substr(start, end - start)) != words.end()) {
-                    queue.push(end);
+
+                    q.push(end);
                     seen[end] = true;
                 }
             }
         }
-        
+
         return false;
     }
 };
