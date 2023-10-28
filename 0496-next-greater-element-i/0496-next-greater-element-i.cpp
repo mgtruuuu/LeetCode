cff752cf22e4b3df40b9e4831153be41e45ccbea
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-28 17:56:38 +0900

    Time: 7 ms (47.43%), Space: 9.3 MB (40.69%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -0,0 +1,35 @@
+class Solution {
+  public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2)
+    {
+        std::unordered_map<int, int> curr_2_next;
+
+        {
+            std::stack<int> monotinic_stack;
+            for (auto idx = std::size_t(0); idx != nums2.size(); ++idx) {
+
+                while (monotinic_stack.empty() == false && monotinic_stack.top() < nums2[idx]) {
+
+                    curr_2_next[monotinic_stack.top()] = nums2[idx];
+                    monotinic_stack.pop();
+                }
+
+                monotinic_stack.push(nums2[idx]);
+            }
+
+            while (monotinic_stack.empty() == false) {
+
+                curr_2_next[monotinic_stack.top()] = -1;
+                monotinic_stack.pop();
+            }
+        }
+
+        std::vector<int> res;
+        res.reserve(nums1.size());
+        for (const auto num1 : nums1) {
+            res.push_back(curr_2_next[num1]);
+        }
+
+        return res;
+    }
+};
