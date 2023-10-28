d86412a46e24ef12f2428964c0d62a7aa78c0005
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-28 15:55:30 +0900

    Time: 97 ms (21.84%), Space: 100.1 MB (15.17%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -82,33 +82,33 @@ class Solution {
 */
 
 
-///*
+/*
 // Approach 3-1: DFS
 class Solution {
+  private:
+    void getFirstLand(const std::vector<std::vector<int>>& grid, int& row, int& col)
+    {
+        for (row = 0; row != static_cast<int>(grid.size()); ++row) {
+            for (col = 0; col != static_cast<int>(grid.front().size()); ++col) {
+                if (grid[row][col] == 1) {
+                    return;
+                }
+            }
+        }
+    }
+
   public:
     int islandPerimeter(vector<vector<int>>& grid)
     {
         const auto num_rows = static_cast<int>(grid.size());
         const auto num_cols = static_cast<int>(grid.front().size());
 
+        auto row = 0;
+        auto col = 0;
+        getFirstLand(grid, row, col);
+
         std::stack<std::pair<int, int>> s;
-
-        {
-            auto row = 0;
-            auto col = 0;
-            for (; row != num_rows; ++row) {
-                col = 0;
-                for (; col != num_cols; ++col) {
-
-                    if (grid[row][col] == 1) {
-                        goto FindFirstOneCell;
-                    }
-                }
-            }
-
-        FindFirstOneCell:
-            s.emplace(row, col);
-        }
+        s.emplace(row, col);
 
         auto perimeter = 0;
 
@@ -158,6 +158,97 @@ class Solution {
 
                 if (grid[row][col + 1] == 1) {
                     s.emplace(row, col + 1);
+                }
+            }
+        }
+
+        return perimeter;
+    }
+};
+*/
+
+
+///*
+// Approach 3-1: DFS
+class Solution {
+  private:
+    void getFirstLand(const std::vector<std::vector<int>>& grid, int& row, int& col)
+    {
+        for (row = 0; row != static_cast<int>(grid.size()); ++row) {
+            for (col = 0; col != static_cast<int>(grid.front().size()); ++col) {
+                if (grid[row][col] == 1) {
+                    return;
+                }
+            }
+        }
+    }
+
+  public:
+    int islandPerimeter(vector<vector<int>>& grid)
+    {
+        const auto num_rows = static_cast<int>(grid.size());
+        const auto num_cols = static_cast<int>(grid.front().size());
+
+        auto row = 0;
+        auto col = 0;
+        getFirstLand(grid, row, col);
+
+        std::stack<std::pair<int, int>> s;
+        s.emplace(row, col);
+        grid[row][col] = -1;
+
+        auto perimeter = 0;
+
+        while (s.empty() == false) {
+
+            const auto row = s.top().first;
+            const auto col = s.top().second;
+            s.pop();
+
+            // if (grid[row][col] == -1) {
+            //     continue;
+            // }
+            // grid[row][col] = -1;
+
+            perimeter += 4;
+
+            if (0 <= row - 1 && grid[row - 1][col] != 0) {
+
+                --perimeter;
+
+                if (grid[row - 1][col] == 1) {
+                    s.emplace(row - 1, col);
+                    grid[row - 1][col] = -1;
+                }
+            }
+
+            if (0 <= col - 1 && grid[row][col - 1] != 0) {
+
+                --perimeter;
+
+                if (grid[row][col - 1] == 1) {
+                    s.emplace(row, col - 1);
+                    grid[row][col - 1] = -1;
+                }
+            }
+
+            if (row + 1 != num_rows && grid[row + 1][col] != 0) {
+
+                --perimeter;
+
+                if (grid[row + 1][col] == 1) {
+                    s.emplace(row + 1, col);
+                    grid[row + 1][col] = -1;
+                }
+            }
+
+            if (col + 1 != num_cols && grid[row][col + 1] != 0) {
+
+                --perimeter;
+
+                if (grid[row][col + 1] == 1) {
+                    s.emplace(row, col + 1);
+                    grid[row][col + 1] = -1;
                 }
             }
         }
