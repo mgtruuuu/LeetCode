b2d6eba0c68932ed36d25d5ddca7a1879e6abc19
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-28 22:55:19 +0900

    Time: 11 ms (39.75%), Space: 22 MB (38.39%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+  private:
+    bool isValidBSTHelper(TreeNode* node, TreeNode* lower_limit = nullptr, TreeNode* upper_limit = nullptr)
+    {
+        if (node == nullptr) {
+            return true;
+        }
+
+        if (node->left != nullptr && node->left->val >= node->val) {
+            return false;
+        }
+
+        if (node->right != nullptr && node->right->val <= node->val) {
+            return false;
+        }
+
+        if (lower_limit != nullptr && lower_limit->val >= node->val) {
+            return false;
+        }
+
+        if (upper_limit != nullptr && upper_limit->val <= node->val) {
+            return false;
+        }
+
+        return isValidBSTHelper(node->left, lower_limit, node) && isValidBSTHelper(node->right, node, upper_limit);
+    }
+
+  public:
+    bool isValidBST(TreeNode* root)
+    {
+        return isValidBSTHelper(root);
+    }
+};
