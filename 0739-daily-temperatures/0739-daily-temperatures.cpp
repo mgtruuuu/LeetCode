e5ab2e3fff67e33f24f823e861b710e9b5e01b5f
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-29 10:16:17 +0900

    Time: 137 ms (70.80%), Space: 103.4 MB (44.04%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,3 +1,5 @@
+/*
+// Approach 1: Monotonic Stack
 class Solution {
   public:
     vector<int> dailyTemperatures(vector<int>& temperatures)
@@ -25,3 +27,34 @@ class Solution {
         return res;
     }
 };
+*/
+
+
+///*
+// Approach 2: Array, Optimized Space
+class Solution {
+  public:
+    vector<int> dailyTemperatures(vector<int>& temperatures)
+    {
+        std::stack<int> s;
+        for (auto idx = 0; idx != static_cast<int>(temperatures.size()); ++idx) {
+
+            while (s.empty() == false && temperatures[s.top()] < temperatures[idx]) {
+
+                temperatures[s.top()] = idx - s.top();
+                s.pop();
+            }
+
+            s.push(idx);
+        }
+
+        while (s.empty() == false) {
+
+            temperatures[s.top()] = 0;
+            s.pop();
+        }
+
+        return temperatures;
+    }
+};
+//*/
