a7e77188084fad73cf0916c322ebed05ebf7272f
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-29 10:44:58 +0900

    Time: 130 ms (6.98%), Space: 69.2 MB (43.45%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.cpp
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.cpp
@@ -1,3 +1,37 @@
+///*
+// Approach 1: Linear Iteration
+class Solution {
+  public:
+    vector<int> findBuildings(vector<int>& heights)
+    {
+        std::stack<int> s;
+        
+        auto idx = static_cast<int>(heights.size());
+        s.push(--idx);
+        for (; idx-- != 0;) {
+
+            if (heights[s.top()] < heights[idx]) {
+                s.push(idx);
+            }
+        }
+
+        std::vector<int> res;
+        res.reserve(s.size());
+
+        while (s.empty() == false) {
+
+            res.push_back(s.top());
+            s.pop();
+        }
+
+        return res;
+    }
+};
+//*/
+
+
+/*
+// Approach 2: Monotonic Stack
 class Solution {
   public:
     vector<int> findBuildings(vector<int>& heights)
@@ -23,3 +57,4 @@ class Solution {
         return res;
     }
 };
+*/
