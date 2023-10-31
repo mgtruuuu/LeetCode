ef358af21b1af92a0fd2c64a1f4b40887f570ead
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-31 14:46:10 +0900

    Time: 55 ms (47.92%), Space: 46.9 MB (23.74%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -1,3 +1,5 @@
+/*
+// Iteration
 class Trie {
   private:
     static constexpr int s_num_children = static_cast<int>('z') - static_cast<int>('a') + 1;
@@ -81,11 +83,100 @@ class Trie {
         delete[] m_children;
     }
 };
+*/
 
-/**
- * Your Trie object will be instantiated and called as such:
- * Trie* obj = new Trie();
- * obj->insert(word);
- * bool param_2 = obj->search(word);
- * bool param_3 = obj->startsWith(prefix);
- */
+
+class Trie {
+  private:
+    static constexpr int s_num_children = static_cast<int>('z') - static_cast<int>('a') + 1;
+    Trie** m_children;
+    bool m_is_done;
+
+  public:
+    Trie()
+    {
+        m_children = new Trie*[s_num_children];
+
+        for (auto i = 0; i != s_num_children; ++i) {
+            m_children[i] = nullptr;
+        }
+
+        m_is_done = false;
+    }
+
+    std::size_t getChildrenIndex(const char ch)
+    {
+        return ch - 'a';
+    }
+
+    void insertHelper(const std::string& word, const std::size_t idx_word)
+    {
+        if (idx_word == word.size()) {
+            m_is_done = true;
+
+            return;
+        }
+
+        const auto idx_children = getChildrenIndex(word[idx_word]);
+
+        if (m_children[idx_children] == nullptr) {
+            m_children[idx_children] = new Trie{};
+        }
+
+        m_children[idx_children]->insertHelper(word, idx_word + 1);
+    }
+
+    void insert(string word)
+    {
+        insertHelper(word, 0);
+    }
+
+    bool searchHelper(const std::string& word, const std::size_t idx_word)
+    {
+        if (idx_word == word.size()) {
+            return m_is_done;
+        }
+
+        const auto idx_children = getChildrenIndex(word[idx_word]);
+
+        if (m_children[idx_children] == nullptr) {
+            return false;
+        }
+
+        return m_children[idx_children]->searchHelper(word, idx_word + 1);
+    }
+
+    bool search(string word)
+    {
+        return searchHelper(word, 0);
+    }
+
+    bool startsWith(string prefix)
+    {
+        auto* node = this;
+
+        for (const auto ch : prefix) {
+
+            const auto idx = static_cast<int>(ch) - static_cast<int>('a');
+
+            if (node->m_children[idx] == nullptr) {
+                return false;
+            }
+
+            node = node->m_children[idx];
+        }
+
+        return true;
+    }
+
+    ~Trie()
+    {
+        for (auto i = 0; i != s_num_children; ++i) {
+            if (m_children[i] != nullptr) {
+                m_children[i]->~Trie();
+            }
+        }
+
+        delete[] m_children;
+    }
+};
