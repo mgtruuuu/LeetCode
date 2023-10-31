43b70cfd7661e1dcce21b66d5f261b4c1ffcff63
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-31 17:30:18 +0900

    Time: 9 ms (32.47%), Space: 13.4 MB (55.41%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -2,9 +2,44 @@ class Solution {
   public:
     int maxProfit(vector<int>& prices)
     {
+        const auto len_prices = prices.size();
+
+        auto max_profit = 0;
+        auto valley = INT_MAX;
+        auto peak = INT_MIN;
+
+        auto idx = std::size_t(0);
+        while (idx != len_prices) {
+
+            while (idx != len_prices && prices[idx] <= valley) {
+                valley = prices[idx++];
+            }
+
+            peak = valley;
+
+            while (idx != len_prices && peak <= prices[idx]) {
+                peak = prices[idx++];
+            }
+
+            max_profit += peak - valley;
+            valley = peak;
+        }
+
+        return max_profit;
+    }
+};
+
+
+
+/*
+// Approach 3: Simple One Pass
+class Solution {
+  public:
+    int maxProfit(vector<int>& prices)
+    {
         auto res = 0;
         for (auto idx = std::size_t(1); idx != prices.size(); ++idx) {
-            if (prices[idx] - prices[idx - 1] > 0) {
+            if (prices[idx - 1] < prices[idx]) {
                 res += (prices[idx] - prices[idx - 1]);
             }
         }
@@ -12,3 +47,4 @@ class Solution {
         return res;
     }
 };
+*/
