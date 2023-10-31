555917c0bbd5224cab96c2aabdf08325485b269f
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-31 09:06:31 +0900

    Time: 390 ms (27.12%), Space: 13.8 MB (62.75%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
   private:
     bool canPartitionHelper(const std::vector<int>& nums, const int row, const int col,
@@ -47,5 +48,54 @@ class Solution {
 
         auto dp = std::vector<std::vector<int>>(nums.size() + 1, std::vector<int>(half_sum + 1, -1));
         return canPartitionHelper(nums, nums.size(), half_sum, dp) == true;
+    }
+};
+*/
+
+
+class Solution {
+  public:
+    bool canPartition(vector<int>& nums)
+    {
+        auto half_sum = std::accumulate(nums.begin(), nums.end(), 0);
+
+        if (half_sum & 1) {
+            return false;
+        }
+        half_sum /= 2;
+
+        auto dp = std::vector<std::vector<bool>>(nums.size() + 1, std::vector<bool>(half_sum + 1));
+        {
+            for (auto row = 0; row != nums.size() + 1; ++row) {
+                dp[row][0] = true;
+            }
+
+            for (auto col = 1; col != half_sum + 1; ++col) {
+                dp[0][col] = false;
+            }
+        }
+
+        for (auto row = 1; row != nums.size() + 1; ++row) {
+            for (auto col = 1; col != half_sum + 1; ++col) {
+
+                if (dp[row - 1][col] == true) {
+
+                    dp[row][col] = true;
+
+                    continue;
+                }
+
+                if (col >= nums[row - 1] && dp[row - 1][col - nums[row - 1]] == true) {
+
+                    dp[row][col] = true;
+
+                    continue;
+                }
+
+                dp[row][col] = false;
+            }
+        }
+
+        return dp.back().back();
     }
 };
