b32cd99da6129502436e4fe3baf08764e6f2f42f
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-10-31 15:23:09 +0900

    Time: 47 ms (74.72%), Space: 46.7 MB (25.00%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -1,5 +1,3 @@
-///*
-// Approach 1 : Iteration
 class Trie {
   private:
     static constexpr int s_num_children = static_cast<int>('z') - static_cast<int>('a') + 1;
@@ -7,8 +5,15 @@ class Trie {
     bool m_is_done;
 
   public:
-    Trie() : m_children{ new Trie* [s_num_children] {} }, m_is_done{ false }
+    Trie()
     {
+        m_children = new Trie*[s_num_children];
+
+        for (auto i = 0; i != s_num_children; ++i) {
+            m_children[i] = nullptr;
+        }
+
+        m_is_done = false;
     }
 
     void insert(string word)
@@ -17,12 +22,12 @@ class Trie {
 
         for (const auto ch : word) {
 
-            const auto idx = getChildrenIndex(ch);
+            const auto idx = static_cast<int>(ch) - static_cast<int>('a');
 
             if (node->m_children[idx] == nullptr) {
                 node->m_children[idx] = new Trie{};
             }
-
+            
             node = node->m_children[idx];
         }
 
@@ -35,7 +40,7 @@ class Trie {
 
         for (const auto ch : word) {
 
-            const auto idx = getChildrenIndex(ch);
+            const auto idx = static_cast<int>(ch) - static_cast<int>('a');
 
             if (node->m_children[idx] == nullptr) {
                 return false;
@@ -53,7 +58,7 @@ class Trie {
 
         for (const auto ch : prefix) {
 
-            const auto idx = getChildrenIndex(ch);
+            const auto idx = static_cast<int>(ch) - static_cast<int>('a');
 
             if (node->m_children[idx] == nullptr) {
                 return false;
@@ -75,104 +80,12 @@ class Trie {
 
         delete[] m_children;
     }
-
-    std::size_t getChildrenIndex(const char ch)
-    {
-        return ch - 'a';
-    }
 };
-//*/
 
-
-/*
-// Approach 2 : Recursion
-class Trie {
-  private:
-    static constexpr int s_num_children = static_cast<int>('z') - static_cast<int>('a') + 1;
-    Trie** m_children;
-    bool m_is_done;
-
-  public:
-    Trie() : m_children{ new Trie* [s_num_children] {} }, m_is_done{ false }
-    {
-    }
-
-    void insertHelper(const std::string& word, const std::size_t idx_word)
-    {
-        if (idx_word == word.size()) {
-            m_is_done = true;
-
-            return;
-        }
-
-        const auto idx_children = getChildrenIndex(word[idx_word]);
-
-        if (m_children[idx_children] == nullptr) {
-            m_children[idx_children] = new Trie{};
-        }
-
-        m_children[idx_children]->insertHelper(word, idx_word + 1);
-    }
-
-    void insert(string word)
-    {
-        insertHelper(word, 0);
-    }
-
-    bool searchHelper(const std::string& word, const std::size_t idx_word)
-    {
-        if (idx_word == word.size()) {
-            return m_is_done;
-        }
-
-        const auto idx_children = getChildrenIndex(word[idx_word]);
-
-        if (m_children[idx_children] == nullptr) {
-            return false;
-        }
-
-        return m_children[idx_children]->searchHelper(word, idx_word + 1);
-    }
-
-    bool search(string word)
-    {
-        return searchHelper(word, 0);
-    }
-
-    bool startsWithHelper(const std::string& word, const std::size_t idx_word)
-    {
-        if (idx_word == word.size()) {
-            return true;
-        }
-
-        const auto idx_children = getChildrenIndex(word[idx_word]);
-
-        if (m_children[idx_children] == nullptr) {
-            return false;
-        }
-
-        return m_children[idx_children]->startsWithHelper(word, idx_word + 1);
-    }
-
-    bool startsWith(string prefix)
-    {
-        return startsWithHelper(prefix, 0);
-    }
-
-    ~Trie()
-    {
-        for (auto i = 0; i != s_num_children; ++i) {
-            if (m_children[i] != nullptr) {
-                m_children[i]->~Trie();
-            }
-        }
-
-        delete[] m_children;
-    }
-    
-    std::size_t getChildrenIndex(const char ch)
-    {
-        return ch - 'a';
-    }
-};
-*/
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
