59359dd530f027b33c8fb18a688a1b56a78bb0a8
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-11-02 23:14:06 +0900

    Time: 96 ms (56.18%), Space: 112.2 MB (25.72%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,3 +1,5 @@
+/*
+// Approach 1: Linear Scan - Generalized (using LIS)
 class Solution {
   public:
     bool increasingTriplet(vector<int>& nums)
@@ -18,6 +20,38 @@ class Solution {
             else {
                 *std::lower_bound(subs.begin(), subs.end(), num) = num;
             }
+        }
+
+        return false;
+    }
+};
+*/
+
+
+class Solution {
+  public:
+    bool increasingTriplet(vector<int>& nums)
+    {
+        auto subs = std::vector<int>{ nums.front() };
+        subs.reserve(2);
+
+        for (const auto num : nums) {
+
+            if (subs.back() < num) {
+                if (subs.size() == 2) {
+                    return true;
+                }
+                else {
+                    subs.push_back(num);
+                }
+            }
+            else if (subs.front() < num) {
+                subs.back() = num;
+            }
+            else {
+                subs.front() = num;
+            }
+
         }
 
         return false;
