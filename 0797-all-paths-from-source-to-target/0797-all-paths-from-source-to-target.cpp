3fc5f7da1373c8fe39ee3b32e64137ebf0620b92
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-11-06 11:22:53 +0900

    Time: 32 ms (11.02%), Space: 21.2 MB (8.03%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,3 +1,5 @@
+
+/*
 class Solution {
   private:
     void allPathsSourceTargetHelper(const std::vector<std::vector<int>>& graph, const int node, std::vector<int>& path,
@@ -11,6 +13,7 @@ class Solution {
         }
 
         for (const auto neighbor : graph[node]) {
+            
             path.push_back(neighbor);
             allPathsSourceTargetHelper(graph, neighbor, path, paths);
             path.pop_back();
@@ -26,5 +29,56 @@ class Solution {
         allPathsSourceTargetHelper(graph, 0, path, paths);
 
         return paths;
+    }
+};
+*/
+
+
+
+
+class Solution {
+  private:
+    void dp(const std::vector<std::vector<int>>& graph, const int node,
+            std::unordered_map<int, std::vector<std::vector<int>>>& source2paths)
+    {
+        if (source2paths.find(node) != source2paths.end()) {
+
+            return;
+        }
+
+        if (node == graph.size() - 1) {
+
+            source2paths[node] = std::vector<std::vector<int>>{ { static_cast<int>(graph.size() - 1) } };
+
+            return;
+        }
+
+        auto union_neighbors = std::vector<std::vector<int>>();
+        for (const auto neighbor : graph[node]) {
+
+            dp(graph, neighbor, source2paths);
+
+            union_neighbors.reserve(union_neighbors.size() + source2paths[neighbor].size());
+            for (const auto& paths : source2paths[neighbor]) {
+
+                auto path_to_insert = std::vector<int>(paths.size() + 1);
+                path_to_insert.front() = node;
+                memcpy(path_to_insert.data() + 1, paths.data(), sizeof(int) * paths.size());
+                
+                union_neighbors.push_back(path_to_insert);
+            }
+        }
+
+        source2paths[node] = union_neighbors;
+    }
+
+  public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph)
+    {
+        std::unordered_map<int, std::vector<std::vector<int>>> source2paths;
+
+        dp(graph, 0, source2paths);
+
+        return source2paths[0];
     }
 };
