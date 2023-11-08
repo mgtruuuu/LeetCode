bca5be7a32831415ca57dc84205beb40c8881405
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-11-08 15:04:07 +0900

    Time: 4 ms (43.45%), Space: 7.8 MB (49.16%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -8,6 +8,9 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+
+///*
 class Solution {
   public:
     ListNode* swapPairs(ListNode* head)
@@ -18,26 +21,41 @@ class Solution {
 
         auto* res = head->next;
 
-        ListNode* prev = new ListNode{};
-        ListNode* to_delete = prev;
+        auto dummy = ListNode{};
+        auto* prev = &dummy;
+
         auto* curr = head;
-
         while (curr != nullptr && curr->next != nullptr) {
-
-            // prev->next = prev->next;
-            // prev = curr;
 
             prev->next = curr->next;
             prev = curr;
 
-            auto* temp = curr->next->next;
-            curr->next->next = curr;
-            curr = curr->next;
-            curr->next->next = temp;
-            curr = temp;
+            curr = curr->next->next;
+            prev->next->next = prev;
+            prev->next = curr;
         }
 
-        delete to_delete;
         return res;
     }
 };
+//*/
+
+
+/*
+class Solution {
+  public:
+    ListNode* swapPairs(ListNode* head)
+    {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+
+        auto* temp = head->next;
+
+        head->next = swapPairs(head->next->next);
+
+        temp->next = head;
+        return temp;
+    }
+};
+*/
