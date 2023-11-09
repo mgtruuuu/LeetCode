dbf40cfe822d4119fea0342807476cacb0eca469
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-11-09 16:34:23 +0900

    Time: 4 ms, Memory: 8.7 MB - LeetHub

---
+++ reverse-linked-list/reverse-linked-list.cpp
@@ -0,0 +1,93 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+
+///*
+// Approach 2-1: Recursive
+class Solution {
+  public:
+    ListNode* reverseList(ListNode* head)
+    {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+
+        auto* end_node = reverseList(head->next);
+
+        head->next->next = head;
+        head->next = nullptr;
+
+        return end_node;
+    }
+};
+//*/
+
+
+/*
+// Approach 2-2: Recursive (My sol)
+class Solution {
+  private:
+    ListNode* kkk;
+    ListNode* reverseListHelper(ListNode* head)
+    {
+        if (head->next == nullptr) {
+
+            kkk = head;
+
+            return head;
+        }
+
+        auto* node = reverseListHelper(head->next);
+
+        node->next = head;
+
+        head->next = nullptr;
+
+        return head;
+    }
+
+  public:
+    ListNode* reverseList(ListNode* head)
+    {
+        if (head == nullptr) {
+            return head;
+        }
+
+        reverseListHelper(head);
+
+        return kkk;
+    }
+};
+*/
+
+
+/*
+// Approach 1: Iterative
+class Solution {
+  public:
+    ListNode* reverseList(ListNode* head)
+    {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+
+        while (curr != nullptr) {
+
+            auto* temp = curr;
+            curr = curr->next;
+
+            temp->next = prev;
+            prev = temp;
+        }
+
+        return prev;
+    }
+};
+*/
