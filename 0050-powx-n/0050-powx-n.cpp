793810660ad00f046ffe33176031fcde59d670fb
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-11-10 22:20:32 +0900

    Time: 3 ms (41.81%), Space: 6.3 MB (88.71%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,68 +1,41 @@
 class Solution {
+  private:
+    double myPowHelper(double x, int n)
+    {
+        if (n == 0) {
+            return 1.0;
+        }
+
+        if (n & 1) {
+            return x * myPowHelper(x * x, n >> 1);
+        }
+        else {
+            return myPowHelper(x * x, n >> 1);
+        }
+    }
+
   public:
     double myPow(double x, int n)
     {
-        // Base case, to stop recursive calls.
+        if (x == 0.0 || x == 1.0) {
+            return x;
+        }
+
         if (n == 0) {
-            return 1;
+            return 1.0;
         }
 
-        // Handle case where, n < 0.
-        if (n == std::numeric_limits<int>::min()) {
-            return 1.0 / myPow(x * x, static_cast<int>(static_cast<uint32_t>(n) >> 1));
+        if (n == INT_MIN) {
+
+            x = x * x;
+            n >>= 1;  // the same as n /= 2;
         }
-        
+
         if (n < 0) {
-            n = static_cast<int>((n ^ 0xFFFFFFFF) + 1);
             x = 1.0 / x;
+            n = (n ^ 0xFFFFFFFF) + 1; // the same as -n;
         }
 
-        // Perform Binary Exponentiation.
-        auto result = 1.0;
-        while (n != 0) {
-
-            // If 'n' is odd we multiply result with 'x' and reduce 'n' by '1'.
-            if (n & 1) {
-                result *= x;
-                // n -= 1;
-            }
-
-            // We square 'x' and reduce 'n' by half, x^n => (x^2)^(n/2).
-            x *= x;
-            n >>= 1;
-        }
-
-        return result;
+        return myPowHelper(x, n);
     }
 };
-
-
-
-// // class Solution {
-// //   public:
-// //     double myPow(double x, int n)
-// //     {
-// //         // Base case, to stop recursive calls.
-// //         if (n == 0) {
-// //             return 1;
-// //         }
-
-// //         // Handle case where, n < 0.
-// //         if (n == std::numeric_limits<int>::min()) {
-// //             return 1.0 / myPow(x * x, static_cast<int>(static_cast<uint32_t>(n) >> 1));
-// //         }
-// //         else if (n < 0) {
-// //             return 1.0 / myPow(x, static_cast<int>((n ^ 0xFFFFFFFF) + 1));
-// //         }
-
-// //         // Perform Binary Exponentiation.
-// //         // If 'n' is odd we perform Binary Exponentiation on 'n - 1' and multiply result with 'x'.
-// //         if (n & 1) {
-// //             return x * myPow(x * x, (n - 1) >> 1);
-// //         }
-// //         // Otherwise we calculate result by performing Binary Exponentiation on 'n'.
-// //         else {
-// //             return myPow(x * x, n >> 1);
-// //         }
-// //     }
-// // };
