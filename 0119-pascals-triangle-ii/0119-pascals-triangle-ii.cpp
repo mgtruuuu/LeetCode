c08653b72a6779df3814e484c4ae46a7768e8161
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-11-10 10:04:44 +0900

    Time: 0 ms (100%), Space: 6.9 MB (47.18%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,3 +1,41 @@
+///*
+// Approach 1: Brute Force Recursion
+class Solution {
+  public:
+    void dp(const int row, const int col, std::vector<std::vector<int>>& matrix)
+    {
+        if (matrix[row][col] != -1) {
+            return;
+        }
+
+        if (row == 0 || col == 0 || row == col) {
+
+            matrix[row][col] = 1;
+            return;
+        }
+
+        dp(row - 1, col - 1, matrix);
+        dp(row - 1, col, matrix);
+        matrix[row][col] = matrix[row - 1][col - 1] + matrix[row - 1][col];
+    }
+
+    vector<int> getRow(int rowIndex)
+    {
+        auto matrix = std::vector<std::vector<int>>(rowIndex + 1, std::vector<int>(rowIndex + 1, -1));
+        vector<int> row;
+
+        for (int i = 0; i <= rowIndex; i++) {
+            dp(rowIndex, i, matrix);
+        }
+
+        return matrix.back();
+    }
+};
+//*/
+
+
+/*
+// Approach 2: Memory-efficient Dynamic Programming
 class Solution {
   public:
     vector<int> getRow(int rowIndex)
@@ -13,3 +51,4 @@ class Solution {
         return row;
     }
 };
+*/
