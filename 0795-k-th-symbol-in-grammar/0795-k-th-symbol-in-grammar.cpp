7dfbcceb270a3f20162864a1a4d842d4ecac249c
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-11-13 16:09:49 +0900

    Time: 0 ms (100%), Space: 6.3 MB (79.84%) - LeetHub

--- 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.cpp
+++ 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.cpp
@@ -2,10 +2,24 @@ class Solution {
   public:
     int kthGrammar(int n, int k)
     {
-        if (n == 1) {
-            return 0;
+        auto left = 1;
+        auto right = static_cast<int>(pow(2, n - 1));
+
+        auto init = 0;
+        while (--n != 0) {
+
+            const auto middle = (left + right) / 2;
+
+            if (k <= middle) {
+                right = middle;
+            }
+            else {
+
+                init = 1 - init;
+                left = middle + 1;
+            }
         }
 
-        return k & 1 ? kthGrammar(n - 1, (k + 1) / 2) : 1 - kthGrammar(n - 1, (k + 1) / 2);
+        return init;
     }
 };
