8f29df9f35dbecc7654a50d70a6188dacbe44e7c
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-11-16 08:43:45 +0900

    Time: 15 ms (12.73%), Space: 10.9 MB (14.54%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -0,0 +1,66 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+
+///*
+// Approach 1: Hash Table
+class Solution {
+  public:
+    bool hasCycle(ListNode* head)
+    {
+        std::unordered_set<ListNode*> nodes;
+
+        while (head != nullptr) {
+
+            if (nodes.find(head) != nodes.end()) {
+                return true;
+            }
+
+            nodes.insert(head);
+
+            head = head->next;
+        }
+
+        return false;
+    }
+};
+//*/
+
+
+/*
+// Approach 2: Floyd's Cycle Finding Algorithm
+class Solution {
+  public:
+    bool hasCycle(ListNode* head)
+    {
+        if (head == nullptr) {
+            return false;
+        }
+
+        const auto* fast = head;
+        const auto* slow = head;
+
+        constexpr auto n_fast = 2;
+
+        do {
+            for (auto i = 0; i != n_fast; ++i) {
+                fast = fast->next;
+                if (fast == nullptr) {
+                    return false;
+                }
+            }
+
+            slow = slow->next;
+
+        } while (fast != slow);
+
+        return true;
+    }
+};
+*/
