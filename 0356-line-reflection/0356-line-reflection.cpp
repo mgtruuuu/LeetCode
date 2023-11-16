34c4fd0aa93e64999d683027e4bdc72256808416
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-11-16 09:15:14 +0900

    Time: 24 ms (68.42%), Space: 15.7 MB (64.59%) - LeetHub

---
+++ 0356-line-reflection/0356-line-reflection.cpp
@@ -0,0 +1,45 @@
+class Solution {
+  private:
+    int64_t get1DIndex(const std::vector<int>& point)
+    {
+        return point.front() + int64_t(1'000'000'000) * point.back();
+    }
+
+  public:
+    bool isReflected(vector<vector<int>>& points)
+    {
+        std::unordered_map<int64_t, int> point2count;
+
+        auto min = INT_MAX;
+        auto max = INT_MIN;
+        for (const auto& point : points) {
+
+            ++point2count[get1DIndex(point)];
+
+            if (min > point.front()) {
+                min = point.front();
+            }
+
+            if (max < point.front()) {
+                max = point.front();
+            }
+        }
+
+        const auto x_middle_twice = (min + max);
+
+        for (const auto& point : points) {
+
+            if (point.front() < (x_middle_twice + 1) / 2 &&
+                point2count.find(get1DIndex({ x_middle_twice - point.front(), point.back() })) == point2count.end()) {
+                return false;
+            }
+
+            if (point.front() > x_middle_twice / 2 &&
+                point2count.find(get1DIndex({ x_middle_twice - point.front(), point.back() })) == point2count.end()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
