3db5494075cf4335e50918813339c99a6a2f9ffb
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-11-17 12:40:18 +0900

    Time: 0 ms (100.00%), Space: 15.1 MB (80.99%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -1,14 +1,5 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-
+/*
+// Approach 1: Recursion
 class Solution {
   public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
@@ -20,23 +11,7 @@ class Solution {
             return list1;
         }
 
-
-        ListNode** ppLeftArgs = &list1;
-        ListNode** ppRightArgs = &list2;
-        ListNode** temp;
-        if (list1->val < list2->val) {
-            temp = ppLeftArgs;
-            ppLeftArgs = &list1->next;
-        }
-        else {
-            temp = ppRightArgs;
-            ppRightArgs = &list2->next;
-        }
-
-        (*temp)->next = mergeTwoLists(*ppLeftArgs, *ppRightArgs);
-        return *temp;
-
-
+        // method 1
         if (list1->val < list2->val) {
             list1->next = mergeTwoLists(list1->next, list2);
             return list1;
@@ -45,5 +20,53 @@ class Solution {
             list2->next = mergeTwoLists(list1, list2->next);
             return list2;
         }
+
+
+        // method 2
+        ListNode** ppLeftArgs = &list1;
+        ListNode** ppRightArgs = &list2;
+        ListNode** ppReturn;
+        if (list1->val < list2->val) {
+            ppReturn = ppLeftArgs;
+            ppLeftArgs = &list1->next;
+        }
+        else {
+            ppReturn = ppRightArgs;
+            ppRightArgs = &list2->next;
+        }
+
+        (*ppReturn)->next = mergeTwoLists(*ppLeftArgs, *ppRightArgs);
+        return *ppReturn;
+
     }
 };
+*/
+    
+///*
+// Approach 2: Iteration
+class Solution {
+  public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
+    {
+        ListNode* pMerged;
+        ListNode** ppNode = &pMerged;
+        while (list1 != nullptr && list2 != nullptr) {
+
+            if (list1->val < list2->val) {
+                *ppNode = list1;
+                list1 = list1->next;
+            }
+            else {
+                *ppNode = list2;
+                list2 = list2->next;
+            }
+
+            ppNode = &(*ppNode)->next;
+        }
+
+        list1 == nullptr ? * ppNode = list2 : * ppNode = list1;
+
+        return pMerged;
+    }
+};
+//*/
