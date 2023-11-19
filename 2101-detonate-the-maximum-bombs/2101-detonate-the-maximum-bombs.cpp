24fb6fef5ac7fa3b3ada7d29500bbc2287c12ae8
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-11-19 14:46:00 +0900

    Time: 112 ms (68.56%), Space: 15.2 MB (79.51%) - LeetHub

---
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
@@ -0,0 +1,59 @@
+class Solution {
+  private:
+    bool isConnected(const std::vector<int>& from, const std::vector<int>& to)
+    {
+        const auto delta_x = int64_t(from[0] - to[0]);
+        const auto delta_y = int64_t(from[1] - to[1]);
+
+        return delta_x * delta_x + delta_y * delta_y <= int64_t(from[2]) * from[2];
+    }
+
+    int dfs(const std::vector<std::vector<int>>& list_adj, const int node, std::vector<bool>& visited)
+    {
+        visited[node] = true;
+
+        auto ret = 1;
+        for (const auto& neighbor : list_adj[node]) {
+            
+            if (visited[neighbor] == false) {
+                ret += dfs(list_adj, neighbor, visited);    
+            }
+        }
+
+        return ret;
+    }
+
+  public:
+    int maximumDetonation(vector<vector<int>>& bombs)
+    {
+        const auto len_bombs = static_cast<int>(bombs.size());
+
+        std::vector<std::vector<int>> list_adj(len_bombs);
+
+        for (auto i = 0; i != len_bombs; ++i) {
+            for (auto j = 0; j != len_bombs; ++j) {
+
+                if (i == j) {
+                    continue;
+                }
+
+                if (isConnected(bombs[i], bombs[j]) == true) {
+                    list_adj[i].push_back(j);
+                }
+            }
+        }
+
+        auto maximum_detonation = INT_MIN;
+        for (auto i = 0; i != len_bombs; ++i) {
+
+            std::vector<bool> visited(len_bombs, false);
+            
+            const auto temp = dfs(list_adj, i, visited);
+            if (maximum_detonation < temp) {
+                maximum_detonation = temp;
+            }
+        }
+
+        return maximum_detonation;
+    }
+};
