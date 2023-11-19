68e707e53771a4699b5954aae594a07daf64c85d
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-11-19 09:38:33 +0900

    Time: 0 ms (100.00%), Space: 8 MB (72.65%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.cpp
+++ 0836-rectangle-overlap/0836-rectangle-overlap.cpp
@@ -1,16 +1,18 @@
 class Solution {
-public:
-    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
-        // check if either rectangle is actually a line
-        if (rec1[0] == rec1[2] || rec1[1] == rec1[3] ||
-            rec2[0] == rec2[2] || rec2[1] == rec2[3]) {
-            // the line cannot have positive overlap
-            return false;
-        }
+  public:
+    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2)
+    {
+        const auto left_bottom_max_x = std::max(rec1[0], rec2[0]);
+        const auto left_bottom_max_y = std::max(rec1[1], rec2[1]);
+        const auto right_up_min_x = std::min(rec1[2], rec2[2]);
+        const auto right_up_min_y = std::min(rec1[3], rec2[3]);
 
-        return !(rec1[2] <= rec2[0] ||   // left
-                 rec1[3] <= rec2[1] ||   // bottom
-                 rec1[0] >= rec2[2] ||   // right
-                 rec1[1] >= rec2[3]);    // top
+        return left_bottom_max_x < right_up_min_x && left_bottom_max_y < right_up_min_y ? true : false;
+
+        if ((rec1[0] < rec2[0] && rec2[0] < rec1[2] && rec1[1] < rec2[1] && rec2[1] < rec1[3]) ||
+            (rec2[0] < rec1[0] && rec1[0] < rec2[2] && rec2[1] < rec1[1] && rec1[1] < rec2[3])) {
+            return true;
+        }
+        return false;
     }
 };
