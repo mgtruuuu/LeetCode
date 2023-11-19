61471ab8ecd8fd351b8678e36e41f09c30bb04e1
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-11-19 20:34:34 +0900

    Time: 2 ms (51.64%), Space: 6.3 MB (79.80%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -0,0 +1,48 @@
+/*
+// Approach 1: BST ??
+class Solution {
+  public:
+    int kthGrammar(int n, int k)
+    {
+        auto left = 1;
+        auto right = static_cast<int>(pow(2, n - 1));
+
+        auto init = 0;
+        while (--n != 0) {
+
+            const auto middle = (left + right) / 2;
+
+            if (k <= middle) {
+                right = middle;
+            }
+            else {
+
+                init = 1 - init;
+                left = middle + 1;
+            }
+        }
+
+        return init;
+    }
+};
+*/
+
+
+
+///*
+// Approach 2: Normal Recursion
+class Solution {
+  public:
+    int kthGrammar(int n, int k)
+    {
+        if (n == 1) {
+            return 0;
+        }
+
+        return k & 1 ? kthGrammar(n - 1, (k + 1) / 2) : 1 - kthGrammar(n - 1, (k + 1) / 2);
+    }
+};
+//*/
+
+
+// others...
