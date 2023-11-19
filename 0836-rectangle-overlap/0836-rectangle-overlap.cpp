eaed8ec8548013234b36eeafc0ca5d35b0e1cef9
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-11-19 09:52:15 +0900

    Time: 3 ms (39.35%), Space: 8.1 MB (34.71%) - LeetHub

---
+++ 0836-rectangle-overlap/0836-rectangle-overlap.cpp
@@ -0,0 +1,28 @@
+/*
+// Approach #1: Check Area
+class Solution {
+  public:
+    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2)
+    {
+        const auto left_bottom_max_x = std::max(rec1[0], rec2[0]);
+        const auto left_bottom_max_y = std::max(rec1[1], rec2[1]);
+        const auto right_up_min_x = std::min(rec1[2], rec2[2]);
+        const auto right_up_min_y = std::min(rec1[3], rec2[3]);
+
+        return left_bottom_max_x < right_up_min_x && left_bottom_max_y < right_up_min_y ? true : false;
+    }
+};
+*/
+
+
+
+///*
+// Approach #2: Check Position
+class Solution {
+  public:
+    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2)
+    {
+        return rec1[3] <= rec2[1] || rec2[3] <= rec1[1] || rec1[2] <= rec2[0] || rec2[2] <= rec1[0] ? false : true;
+    }
+};
+//*/
