33a38e2500210d97f7a2c5fa8d14d92ab84e603d
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-11-20 17:52:42 +0900

    Time: 6 ms (42.33%), Space: 15.1 MB (81.15%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -1,72 +1,34 @@
-/*
-// Approach 1: Recursion
 class Solution {
   public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
     {
-        if (list1 == nullptr) {
-            return list2;
-        }
-        else if (list2 == nullptr) {
-            return list1;
-        }
+        ListNode* pNode;
+        ListNode** ppNode = &pNode;
 
-        // method 1
-        if (list1->val < list2->val) {
-            list1->next = mergeTwoLists(list1->next, list2);
-            return list1;
-        }
-        else {
-            list2->next = mergeTwoLists(list1, list2->next);
-            return list2;
-        }
-
-
-        // method 2
-        ListNode** ppLeftArgs = &list1;
-        ListNode** ppRightArgs = &list2;
-        ListNode** ppReturn;
-        if (list1->val < list2->val) {
-            ppReturn = ppLeftArgs;
-            ppLeftArgs = &list1->next;
-        }
-        else {
-            ppReturn = ppRightArgs;
-            ppRightArgs = &list2->next;
-        }
-
-        (*ppReturn)->next = mergeTwoLists(*ppLeftArgs, *ppRightArgs);
-        return *ppReturn;
-
-    }
-};
-*/
-    
-///*
-// Approach 2: Iteration
-class Solution {
-  public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
-    {
-        ListNode* pMerged;
-        ListNode** ppNode = &pMerged;
         while (list1 != nullptr && list2 != nullptr) {
 
             if (list1->val < list2->val) {
                 *ppNode = list1;
                 list1 = list1->next;
+
+                ppNode = &(**ppNode).next;
             }
             else {
                 *ppNode = list2;
                 list2 = list2->next;
-            }
 
-            ppNode = &(*ppNode)->next;
+                ppNode = &(**ppNode).next;
+            }
         }
 
-        list1 == nullptr ? * ppNode = list2 : * ppNode = list1;
+        if (list1 == nullptr) {
+            *ppNode = list2;
+        }
 
-        return pMerged;
+        if (list2 == nullptr) {
+            *ppNode = list1;
+        }
+
+        return pNode;
     }
 };
-//*/
