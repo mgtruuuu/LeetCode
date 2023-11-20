a17ef0b6aa88955ac2707a362b503284c3ea9232
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-11-20 18:39:31 +0900

    Time: 11 ms (39.43%), Space: 21.9 MB (84.79%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,17 +1,3 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-
-///*
-// Approach 1: Recursive Traversal with Valid Range
 class Solution {
   private:
     bool isValidBSTHelper(TreeNode* node, TreeNode* lower_limit = nullptr, TreeNode* upper_limit = nullptr)
@@ -20,18 +6,11 @@ class Solution {
             return true;
         }
 
-        // if (node->left != nullptr && node->left->val >= node->val) {
-        //     return false;
-        // }
-        // if (node->right != nullptr && node->right->val <= node->val) {
-        //     return false;
-        // }
-
         if (lower_limit != nullptr && lower_limit->val >= node->val) {
             return false;
         }
 
-        if (upper_limit != nullptr && upper_limit->val <= node->val) {
+        if (upper_limit != nullptr && node->val >= upper_limit->val) {
             return false;
         }
 
@@ -44,13 +23,3 @@ class Solution {
         return isValidBSTHelper(root);
     }
 };
-//*/
-
-
-// Approach 2: Iterative Traversal with Valid Range
-
-
-// Approach 3: Recursive Inorder Traversal
-
-
-// Approach 4: Iterative Inorder Traversal
