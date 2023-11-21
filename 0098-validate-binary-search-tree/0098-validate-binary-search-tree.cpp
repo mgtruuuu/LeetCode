aca59932f0af8d7edc991a1f8e1681daea0310cd
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-11-21 09:46:31 +0900

    Time: 8 ms (68.47%), Space: 22.6 MB (6.98%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -28,6 +28,7 @@ class Solution {
 */
 
 
+/*
 // Approach 2: Iterative Traversal with Valid Range
 class Solution {
   public:
@@ -56,6 +57,62 @@ class Solution {
 
             s.push({node[0]->right, node[0], node[2]});
             s.push({node[0]->left, node[1], node[0]});
+        }
+
+        return true;
+    }
+};
+*/
+
+
+
+// Approach 3: Recursive Inorder Traversal
+class Solution {
+  private:
+    std::stack<TreeNode*> s;
+    // bool isValidBSTHelper(TreeNode* node, TreeNode* lower_limit = nullptr, TreeNode* upper_limit = nullptr)
+    // {
+    //     if (node == nullptr) {
+    //         return true;
+    //     }
+
+    //     if (lower_limit != nullptr && lower_limit->val >= node->val) {
+    //         return false;
+    //     }
+
+    //     if (upper_limit != nullptr && node->val >= upper_limit->val) {
+    //         return false;
+    //     }
+
+    //     return isValidBSTHelper(node->left, lower_limit, node) && isValidBSTHelper(node->right, node, upper_limit);
+    // }
+
+  public:
+    bool isValidBST(TreeNode* root)
+    {
+        // return isValidBSTHelper(root);
+        if (root == nullptr) {
+            return true;
+        }
+
+        if (isValidBST(root->left) == false) {
+            return false;
+        }
+
+        if (s.empty() == true) {
+            s.push(root);
+        }
+        else {
+            if (s.top()->val < root->val) {
+                s.push(root);
+            }
+            else {
+                return false;
+            }
+        }
+
+        if (isValidBST(root->right) == false) {
+            return false;
         }
 
         return true;
