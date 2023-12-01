94931d04bb6fe5de9cfbe0ed1466be2e5818930f
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-12-01 22:37:49 +0900

    Time: 0 ms (100.00%), Space: 6.3 MB (35.02%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -1,6 +1,8 @@
 // The API isBadVersion is defined for you.
 // bool isBadVersion(int version);
 
+
+/*
 class Solution {
   public:
     int firstBadVersion(int n)
@@ -24,3 +26,28 @@ class Solution {
         return idx_start;
     }
 };
+*/
+
+
+class Solution {
+  public:
+    int firstBadVersion(int n)
+    {
+        auto idx_left = 1;
+        auto idx_right = n;
+        while (idx_left < idx_right) {
+
+            const auto mid = idx_left + (idx_right - idx_left) / 2;
+
+            if (isBadVersion(mid) == true) {
+                idx_right = mid;
+            }
+            else {
+                idx_left = mid + 1;
+            }
+        }
+
+        return idx_left;
+    }
+};
+
