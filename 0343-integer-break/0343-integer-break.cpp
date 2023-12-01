595c4c57d6812619f00598c2aeebd2f8e7874c9f
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-12-01 20:31:45 +0900

    Time: 0 ms (100.00%), Space: 6.6 MB (19.60%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,31 +1,28 @@
 class Solution {
-public:
-    vector<int> memo;
-    
-    int integerBreak(int n) {
+  public:
+    int integerBreak(int n)
+    {
         if (n <= 3) {
             return n - 1;
         }
-        
-        memo = vector(n + 1, 0);
-        return dp(n);
-    }
-    
-    int dp(int num) {
-        if (num <= 3) {
-            return num;
+
+        std::vector<int> memo(n + 1, 0);
+        memo[1] = 1;
+        memo[2] = 2;
+        memo[3] = 3;
+
+        for (auto num = 4; num <= n; ++num) {
+
+            for (auto i = 1; i <= num - 1; ++i) {
+
+                if (memo[num] < i * memo[num - i]) {
+                    memo[num] = i * memo[num - i];
+                }
+            }
+
+            std::cout << "memo[" << num << "] : " << memo[num] << std::endl;
         }
-        
-        if (memo[num] != 0) {
-            return memo[num];
-        }
-        
-        int ans = num;
-        for (int i = 2; i < num; i++) {
-            ans = max(ans, i * dp(num - i));
-        }
-        
-        memo[num] = ans;
-        return ans;
+
+        return memo[n];
     }
 };
