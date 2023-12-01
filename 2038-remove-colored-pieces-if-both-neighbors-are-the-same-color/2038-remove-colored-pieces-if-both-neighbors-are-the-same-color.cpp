d93eb9cc0320f08c7584bae51608baa497e1d504
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-12-01 22:13:23 +0900

    Time: 38 ms (32.15%), Space: 13.8 MB (34.69%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -1,3 +1,26 @@
+///*
+// Approach 1-1: Count
+class Solution {
+  public:
+    bool winnerOfGame(string colors)
+    {
+        auto res = 0;
+
+        for (auto i = 1; i < static_cast<int>(colors.size()) - 1; ++i) {
+            if (colors[i - 1] == colors[i] && colors[i] == colors[i + 1]) {
+                colors[i] == 'A' ? ++res : --res;
+            }
+        }
+
+        return res > 0;
+    }
+};
+//*/
+
+
+
+/*
+// Approach 1-2: Count (My sol)
 class Solution {
   public:
     bool winnerOfGame(string colors)
@@ -6,6 +29,7 @@ class Solution {
 
         auto count = 0;
         auto prev = colors.front() == 'A' ? 'B' : 'A';
+
         for (const auto color : colors) {
 
             if (prev == color) {
@@ -18,7 +42,7 @@ class Solution {
             if (count > 2) {
 
                 count -= 2;
-                
+
                 if (prev == 'A') {
                     res += count;
                 }
@@ -42,10 +66,8 @@ class Solution {
                 res -= count;
             }
         }
-        
-        std::cout << res << std::endl;
 
-        
         return res > 0;
     }
 };
+*/
