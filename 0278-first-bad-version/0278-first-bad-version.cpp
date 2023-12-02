0b74e46e181ef74ed4ba72c05d000b681b9f3184
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-12-02 10:21:53 +0900

    Time: 0 ms (100.00%), Space: 6.2 MB (75.80%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -7,23 +7,22 @@ class Solution {
   public:
     int firstBadVersion(int n)
     {
-        auto idx_start = 1;
-        auto idx_end = n;
-        while (idx_start <= idx_end) {
+        auto idx_left = 1;
+        auto idx_right = n;
+        while (idx_left <= idx_right) {
 
-            const auto idx_middle = idx_start + (idx_end - idx_start) / 2;
-
+            const auto idx_middle = idx_left + (idx_right - idx_left) / 2;
             const auto temp = isBadVersion(idx_middle);
 
             if (temp == true) {
-                idx_end = idx_middle - 1;
+                idx_right = idx_middle - 1;
             }
             else {
-                idx_start = idx_middle + 1;
+                idx_left = idx_middle + 1;
             }
         }
 
-        return idx_start;
+        return idx_left;
     }
 };
 //*/
