1fcb1390a7560c534504fdbd57ef020dc73f71de
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-12-04 23:13:50 +0900

    Time: 7 ms (5.67%), Space: 7.5 MB (5.51%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -2,36 +2,47 @@ class Solution {
   public:
     string reorganizeString(string s)
     {
-        vector<int> charCounts(26, 0);
-        for (char c : s) {
-            charCounts[c - 'a']++;
+        constexpr auto len = 'z' - 'a' + 1;
+
+        int counts[len] = {
+            0,
+        };
+        for (const auto c : s) {
+            ++counts[c - 'a'];
         }
 
         // Max heap ordered by character counts
         priority_queue<vector<int>> pq;
-        for (int i = 0; i < 26; i++) {
-            if (charCounts[i] > 0) {
-                pq.push(vector<int>{ charCounts[i], i + 'a' });
+        for (auto i = 0; i != 26; ++i) {
+            if (counts[i] > 0) {
+                pq.push(vector<int>{ counts[i], i + 'a' });
             }
         }
 
-        string result;
-        while (!pq.empty()) {
+        std::string result;
+        while (pq.empty() == false) {
+
             auto first = pq.top();
             pq.pop();
-            if (result.empty() || first[1] != result.back()) {
+            if (result.empty() == true || first[1] != result.back()) {
+
                 result += char(first[1]);
+
                 if (--first[0] > 0) {
                     pq.push(first);
                 }
             }
             else {
+
                 if (pq.empty()) {
                     return "";
                 }
+
                 auto second = pq.top();
                 pq.pop();
+
                 result += char(second[1]);
+
                 if (--second[0] > 0) {
                     pq.push(second);
                 }
