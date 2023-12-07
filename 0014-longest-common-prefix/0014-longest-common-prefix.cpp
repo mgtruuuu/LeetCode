22014b2c8bdba74c5645d24194b7c6e5298168f1
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-12-07 10:19:52 +0900

    Time: 6 ms (33.76%), Space: 9.8 MB (24.49%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,24 +1,26 @@
+///*
+// Approach 1: Vertical scanning
 class Solution {
   public:
     string longestCommonPrefix(vector<string>& strs)
     {
         auto res = std::string{};
 
-        for (auto idx_zero = 0; idx_zero != strs.front().size(); ++idx_zero) {
+        const auto& str_front = strs.front();
+
+        for (auto idx_front = std::size_t(0); idx_front != str_front.size(); ++idx_front) {
+
             for (auto idx_strs = std::size_t(1); idx_strs != strs.size(); ++idx_strs) {
 
-                if (idx_zero == strs[idx_strs].size()) {
-                    return res;
-                }
-
-                if (strs.front()[idx_zero] != strs[idx_strs][idx_zero]) {
+                if (idx_front == strs[idx_strs].size() || str_front[idx_front] != strs[idx_strs][idx_front]) {
                     return res;
                 }
             }
 
-            res.push_back(strs.front()[idx_zero]);
+            res.push_back(str_front[idx_front]);
         }
 
         return res;
     }
 };
+//*/
