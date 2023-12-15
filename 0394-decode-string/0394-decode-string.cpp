5e17104741501af8862691435895b1084c9e0e5b
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-12-15 14:53:33 +0800

    Time: 0 ms (100.00%), Space: 6.8 MB (74.25%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.cpp
@@ -0,0 +1,49 @@
+class Solution {
+  public:
+    string decodeString(string s)
+    {
+        std::stack<int> s_int;
+        std::stack<std::string> s_str;
+
+        s_int.push(1);
+        s_str.push(std::string());
+
+        std::string num;
+        num.reserve(3);
+        for (const auto ch : s) {
+
+            if (ch == '[') {
+
+                s_int.push(std::stoi(num));
+
+                num.clear();
+                num.reserve(3);
+
+                s_str.push(std::string());
+            }
+            else if (ch == ']') {
+
+                auto n = s_int.top();
+
+                std::string temp;
+                temp.reserve(n * s_str.top().size());
+                while (n-- != 0) {
+                    temp += s_str.top();
+                }
+
+                s_int.pop();
+                s_str.pop();
+                s_str.top() += temp;
+            }
+            else if (ch >= '0' && ch <= '9') {
+                num += ch;
+            }
+            else {
+                // 'a' ~ 'z'
+                s_str.top().push_back(ch);
+            }
+        }
+
+        return s_str.top();
+    }
+};
