50a8e5d1027f53fd90b2b50e032aa0114826031a
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-12-18 19:37:54 +0800

    Time: 12 ms (45.11%), Space: 18.4 MB (6.88%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -1,15 +1,24 @@
 class Solution {
-public:
-    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
-        unordered_map<int, int> groups;
-        for (int r = 0; r < matrix.size(); ++r) {
-            for (int c = 0; c < matrix[0].size(); ++c) {
-                if (groups.find(r-c) == groups.end())
-                    groups[r-c] = matrix[r][c];
-                else if (groups[r-c] != matrix[r][c])
+  public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix)
+    {
+        const auto num_rows = static_cast<int>(matrix.size());
+        const auto num_cols = static_cast<int>(matrix.front().size());
+
+        std::unordered_map<int, int> h;
+
+        for (auto r = 0; r != num_rows; ++r) {
+            for (auto c = 0; c != num_cols; ++c) {
+
+                if (h.find(r - c) == h.end()) {
+                    h[r - c] = matrix[r][c];
+                }
+                else if (h[r - c] != matrix[r][c]) {
                     return false;
+                }
             }
         }
+
         return true;
     }
 };
