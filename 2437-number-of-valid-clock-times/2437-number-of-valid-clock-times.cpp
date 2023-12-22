f32680cc04a8f2f0a74d2f8e7f9a01d1338b1ba9
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-12-22 22:26:13 +0800

    Time: 2 ms (50.75%), Space: 6.3 MB (36.79%) - LeetHub

--- 2437-number-of-valid-clock-times/2437-number-of-valid-clock-times.cpp
+++ 2437-number-of-valid-clock-times/2437-number-of-valid-clock-times.cpp
@@ -1,21 +1,41 @@
 class Solution {
   public:
-    int countTime(string time) {
-        int ans = 1;
-        if(time[4] == '?') ans = ans * 10;
-        if(time[3] == '?') ans = ans * 6;
-        
-        if(time[0] == '?' && time[1] == '?') ans = ans * 24;
-        else{
-            if(time[1] == '?'){ 
-                if(time[0] == '2' ) ans = ans * 4;
-                else ans = ans * 10;
+    int countTime(string time)
+    {
+        auto choices = 1;
+
+        int numbers[5] = { 3, 10, -1, 6, 10 };
+
+        const auto hh = time.substr(0, 2);
+        auto hh_reverse = hh;
+        std::reverse(hh_reverse.begin(), hh_reverse.end());
+
+        if (hh == "??") {
+            choices *= 24;
+        }
+        else if (hh[0] == '?') {
+
+            if (hh_reverse[0] - '0' > 3) {
+                numbers[0] -= 1;
             }
-            if(time[0] == '?'){
-                if(time[1] < '4') ans = ans * 3;
-                else ans = ans * 2;
+
+            choices *= numbers[0];
+        }
+        else if (hh[1] == '?') {
+
+            if (hh_reverse[1] - '0' == 2) {
+                numbers[1] -= 6;
+            }
+
+            choices *= numbers[1];
+        }
+
+        for (auto idx = 3; idx != 5; ++idx) {
+            if (time[idx] == '?') {
+                choices *= numbers[idx];
             }
         }
-        return ans;
+
+        return choices;
     }
 };
