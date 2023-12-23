999e8d49e9ea2329a29a2b9059e9692331694bb7
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2023-12-23 22:57:38 +0800

    Time: 14 ms (77.49%), Space: 25.8 MB (44.19%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
@@ -10,33 +10,33 @@
  * };
  */
 class Solution {
-public:
-    
+  public:
     vector<TreeNode*> res;
     set<int> dp;
-    
-    void util (TreeNode* &root)
+
+    void util(TreeNode*& root)
     {
-        if(root!=NULL)
-        {
+        if (root != nullptr) {
             util(root->left);
             util(root->right);
-            if(dp.find(root->val)!=dp.end())
-            {
-                if(root->left)  res.push_back(root->left);
-                if(root->right) res.push_back(root->right);
-                root=NULL;
-				delete root;
+            if (dp.find(root->val) != dp.end()) {
+                if (root->left)
+                    res.push_back(root->left);
+                if (root->right)
+                    res.push_back(root->right);
+                root = nullptr;
+                delete root;
             }
         }
     }
-    
-    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
-	
-        for(int i=0;i<to_delete.size();i++)
+
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete)
+    {
+
+        for (int i = 0; i < to_delete.size(); i++)
             dp.insert(to_delete[i]);
         util(root);
-        if(root)
+        if (root)
             res.push_back(root);
         return res;
     }
