90eee7f518520e21b48c101e10add53ff9f7c51c
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2024-01-01 11:12:24 +0800

    Time: 49 ms (7.67%), Space: 25.4 MB (86.80%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
@@ -9,35 +9,54 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-  public:
-    vector<TreeNode*> res;
-    set<int> dp;
 
-    void util(TreeNode*& root)
+
+class Solution {
+  private:
+    void delNodesHelper(TreeNode* node, std::unordered_set<int>& to_delete, std::vector<TreeNode*>& nodes)
     {
-        if (root != nullptr) {
-            util(root->left);
-            util(root->right);
-            if (dp.find(root->val) != dp.end()) {
-                if (root->left)
-                    res.push_back(root->left);
-                if (root->right)
-                    res.push_back(root->right);
-                root = nullptr;
-                delete root;
+        if (node == nullptr) {
+            return;
+        }
+
+        delNodesHelper(node->left, to_delete, nodes);
+        delNodesHelper(node->right, to_delete, nodes);
+
+        if (to_delete.find(node->val) != to_delete.end()) {
+
+            if (node->right != nullptr && to_delete.find(node->right->val) == to_delete.end()) {
+                nodes.push_back(node->right);
             }
+
+            if (node->left != nullptr && to_delete.find(node->left->val) == to_delete.end()) {
+                nodes.push_back(node->left);
+            }
+        }
+
+        if (node->right != nullptr && to_delete.find(node->right->val) != to_delete.end()) {
+            node->right = nullptr;
+        }
+
+        if (node->left != nullptr && to_delete.find(node->left->val) != to_delete.end()) {
+            node->left = nullptr;
         }
     }
 
+  public:
     vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete)
     {
+        std::unordered_set<int> dddd;
+        for (const auto i : to_delete) {
+            dddd.insert(i);
+        }
 
-        for (int i = 0; i < to_delete.size(); i++)
-            dp.insert(to_delete[i]);
-        util(root);
-        if (root)
-            res.push_back(root);
-        return res;
+        std::vector<TreeNode*> nodes;
+        delNodesHelper(root, dddd, nodes);
+
+        if (dddd.find(root->val) == dddd.end()) {
+            nodes.push_back(root);
+        }
+        
+        return nodes;
     }
 };
