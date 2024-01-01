8e90a9737edf8197e459e4e5e28a67cede5a7f9d
Mungap Kim <85836463+mgtruuuu@users.noreply.github.com>
2024-01-01 22:06:05 +0800

    Time: 38 ms (9.01%), Space: 25.7 MB (55.49%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
@@ -11,6 +11,7 @@
  */
 
 
+/*
 class Solution {
   private:
     void delNodesHelper(TreeNode* node, std::unordered_set<int>& to_delete, std::vector<TreeNode*>& nodes)
@@ -57,6 +58,55 @@ class Solution {
             nodes.push_back(root);
         }
         
+        return nodes;
+    }
+};
+*/
+
+
+class Solution {
+  private:
+    void delNodesHelper(TreeNode** pp_node, std::unordered_set<int>& to_delete, std::vector<TreeNode*>& nodes)
+    {
+        if (*pp_node == nullptr) {
+            return;
+        }
+
+        delNodesHelper(&(*pp_node)->left, to_delete, nodes);
+        delNodesHelper(&(*pp_node)->right, to_delete, nodes);
+
+        if (to_delete.find((*pp_node)->val) == to_delete.end()) {
+            return;
+        }
+        
+        if ((*pp_node)->right != nullptr) {
+            nodes.push_back((*pp_node)->right);
+        }
+
+        if ((*pp_node)->left != nullptr) {
+            nodes.push_back((*pp_node)->left);
+        }
+        
+        *pp_node = nullptr;
+    }
+
+  public:
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete)
+    {
+        std::unordered_set<int> dddd;
+        for (const auto i : to_delete) {
+            dddd.insert(i);
+        }
+
+        std::vector<TreeNode*> nodes;
+
+        TreeNode** pp_root = &root;
+        delNodesHelper(pp_root, dddd, nodes);
+
+        if (*pp_root != nullptr) {
+            nodes.push_back(*pp_root);
+        }
+
         return nodes;
     }
 };
